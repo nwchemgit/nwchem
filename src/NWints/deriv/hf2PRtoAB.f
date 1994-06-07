814fd593cc0bb8773ea88cea6214f3d91a7055c6
Ricky Kendall <kendallra@ornl.gov>
1994-06-07 00:31:18 +0000

    fix for non-unique centers is in rak

--- src/NWints/deriv/hf2PRtoAB.f
+++ src/NWints/deriv/hf2PRtoAB.f
@@ -1,8 +1,10 @@
-      Subroutine hf2PRtoAB(dP,dR,dA,dB,alpha,ipair,ff,NPP,NPQ,Nint3)
-c $Id: hf2PRtoAB.f,v 1.3 1994-06-02 20:33:14 d3e129 Exp $
+      Subroutine hf2PRtoAB(dP,dR,dA,dB,alpha,ipair,ff,NPP,NPQ,Nint3,
+     &       ictra,ictrb)
+c $Id: hf2PRtoAB.f,v 1.4 1994-06-07 00:31:15 d3e129 Exp $
 
       Implicit real*8 (a-h,o-z)
       Implicit integer (i-n)
+      integer ictra,ictrb
 
 c--> Derivative Integrals WRT (P,R)
 
@@ -37,35 +39,42 @@ c Initialize derivative integrals wrt to (A,B).
        dB(nn) = 0.D0
    10 continue
 
+      if (ictra.eq.ictrb) then
+        do 00100 nn=1,Nint3
+          do 00200 mr = 1,(NPQ*NPP)
+            dA(nn) = dA(nn) + dP(mr,nn)
+00200     continue
+00100   continue
+      else
 c Compute exponent ratios.
 
-      mr = 0
-
-      do 25 mp = 1,NPP
-
-       ap = alpha(1,mp)/(alpha(1,mp) + alpha(2,mp))
-       bp = alpha(2,mp)/(alpha(1,mp) + alpha(2,mp))
-
-       do 20 mq = 1,NPQ
-
-        mr = mr + 1
-
-        ff(1,mr) = ap
-        ff(2,mr) = bp
-
-   20  continue
-
-   25 continue
-
+        mr = 0
+        
+        do 25 mp = 1,NPP
+          
+          ap = alpha(1,mp)/(alpha(1,mp) + alpha(2,mp))
+          bp = alpha(2,mp)/(alpha(1,mp) + alpha(2,mp))
+          
+          do 20 mq = 1,NPQ
+            
+            mr = mr + 1
+            
+            ff(1,mr) = ap
+            ff(2,mr) = bp
+            
+20        continue
+          
+25      continue
+        
 c Transform.
-
-      do 40 nn = 1,Nint3
-
-       do 30 mr = 1,(NPQ*NPP)
-        dA(nn) = dA(nn) + (ff(1,mr)*dP(mr,nn) + dR(mr,nn))
-        dB(nn) = dB(nn) + (ff(2,mr)*dP(mr,nn) - dR(mr,nn))
-   30  continue
-
-   40 continue
-
+        
+        do 40 nn = 1,Nint3
+          
+          do 30 mr = 1,(NPQ*NPP)
+            dA(nn) = dA(nn) + (ff(1,mr)*dP(mr,nn) + dR(mr,nn))
+            dB(nn) = dB(nn) + (ff(2,mr)*dP(mr,nn) - dR(mr,nn))
+30        continue
+          
+40      continue
+      endif
       end
--- src/NWints/deriv/hf2QStoCD.f
+++ src/NWints/deriv/hf2QStoCD.f
@@ -1,8 +1,10 @@
-      Subroutine hf2QStoCD(dQ,dS,dC,dD,alpha,ipair,ff,NPP,NPQ,Nint)
-c $Id: hf2QStoCD.f,v 1.2 1994-05-24 21:51:30 d3e129 Exp $
+      Subroutine hf2QStoCD(dQ,dS,dC,dD,alpha,ipair,ff,NPP,NPQ,Nint,
+     &       ictrc,ictrd)
+c $Id: hf2QStoCD.f,v 1.3 1994-06-07 00:31:17 d3e129 Exp $
 
       Implicit real*8 (a-h,o-z)
       Implicit integer (i-n)
+      integer ictrc,ictrd
 
 c--> Derivative Integrals WRT (Q,S)
 
@@ -36,33 +38,41 @@ c Initialize derivative integrals wrt to (C,D).
        dD(nn) = 0.D0
    10 continue
 
+      if (ictrc.eq.ictrd) then
+        do 00100 nn = 1,Nint
+          do 00200 mr = 1,(NPQ*NPP)
+            dC(nn) = dC(nn) + dQ(mr,nn)
+00200     continue
+00100   continue
+      else
 c Compute exponent ratios.
 
-      do 15 mq = 1,NPQ
-       ff(1,mq) = alpha(1,mq)/(alpha(1,mq) + alpha(2,mq))
-       ff(2,mq) = alpha(2,mq)/(alpha(1,mq) + alpha(2,mq))
-   15 continue
-
-      mr = NPQ
-      do 25 mp = 2,NPP
-       do 20 mq = 1,NPQ
-        mr = mr + 1
-
-        ff(1,mr) = ff(1,mq)
-        ff(2,mr) = ff(2,mq)
-
-   20  continue
-   25 continue
-
+        do 15 mq = 1,NPQ
+          ff(1,mq) = alpha(1,mq)/(alpha(1,mq) + alpha(2,mq))
+          ff(2,mq) = alpha(2,mq)/(alpha(1,mq) + alpha(2,mq))
+15      continue
+        
+        mr = NPQ
+        do 25 mp = 2,NPP
+          do 20 mq = 1,NPQ
+            mr = mr + 1
+            
+            ff(1,mr) = ff(1,mq)
+            ff(2,mr) = ff(2,mq)
+            
+20        continue
+25      continue
+        
 c Transform.
-
-      do 40 nn = 1,Nint
-
-       do 30 mr = 1,(NPQ*NPP)
-        dC(nn) = dC(nn) + (ff(1,mr)*dQ(mr,nn) + dS(mr,nn))
-        dD(nn) = dD(nn) + (ff(2,mr)*dQ(mr,nn) - dS(mr,nn))
-   30  continue
-
-   40 continue
-
+        
+        do 40 nn = 1,Nint
+          
+          do 30 mr = 1,(NPQ*NPP)
+            dC(nn) = dC(nn) + (ff(1,mr)*dQ(mr,nn) + dS(mr,nn))
+            dD(nn) = dD(nn) + (ff(2,mr)*dQ(mr,nn) - dS(mr,nn))
+30        continue
+          
+40      continue
+        
+      endif
       end
