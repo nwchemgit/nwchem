6d0551c062e23121a2f6fc0538553bc8865fc432
David E Bernholdt <bernholdtde@ornl.gov>
1994-07-20 20:54:54 +0000

    Utilit program to check on units for direct access records

---
+++ src/config/da_rec_size.f
@@ -0,0 +1,15 @@
+C     This is a simple program to determine the units of the Fortran
+c     direct access record length.  It writes 2 records of 512 units
+c     each.  Simply run this program and check the size resulting file
+c     'da_rec_size.test' (in bytes) using 'ls' and divide by 1024 units.
+C
+      Program da_rec_size
+      Integer RecLen
+      Parameter (RecLen = 512)
+      Integer A(1)
+      Open(1, FILE='da_rec_size.test', ACCESS='DIRECT', RECL=RecLen)
+      Do I = 1, 2
+         Write (1, REC=I) A
+      EndDo
+      Stop
+      End
