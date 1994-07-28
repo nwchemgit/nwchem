a482c0535dfddd4fa54a1e4c6ff6f20f8b5f354f
Ricky Kendall <kendallra@ornl.gov>
1994-07-28 15:52:04 +0000

    input parse in nwchem style rak

--- src/stepper/stpr_stepper.f
+++ src/stepper/stpr_stepper.f
@@ -1,7 +1,8 @@
       SUBROUTINE stpr_stepper(A,ILEFT,G,C,ETOT,NATD,CONVGE,CONVGG,
-     &    converged)
-c $Id: stpr_stepper.f,v 1.1 1994-06-24 20:42:46 d3e129 Exp $
+     &    converged, rtdb)
+c $Id: stpr_stepper.f,v 1.2 1994-07-28 15:52:03 d3e129 Exp $
       IMPLICIT  REAL*8(A-H,O-Z), INTEGER(I-N)
+      integer rtdb
       logical converged
       DIMENSION G(NATD*3),A(ILEFT)
       DIMENSION C(3,NATD)
@@ -13,14 +14,13 @@ C
 C
 C     Convergence tolerances.
 C
-      if(convgg.eq.0.0d0)CONVGG=1.D-5
-      if(convge.eq.0.0d0)CONVGE=1.D-8
       IF (INEED.GT.ILEFT) THEN
         WRITE(6,*)'  STEPPER| NOT ENOUGH MEMORY NEED ',INEED-ILEFT,
      *         ' MORE WORDS!'
         stop
       ELSE
-        CALL stpr_stepcor(A,INEED,G,C,ETOT,NATD,CONVGE,CONVGG,converged)
+        CALL stpr_stepcor(A,INEED,G,C,ETOT,NATD,CONVGE,CONVGG,converged,
+     &      rtdb)
       ENDIF
       RETURN
       END
