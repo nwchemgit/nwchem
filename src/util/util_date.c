4cb8c890db6d7b68a7ee9d05e8be79af67a1994d
Robert Harrison <robert.harrison@stonybrook.edu>
1994-08-20 00:28:22 +0000

    RJH: Fortran wrappers for standard C functionality

---
+++ src/util/util_date.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#ifndef IPSC
+#include <sys/time.h>
+#endif
+
+extern int string_to_fortchar(char *, int, const char *);
+  
+void util_date_(char *date, int dlen)
+/*
+  Routine to return to FORTRAN the current date in 
+  same format as the C routine ctime.
+
+  
+  character*(*) date
+  call util_date(date)
+*/
+{
+  time_t t = time((time_t *) 0);
+  char *tmp = ctime(&t);
+  tmp[24] = 0;
+  
+  (void) string_to_fortchar(date, dlen, tmp);
+}
