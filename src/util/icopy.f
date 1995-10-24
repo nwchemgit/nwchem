c6a589db8803910a8b6cd3a309e5683a6ea29851
Adrian Wong <rg240>
1995-10-24 19:25:15 +0000

    ATW: Additions for MCSCF

---
+++ src/util/icopy.f
@@ -0,0 +1,15 @@
+      subroutine icopy(n,src,ix,dest,iy)
+      implicit none
+      integer n, src(*), ix, dest(*), iy
+      integer i,ii,jj
+
+      ii = 1
+      jj = 1
+      do i=1,n
+        dest(jj) = src(ii)
+        ii = ii + ix
+        jj = jj + iy
+      enddo
+      return
+      end
+
