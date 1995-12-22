d496676f27f5df194510d5669843d737432c91d6
Krzysztof P Wolinski <wolinski@vsop401.umcs.lublin.pl>
1995-12-22 01:35:31 +0000

    Changes concerning integral's threshold setup ; now it is set by texas_set_accy() called before texas_init is called. Kwol

--- src/NWints/texas/prepint.f
+++ src/NWints/texas/prepint.f
@@ -1,6 +1,7 @@
-c $Id: prepint.f,v 1.4 1995-12-21 18:56:58 pg481 Exp $
+c $Id: prepint.f,v 1.5 1995-12-22 01:35:31 pg481 Exp $
       subroutine prepint2(bl,eps,inuc,ibas,na,nbf,nsh,ncf,ncs,inx,
      *                    lcore,nsym,maxprice,scftype)
+c   !!!///////
 c---------------------------------------------------------------
 c EPS is not used here, threshold is now set by calling 
 c texas_set_accy before texas_init is called.
