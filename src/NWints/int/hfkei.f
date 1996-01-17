f1de1f156d1932984fa37e721664e2044e74bd2e
Ricky Kendall <kendallra@ornl.gov>
1996-01-17 22:03:55 +0000

    first round of general contraction critical path code rak

--- src/NWints/int/hfkei.f
+++ src/NWints/int/hfkei.f
@@ -1,5 +1,5 @@
       Subroutine hfkei(alpha,E,Tab,Ti,Nint,NPP,La,Lb,Li,canAB)
-c $Id: hfkei.f,v 1.2 1994-04-04 20:31:07 d3e129 Exp $
+c $Id: hfkei.f,v 1.3 1996-01-17 22:03:50 d3e129 Exp $
 
       Implicit real*8 (a-h,o-z)
       Implicit integer (i-n)
@@ -176,4 +176,227 @@ c Add Ex*Ey*Tz to Tab.
 
   420 continue
   
+      end
+      Subroutine hfkei_gc(alpha,E,Tab,TabP,TabH,Ti,Nint,
+     &    NCA,NCB,NPP,
+     &    La,Lb,Li,gct_a,gct_b,canAB)
+c
+      Implicit real*8 (a-h,o-z)
+      Implicit integer (i-n)
+
+      Logical canAB
+
+c--> Hermite Linear Expansion Coefficients
+
+      Dimension E(3,NPP,0:((La+Li)+(Lb+Li)),0:(La+Li),0:(Lb+Li))
+
+c--> Exponents
+
+      Dimension alpha(2,NPP)
+
+c--> Kinetic Energy Integrals
+
+      Dimension Tab(Nint*nca*ncb)
+      double precision TabP(NPP,Nint)
+      double precision TabH(NPP,Nint,NCA)
+c--> general contraction matrices
+      double precision gct_a(NPP,NCA) ! [output] general contraction coefs for A multiply
+      double precision gct_b(NCB,NPP) ! [output] general contraction coefs for B multiply
+
+c--> Scratch Space
+
+      Dimension Nxyz(3),Ti(NPP)
+c
+c Compute the kinetic energy integrals.
+c
+c     Formula:                                                       
+c
+c            1  /  Ia,Ib   Ja,Jb   Ka,Kb                       \
+c     Tab =  - | T      Ey      Ez      + "Y-term" + "Z-term"   |
+c            2  \  X       0       0                           /
+c                                                                     
+c      i,j         i-1,j-1      i-1,j+1       i+1,j-1       i+1,j+1   
+c     T      = ijEx       - 2ibEx      - 2ajEx       + 4abEx          
+c      X           0             0            0             0
+c                                                                     
+c******************************************************************************
+  
+c Initialize the block of KEIs.
+
+      call dfill(Nint*nca*ncb,0.0d00,Tab,1)
+      call dfill(NPP*Nint,0.0d00,TabP,1)
+      call dfill(NPP*Nint*NCA,0.0d00,TabH,1)
+c Define the number of shell components on each center.
+
+      La2 = ((La+1)*(La+2))/2
+      Lb2 = ((Lb+1)*(Lb+2))/2
+
+c Loop over shell components.
+
+      nn = 0
+
+      do 420 ma = 1,La2
+
+c Define the angular momentum indices for shell "A".
+
+       call getNxyz(La,ma,Nxyz)
+
+       Ia = Nxyz(1)
+       Ja = Nxyz(2)
+       Ka = Nxyz(3)
+
+       if( canAB )then
+        mb_limit = ma
+       else
+        mb_limit = Lb2
+       end if
+
+       do 410 mb = 1,mb_limit
+
+c Define the angular momentum indices for shell "B".
+
+        call getNxyz(Lb,mb,Nxyz)
+
+        Ib = Nxyz(1)
+        Jb = Nxyz(2)
+        Kb = Nxyz(3)
+
+        nn = nn + 1
+  
+c Build Tx.
+  
+        if( Ia.gt.0 .and. Ib.gt.0 )then
+         do 100 m = 1,NPP
+          Ti(m) =   0.5D0*(        Ia*Ib        )*E(1,m,0,Ia-1,Ib-1)
+     &            -       (        Ia*alpha(2,m))*E(1,m,0,Ia-1,Ib+1)
+     &            -       (alpha(1,m)*Ib        )*E(1,m,0,Ia+1,Ib-1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(1,m,0,Ia+1,Ib+1)
+  100    continue
+        else if( Ia.gt.0 )then
+         do 110 m = 1,NPP
+          Ti(m) = -       (        Ia*alpha(2,m))*E(1,m,0,Ia-1,Ib+1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(1,m,0,Ia+1,Ib+1)
+  110    continue
+        else if( Ib.gt.0 )then
+         do 120 m = 1,NPP
+          Ti(m) = -       (alpha(1,m)*Ib        )*E(1,m,0,Ia+1,Ib-1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(1,m,0,Ia+1,Ib+1)
+  120    continue
+        else
+         do 130 m = 1,NPP
+          Ti(m) =   2.0D0*(alpha(1,m)*alpha(2,m))*E(1,m,0,Ia+1,Ib+1)
+  130    continue
+        end if
+  
+c Add Tx*Ey*Ez to Tab
+  
+        do 140 m = 1,NPP
+         TabP(m,nn) = TabP(m,nn) + Ti(m)*E(2,m,0,Ja,Jb)*E(3,m,0,Ka,Kb)
+  140   continue
+  
+c Build Ty.
+  
+        if( Ja.gt.0 .and. Jb.gt.0 )then
+         do 200 m = 1,NPP
+          Ti(m) =   0.5D0*(        Ja*Jb        )*E(2,m,0,Ja-1,Jb-1)
+     &            -       (        Ja*alpha(2,m))*E(2,m,0,Ja-1,Jb+1)
+     &            -       (alpha(1,m)*Jb        )*E(2,m,0,Ja+1,Jb-1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(2,m,0,Ja+1,Jb+1)
+  200    continue
+        else if( Ja.gt.0 )then
+         do 210 m = 1,NPP
+          Ti(m) = -       (        Ja*alpha(2,m))*E(2,m,0,Ja-1,Jb+1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(2,m,0,Ja+1,Jb+1)
+  210    continue
+        else if( Jb.gt.0 )then
+         do 220 m = 1,NPP
+          Ti(m) = -       (alpha(1,m)*Jb        )*E(2,m,0,Ja+1,Jb-1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(2,m,0,Ja+1,Jb+1)
+  220    continue
+        else
+         do 230 m = 1,NPP
+          Ti(m) =   2.0D0*(alpha(1,m)*alpha(2,m))*E(2,m,0,Ja+1,Jb+1)
+  230    continue
+        end if
+  
+c Add Ex*Ty*Ez to Tab.
+  
+        do 240 m = 1,NPP
+         TabP(m,nn) = TabP(m,nn) + E(1,m,0,Ia,Ib)*Ti(m)*E(3,m,0,Ka,Kb)
+  240   continue
+  
+c Build Tz.
+  
+        if( Ka.gt.0 .and. Kb.gt.0 )then
+         do 300 m = 1,NPP
+          Ti(m) =   0.5D0*(        Ka*Kb        )*E(3,m,0,Ka-1,Kb-1)
+     &            -       (        Ka*alpha(2,m))*E(3,m,0,Ka-1,Kb+1)
+     &            -       (alpha(1,m)*Kb        )*E(3,m,0,Ka+1,Kb-1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(3,m,0,Ka+1,Kb+1)
+  300    continue
+        else if( Ka.gt.0 )then
+         do 310 m = 1,NPP
+          Ti(m) = -       (        Ka*alpha(2,m))*E(3,m,0,Ka-1,Kb+1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(3,m,0,Ka+1,Kb+1)
+  310    continue
+        else if( Kb.gt.0 )then
+         do 320 m = 1,NPP
+          Ti(m) = -       (alpha(1,m)*Kb        )*E(3,m,0,Ka+1,Kb-1)
+     &            + 2.0D0*(alpha(1,m)*alpha(2,m))*E(3,m,0,Ka+1,Kb+1)
+  320    continue
+        else
+         do 330 m = 1,NPP
+          Ti(m) =   2.0D0*(alpha(1,m)*alpha(2,m))*E(3,m,0,Ka+1,Kb+1)
+  330    continue
+        end if
+  
+c Add Ex*Ey*Tz to Tab.
+  
+        do 340 m = 1,NPP
+         TabP(m,nn) = TabP(m,nn) + E(1,m,0,Ia,Ib)*E(2,m,0,Ja,Jb)*Ti(m)
+  340   continue
+
+  410  continue
+
+  420 continue
+  
+c
+c take primitives and half transformed multiplied by A general contraction matrix
+
+      do 10100 ica = 1,NCA
+        do 10200 nn = 1,Nint
+          do 10300 mp = 1,NPP
+            TabH(mp,nn,ica) = TabP(mp,nn)*gct_a(mp,ica)
+10300     continue
+10200   continue
+10100 continue
+c
+      nn = 0
+      do 10400 ica = 1,NCA
+        do 10500 icb = 1,NCB
+          do 10600 iii = 1,Nint
+*            if (iwiw.eq.0)
+*     &            write(6,*)'complex integrals for ica = ',
+*     &            ica,'and icb = ',icb
+            iwiw = iwiw + 1
+            nn = nn + 1
+            Tab(nn) = 0.0d00
+            do 10700 mp = 1,NPP
+              e_tmp = TabH(mp,iii,ica)*gct_b(icb,mp)
+              Tab(nn) = Tab(nn) + e_tmp
+*              write(6,10000)
+*     &              ' int=',nn,' mp =',mp,
+*     &              ' val = ',e_tmp,' integral=',Tab(nn)
+10700       continue
+*            write(80,*)' complex ',ica,icb,iii,Tab(nn),nn
+10600     continue
+10500   continue
+10400 continue
+10000 format(a,i4,a,i4,a,1pd20.10,a,1pd20.10)
+c
+c      copy integrals
+c
+      call dcopy((nint*nca*ncb),Tab,1,TabH,1)
+      call hf1_tran_shift(Tab,TabH,(nca*ncb),la,lb,nca,ncb)
+c
       end
--- src/NWints/int/hfnai.f
+++ src/NWints/int/hfnai.f
@@ -1,5 +1,5 @@
       Subroutine hfnai(E,R0,IJK,Vab,Nint,NPP,La,Lb,Li,Lp,Lp3,canAB)
-c $Id: hfnai.f,v 1.2 1994-04-04 20:31:10 d3e129 Exp $
+c $Id: hfnai.f,v 1.3 1996-01-17 22:03:51 d3e129 Exp $
 
       Implicit real*8 (a-h,o-z)
       Implicit integer (i-n)
@@ -94,4 +94,146 @@ c Define the angular momentum indices for shell "B".
 
    50 continue
 
+      end
+      Subroutine hfnai_gc(E,R0,IJK,Vab,VabP,VabH,Nint,
+     &    NCA,NCB,NPP,
+     &    La,Lb,Li,Lp,Lp3,
+     &    gct_a,gct_b,canAB)
+
+      Implicit real*8 (a-h,o-z)
+      Implicit integer (i-n)
+
+      Logical canAB
+
+c--> Hermite Linear Expansion Coefficients
+
+      Dimension E(3,NPP,0:((La+Li)+(Lb+Li)),0:(La+Li),0:(Lb+Li))
+
+c--> Auxiliary Function Integrals & Index
+
+      Dimension R0(NPP,Lp3),IJK(0:Lp,0:Lp,0:Lp)
+
+c--> Nuclear Attraction Integrals
+
+      Dimension Vab(Nint*nca*ncb)
+      double precision VabP(NPP,Nint)
+      double precision VabH(NPP,Nint,NCA)
+c--> general contraction matrices
+      double precision gct_a(NPP,NCA) ! [output] general contraction coefs for A multiply
+      double precision gct_b(NCB,NPP) ! [output] general contraction coefs for B multiply
+
+c--> Scratch Space
+
+      Dimension Nxyz(3)
+c
+c Compute the nuclear attraction integrals.
+c
+c     formula:
+c           __
+c           \    Ia,Ib    Ja,Jb    Ka,Kb
+c     Vab = /  Ex     * Ey     * Ez     * R
+c           --   Ip       Jp       Kp      Ip,Jp,Kp
+c        Ip,Jp,Kp
+c
+c******************************************************************************
+
+c Initialize the block of NAIs.
+
+      call dfill(Nint*nca*ncb,0.0d00,Vab,1)
+      call dfill(NPP*Nint,0.0d00,VabP,1)
+      call dfill(NPP*Nint*NCA,0.0d00,VabH,1)
+
+c Define the number of shell components on each center.
+
+      La2 = ((La+1)*(La+2))/2
+      Lb2 = ((Lb+1)*(Lb+2))/2
+
+c Loop over shell components.
+
+      nn = 0
+
+      do 50 ma = 1,La2
+
+c Define the angular momentum indices for shell "A".
+
+       call getNxyz(La,ma,Nxyz)
+
+       Ia = Nxyz(1)
+       Ja = Nxyz(2)
+       Ka = Nxyz(3)
+
+       if( canAB )then
+        mb_limit = ma
+       else
+        mb_limit = Lb2
+       end if
+
+       do 40 mb = 1,mb_limit
+
+c Define the angular momentum indices for shell "B".
+
+        call getNxyz(Lb,mb,Nxyz)
+
+        Ib = Nxyz(1)
+        Jb = Nxyz(2)
+        Kb = Nxyz(3)
+
+        nn = nn + 1
+
+        do 30 Ip = 0,Ia+Ib
+        do 30 Jp = 0,Ja+Jb
+        do 30 Kp = 0,Ka+Kb
+
+         np = IJK(Ip,Jp,Kp)
+
+         do 20 mp = 1,NPP
+          VabP(mp,nn) = VabP(mp,nn) +
+     &        (E(1,mp,Ip,Ia,Ib)*
+     &         E(2,mp,Jp,Ja,Jb)*
+     &         E(3,mp,Kp,Ka,Kb))  *R0(mp,np)
+   20    continue
+
+   30   continue
+
+   40  continue
+
+   50 continue
+
+c take primitives and half transformed multiplied by A general contraction matrix
+
+      do 10100 ica = 1,NCA
+        do 10200 nn = 1,Nint
+          do 10300 mp = 1,NPP
+            VabH(mp,nn,ica) = VabP(mp,nn)*gct_a(mp,ica)
+10300     continue
+10200   continue
+10100 continue
+c
+      nn = 0
+      do 10400 ica = 1,NCA
+        do 10500 icb = 1,NCB
+          do 10600 iii = 1,Nint
+*            if (iwiw.eq.0)
+*     &            write(6,*)'complex integrals for ica = ',
+*     &            ica,'and icb = ',icb
+            iwiw = iwiw + 1
+            nn = nn + 1
+            Vab(nn) = 0.0d00
+            do 10700 mp = 1,NPP
+              e_tmp = VabH(mp,iii,ica)*gct_b(icb,mp)
+              Vab(nn) = Vab(nn) + e_tmp
+*              write(6,10000)
+*     &              ' int=',nn,' mp =',mp,
+*     &              ' val = ',e_tmp,' integral=',Vab(nn)
+10700       continue
+*            write(80,*)' complex ',ica,icb,iii,Vab(nn),nn
+10600     continue
+10500   continue
+10400 continue
+10000 format(a,i4,a,i4,a,1pd20.10,a,1pd20.10)
+c
+c      copy integrals
+c
+      call dcopy((nint*nca*ncb),Vab,1,VabH,1)
+      call hf1_tran_shift(Vab,VabH,(nca*ncb),la,lb,nca,ncb)
       end
