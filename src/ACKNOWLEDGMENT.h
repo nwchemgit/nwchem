ff53a091331810cc717f1983d570077e96ab22da
Ricky Kendall <kendallra@ornl.gov>
1996-03-07 01:23:22 +0000

    legal disclaimer stuff added

---
+++ src/ACKNOWLEDGMENT.h
@@ -0,0 +1,16 @@
+/**********************************************************************\
+$Id: ACKNOWLEDGMENT.h,v 1.1 1996-03-07 01:22:56 d3e129 Exp $
+\**********************************************************************/
+
+
+/**********************************************************************\
+                            ACKNOWLEDGMENT
+************************************************************************
+This software and its documentation were produced with Government
+support under Contract Number DE-AC06-76RLO-1830 awarded by the United
+States Department of Energy.  The Government retains a paid-up
+non-exclusive, irrevocable worldwide license to reproduce, prepare
+derivative works, perform publicly and display publicly by or for the
+Government, including the right to distribute to other Government
+contractors.
+\**********************************************************************/
---
+++ src/COPYRIGHT.make.h
@@ -0,0 +1,10 @@
+#
+# $Id: COPYRIGHT.make.h,v 1.1 1996-03-07 01:23:04 d3e129 Exp $
+#
+# COPYRIGHT (C) 1994, 1995, 1996, 
+#    Pacific Northwest National Laboratory, 
+#    Battelle Memorial Institute.  
+#--------------------------------------
+#--------> All Rights Reserved <-------
+#--------------------------------------
+#
