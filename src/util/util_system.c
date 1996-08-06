d05a9d654bb87c1fb3bb3f05184ef5a74490f507
Robert Harrison <robert.harrison@stonybrook.edu>
1996-08-06 05:40:05 +0000

    Routine to mask exceptions on DEC Alpha, and wrapper around systetm() call

---
+++ src/util/util_system.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+
+extern int system(const char *);
+
+#ifdef CRAY
+#include <fortran.h>
+#endif
+
+typedef long Integer;		/*  FORTRAN integer */
+
+#ifdef CRAY
+int fortchar_to_string(_fcd, int, char *, const int);
+#else
+int fortchar_to_string(const char *, int, char *, const int);
+#endif
+
+void ga_error(const char *, long);
+
+#ifdef CRAY
+Integer UTIL_SYSTEM(_fcd input)
+{
+    int lin  = _fcdlen(input);
+#else
+Integer util_system_(const char *input, int lin)
+{
+#endif
+    char in[1024];
+    if (!fortchar_to_string(input, lin, in, sizeof(in)))
+	ga_error("util_file_unlink: fortchar_to_string failed for in",0);
+
+#ifdef CRAY
+    return 1;			/* Does not work on the Cray? */
+#else
+    return system(in);
+#endif
+}
+
