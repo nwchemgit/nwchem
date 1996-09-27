7a44235e9627d124f478120fc27b6e7ea922b2d7
Tjerk Straatsma <straatsmatp@ornl.gov>
1996-09-27 15:29:25 +0000

    call to util_cpusec to replace call to etime

--- src/selci/gettim.f
+++ src/selci/gettim.f
@@ -1,17 +1,17 @@
       subroutine selci_gettim(cpud,elapsd)
       implicit double precision (a-h,o-z)
-      real*4 etime,tt(2),elast,timediff
-      external etime
+      real*4 elast,timediff
+      external util_cpusec
       integer last,ienter
       save ienter,last,elast
       data ienter/0/
       if(ienter.eq.0) then
         ienter=1
         call cputm(last)
-        elast = etime(tt)
+        elast = util_cpusec()
       endif
       call cputm(now)
       elapsd = dfloat(now-last)*0.01d0
-      timediff = etime(tt) - elast
+      timediff = util_cpusec() - elast
       cpud = dble(timediff)
       end
