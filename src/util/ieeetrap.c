1019a04a07be6565fc89f521e78dc587e658de1c
Robert Harrison <robert.harrison@stonybrook.edu>
1996-10-04 21:50:25 +0000

    larger field for time output

--- src/util/ieeetrap.c
+++ src/util/ieeetrap.c
@@ -1,19 +1,34 @@
-/*$Id: ieeetrap.c,v 1.2 1995-02-02 17:51:26 d3g681 Exp $*/
+/*$Id: ieeetrap.c,v 1.3 1996-10-04 21:50:24 d3g681 Exp $*/
 #include <floatingpoint.h>
 #include <stdio.h>
 #include <signal.h>
+#include <ucontext.h>
 
-static void catchit()
+static int nunderflow;
+static int ninexact;
+static int ninvalid; 
+
+static void catchunderflow(int sig, siginfo_t *sip, ucontext_t *uap)
 {
-  printf("!!  Floating point interrupt caught  !!\n");
-  fflush(stdout);
-/*  (void) signal(SIGIOT, SIG_DFL);*/
-  abort();
+    nunderflow++;
+    printf(" !! #underflows = %d\n", nunderflow);
+}
+
+static void catchinexact(int sig, siginfo_t *sip, ucontext_t *uap)
+{
+    ninexact++;
+    printf(" !! #inexacts = %d\n", ninexact);
+}
+
+static void catchinvalid(int sig, siginfo_t *sip, ucontext_t *uap)
+{
+    ninvalid++;
+    printf(" !! #invalids = %d\n", ninvalid);
 }
 
 void ieeetrap_()
 {
-  (void) ieee_handler("set","common", SIGFPE_ABORT);
+    /* (void) ieee_handler("set","common", SIGFPE_ABORT); */
 
 /*
   (void) ieee_handler("set","inexact", SIGFPE_IGNORE);
@@ -21,10 +36,10 @@ void ieeetrap_()
   (void) ieee_handler("set","invalid", SIGFPE_IGNORE);
 */
 
-/*
- (void) ieee_handler("set","inexact", catchit);
- (void) ieee_handler("set","underflow", catchit);
- (void) ieee_handler("set","invalid", catchit);
-*/
+/* (void) ieee_handler("set","inexact", catchinexact);
+ (void) ieee_handler("set","invalid", catchinvalid); */
+
+ (void) ieee_handler("set","underflow", catchunderflow); 
 
 }
+
