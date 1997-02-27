bddb9b3ece7b7eeec8e238e47c0e5401d8a478da
Robert Harrison <robert.harrison@stonybrook.edu>
1997-02-27 00:15:52 +0000

    attempt at rational cray port

--- src/util/util_date.c
+++ src/util/util_date.c
@@ -1,46 +1,37 @@
-/*$Id: util_date.c,v 1.4 1995-02-02 17:51:45 d3g681 Exp $*/
+/*$Id: util_date.c,v 1.5 1997-02-27 00:15:52 d3g681 Exp $*/
 #include <sys/types.h>
 #include <time.h>
 #ifndef IPSC
 #include <sys/time.h>
 #endif
+
 #ifdef CRAY
 #define util_date_ UTIL_DATE
+#include <fortran.h>
+int string_to_fortchar(_fcd, int, const char *);
+#else
+int string_to_fortchar(char *, int, const char *);
 #endif
-extern int string_to_fortchar(char *, int, const char *);
-  
-void util_date_(date)
-     char *date;
+
 /*
   Routine to return to FORTRAN the current date in 
   same format as the C routine ctime.
 
-  
   character*(*) date
   call util_date(date)
 */
-{
-#ifdef CRAY
-  char farray[256];
-  int len,i;
-#endif
-  int nlen = 26;
 
+
+#ifdef CRAY
+void util_date_(_fcd date)
+{
+  int  nlen = _fcdlen(date);
+#else
+void util_date_(char *date, int nlen)
+{
+#endif
   time_t t = time((time_t *) 0);
   char *tmp = ctime(&t);
-  tmp[24] = 0;
   
-#ifdef CRAY
-  len = strlen(tmp);
-
-  for (i=0; i<len; i++)
-    farray[i] = tmp[i];
-  for (i=len; i<nlen; i++)
-    farray[i] = ' ';
-  strcpy( date, tmp);
-#else
   (void) string_to_fortchar(date, nlen, tmp);
-#endif
 }
-
-
