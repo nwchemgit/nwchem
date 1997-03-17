aad19b1873ba43a726aa09fd11be463c399949f8
Ricky Kendall <kendallra@ornl.gov>
1997-03-17 21:41:41 +0000

    upgrade to lapack 2.0

---
+++ src/blas/single/csscal.f
@@ -0,0 +1,32 @@
+      subroutine  csscal(n,sa,cx,incx)
+*
+* $Id: csscal.f,v 1.1 1997-03-17 21:41:38 d3e129 Exp $
+*
+c
+c     scales a complex vector by a real constant.
+c     jack dongarra, linpack, 3/11/78.
+c     modified 3/93 to return if incx .le. 0.
+c     modified 12/3/93, array(1) declarations changed to array(*)
+c
+      complex cx(*)
+      real sa
+      integer i,incx,n,nincx
+c
+      if( n.le.0 .or. incx.le.0 )return
+      if(incx.eq.1)go to 20
+c
+c        code for increment not equal to 1
+c
+      nincx = n*incx
+      do 10 i = 1,nincx,incx
+        cx(i) = cmplx(sa*real(cx(i)),sa*aimag(cx(i)))
+   10 continue
+      return
+c
+c        code for increment equal to 1
+c
+   20 do 30 i = 1,n
+        cx(i) = cmplx(sa*real(cx(i)),sa*aimag(cx(i)))
+   30 continue
+      return
+      end
