7d0f903e7b3bf27b79aac57d0bee1bd310c20131
Ricky Kendall <kendallra@ornl.gov>
1997-03-17 20:34:39 +0000

    perl script changes

---
+++ src/config/dbl2sngl.pl
@@ -0,0 +1,122 @@
+#
+# $Id: dbl2sngl.pl,v 1.1 1997-03-17 20:34:26 d3e129 Exp $
+#
+#
+#
+# perl script to do transliteration from "double" values to "single" values
+#
+# The script is envoked with the command:
+#    perl dbl2sngl.pl file1.f [file2.f ...]
+#
+# This script uses $NWCHEM_TOP/src/config/data.dbl2sngl which is a datafile 
+# that containes the specific transliterations for "double" to "single" in 
+# simple ascii format.  If you need to add a conversion see the comments in 
+# the data file.
+# 
+# Written:  3/14/97
+# By:       Ricky A. Kendall
+#           High Performance Computational Chemistry Group
+#           Theory Modeling and Simulation Program
+#           Environmental Molecular Sciences Laboratory
+#           Pacific Northwest National Laboratory
+#           P.O. Box 999
+#           Richland, WA 99352-0999
+#           email: ra_kendall@pnl.gov
+#
+$debug = 0;
+$printit = 0;
+@from = ();
+@to   = ();
+$data_path = $ENV{'NWCHEM_TOP'} ;
+if ($data_path eq "") {
+    print "Error: environment variable NWCHEM_TOP is not set\n";
+    print "dbl2sngl: Fatal error\n" ;
+    exit 1;
+}
+if($debug) {print "{$data_path} \n";}
+$data_path = $data_path . "/src/config/data.dbl2sngl";
+if($debug) {print "{$data_path} \n";}
+open (DATA,$data_path) || die " unable to open: $data_path \n";
+while (<DATA>)
+{
+    if (/^[^\#]/) {
+	if($debug) {print $_;}
+	@tokens = split(' ');      
+	$num_tokens = @tokens ;
+	if($debug){print "tokens: @tokens $#tokens $num_tokens \n";}
+	push(from,$tokens[0]);
+	push(to,  $tokens[1]);
+    }
+}
+close (DATA);
+if($debug){
+    print "from array @from \n";
+    print "to   array @to \n";
+}
+$num_from = @from;
+$num_to   = @to;
+if ($num_from != $num_to) {
+    print "To and From token count not identical\n";
+    print "Number of From tokens: $num_from\n";
+    print "number of To   tokens: $num_to\n";
+    die "Fatal dbl2sngl error\n";
+}
+else
+{
+    if ($debug){
+	print "Number of From tokens: $num_from\n";
+	print "number of To   tokens: $num_to\n";
+    }
+    $num_compare = $num_from;
+}
+if ($debug) { print "arguments: @ARGV\n";}
+foreach $file (@ARGV){
+    if ($debug){print "file        : $file\n";}
+    $filebak = $file . ".$$" ;
+    if ($debug){print "backup file : $filebak\n";}
+    rename($file,$filebak);
+    $file = '>' . $file;
+    open(FILETOFIX,$filebak) || die "Could not open file: $filebak\n";
+    open(FIXEDFILE,$file) || die "Could not open file: $file\n";
+    while (<FILETOFIX>) {
+	if (/^[ \d]/){
+	    for ($compare = 0; $compare < $num_compare ; $compare++)
+	    {
+		if ($debug) {
+		    $printit = 0;
+		    if ($from[$compare] eq "dcopy") {$printit = 1;}
+		}
+		if ($printit && $debug) {print "from:$from[$compare]  to:$to[$compare]\n" ;}
+		if ($printit && $debug) {print "one  : $_";}
+		s/(\W{1})$from[$compare](\W{1})/$1$to[$compare]$2/gi ;
+		if ($printit && $debug) {print "two  : $_";}
+#		s/$[(\W{1})]$from[$compare]/$1$to[$compare]/gi ;
+		if ($printit && $debug) {print "three: $_";}
+	    }
+	}
+	if (/^[ ]{5}[^\s]/) {
+	    for ($compare = 0; $compare < $num_compare ; $compare++)
+	    {
+		if ($debug) {
+		    $printit = 0;
+		    if ($from[$compare] eq "dcopy") {$printit = 1;}
+		}
+		if ($printit && $debug) {print "four : $_";}
+		s/([ ]{5}.)$from[$compare](\W{1})/$1$to[$compare]$2/gi ;
+		if ($printit && $debug) {print "five : $_";}
+#		s/$$from[$compare]/$to[$compare]/gi ;
+		if ($printit && $debug) {print "six  : $_";}
+	    }
+	}
+	
+	if ($debug) {print $_ ;}
+	print FIXEDFILE $_;
+    }
+    close(FIXEDFILE);
+    close(FILETOFIX);
+    unlink($filebak);
+}
+
+
+	
+	
---
+++ src/config/hasblas.pl
@@ -0,0 +1,86 @@
+#
+# $Id: hasblas.pl,v 1.1 1997-03-17 20:34:28 d3e129 Exp $
+#
+# perl script searches for both "double" and "single" values of the 
+# blas and lapack routines reporting only those files that have a 
+# recognized routine.
+#
+# The script is envoked with the command:
+#    perl hasblas.pl file1.f [file2.f ...]
+#
+# This script uses $NWCHEM_TOP/src/config/data.dbl2sngl which is a datafile 
+# that containes the specific transliterations for "double" to "single" in 
+# simple ascii format.  If you need to add a conversion see the comments in 
+# the data file.
+#
+# 
+# Written:  3/14/97
+# By:       Ricky A. Kendall
+#           High Performance Computational Chemistry Group
+#           Theory Modeling and Simulation Program
+#           Environmental Molecular Sciences Laboratory
+#           Pacific Northwest National Laboratory
+#           P.O. Box 999
+#           Richland, WA 99352-0999
+#           email: ra_kendall@pnl.gov
+#
+#
+$debug = 0;
+$printit = 0;
+@tokens = ();
+$data_path = $ENV{'NWCHEM_TOP'} ;
+if ($data_path eq "") {
+    print "Error: environment variable NWCHEM_TOP is not set\n";
+    print "dbl2sngl: Fatal error\n" ;
+    exit 1;
+}
+if($debug) {print "{$data_path} \n";}
+$data_path = $data_path . "/src/config/data.dbl2sngl";
+if($debug) {print "{$data_path} \n";}
+open (DATA,$data_path) || die " unable to open: $data_path \n";
+while (<DATA>)
+{
+    if (/^[^\#]/) {
+	if($debug) {print $_;}
+	@newtokens = split(' ');      
+	$num_tokens = @newtokens ;
+	if($debug){print "tokens: @newtokens $#newtokens $num_tokens \n";}
+	push(tokens,@newtokens);
+    }
+}
+close (DATA);
+$num_tokens = @tokens;
+if ($debug){
+    print "tokens array @tokens \n";
+    print "number of tokens: $num_tokens\n";
+}
+if ($debug) { print "arguments: @ARGV\n";}
+@found_files = ();
+foreach $file (@ARGV){
+    if ($debug){print "file        : $file\n";}
+    open(FIXEDFILE,$file) || die "Could not open file: $file\n";
+    $found = 0;
+    $lines = 0;
+  FOUNDIT: {  
+      while (<FIXEDFILE>) {
+	  $lines ++;
+	  if (/^[ \d]/){
+	      $itok = 0;
+	      while ($itok < $num_tokens && (!($found)))
+	      {
+		  if (/[\W{1}]$tokens[$itok][\W{1}]/i) {
+		      $found++;
+		      last FOUNDIT;
+		  }
+		  $itok++;
+	      }
+	  }
+      }
+  }
+    if ($found) {push(found_files,$file);}
+    close(FIXEDFILE);
+}
+$num_found_files = @found_files;
+if ($num_found_files) {
+    print "hasblas: found $num_found_files files: @found_files\n";
+}
