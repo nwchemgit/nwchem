e0ab51c3363bda6d8beb925dae7e849781fdd26b
Ricky Kendall <kendallra@ornl.gov>
1997-05-29 00:54:29 +0000

    changes to find NAN's

---
+++ src/develop/raktestc.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#if defined(SOLARIS)
+#define WORKS_FOR 1
+#endif
+
+#if defined(WORKS_FOR)
+#include <ieeefp.h>
+#endif
+
+#if defined(CRAY)
+int IS_THIS_VAL_OKAY(double *value)
+#else
+int is_this_val_okay_(double *value)
+#endif
+{
+#if defined(WORKS_FOR)
+/*
+ -1 unknown
+  0 zero
+  1 nan
+  2 infinity
+  3 nonzero
+ */
+    double val;
+    fpclass_t fp_res;
+    int ret_val;
+
+    val = *value;
+    fp_res = fpclass (val);
+
+    if (fp_res == FP_NZERO || fp_res == FP_PZERO)
+	{
+	    ret_val = 0;
+	}
+    else if (fp_res == FP_SNAN || fp_res == FP_QNAN) 
+	{
+	    ret_val = 1;
+	}
+    else if (fp_res == FP_NINF || fp_res == FP_PINF)
+	{
+	    ret_val = 2;
+	}
+    else if (fp_res == FP_NDENORM || fp_res == FP_PDENORM || fp_res == FP_NNORM || fp_res == FP_PNORM)
+	{
+	    ret_val = 3;
+	}
+    else
+	{
+	    ret_val = -1;
+	}
+    return ret_val;
+#else
+    return (int) 3;
+#endif
+}
