971044bce0f84766cfe9400c963dca29bc707120
Ricky Kendall <kendallra@ornl.gov>
1997-06-16 22:13:30 +0000

    moved first \#endif to after onbitmask_; checked this with atwong; rak

--- src/moints/moints_cutil.c
+++ src/moints/moints_cutil.c
@@ -17,13 +17,13 @@ long onbitmask_( long *len )
 int ONBITMASK( int *len )
 #else
 int onbitmask_( int *len )
+#endif
 {
   unsigned int		mask;
 
   mask = ~((~0) << *len);
   return ((int)mask);
 }
-#endif
 #endif
 
   
