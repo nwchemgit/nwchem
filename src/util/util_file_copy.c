0f44e4a24c412ffa985f434dd43509b513193b11
Robert Harrison <robert.harrison@stonybrook.edu>
1998-06-16 17:18:23 +0000

    added C interface for parallel file copy ... need to do a Fortran one

--- src/util/util_file_copy.c
+++ src/util/util_file_copy.c
@@ -1,5 +1,5 @@
 /*
- $Id: util_file_copy.c,v 1.2 1997-10-31 20:45:37 d3e129 Exp $
+ $Id: util_file_copy.c,v 1.3 1998-06-16 17:18:23 d3g681 Exp $
  */
 
 #include <stdio.h>
@@ -48,6 +48,65 @@ void util_file_copy(const char *input, const char *output)
 	
     (void) fclose(fin);
     (void) fclose(fout);
+}
+
+void util_file_parallel_copy(const char *input, const char *output)
+/*
+  Process 0 broadcasts the input file to all nodes which write to their
+  output file.  Any existing file is overwritten.  Any error is fatal.
+
+  It is same to call it with a same/different output file name on all nodes.
+  If the input and output files have the same name on node0 it is also safe.
+  */
+{
+    FILE *fin=0, *fout=0;
+    int differ = strcmp(input,output);
+
+    if (ga_nodeid_() == 0) {
+      if (!(fin = fopen(input, "r"))) {
+	fprintf(stderr,"util_file_copy: unable to open input %s\n", input);
+	ga_error("util_file_parallel_copy",0);
+      }
+      if (differ) {
+	if (!(fout = fopen(output, "w+"))) {
+	  fprintf(stderr,"util_file_copy: unable to open output %s\n", input);
+	  ga_error("util_file_parallel_copy",0);
+	}
+      }
+    }
+    else if (!(fout = fopen(output, "w+"))) {
+      fprintf(stderr,"util_file_copy: unable to open output %s\n", input);
+      ga_error("util_file_parallel_copy",0);
+    }
+
+    while (1) {
+      char buf[8192];
+      int nread, msgnread=44,msgbuf=45,msglen=sizeof(int),node0=0;
+      if (ga_nodeid_() == 0)
+	nread = fread(buf, 1, sizeof(buf), fin);
+      ga_brdcst_(&msgnread, &nread, &msglen, &node0);
+      if (nread > 0) {
+	ga_brdcst_(&msgbuf, buf, &nread, &node0);
+	if ((ga_nodeid_() != 0) || (differ != 0)) {
+	  if (fwrite(buf, 1, nread, fout) != nread) {
+	    fprintf(stderr,"util_file_parallel_copy: failed writing %s\n", output);
+	    ga_error("util_file_parallel_copy",0);
+	  }
+	}
+      }
+      else
+	break;
+    }
+	
+    if (ga_nodeid_() == 0) {
+      if (!feof(fin)) {
+	fprintf(stderr,"util_file_parallel_copy: failed reading %s\n", input);
+	ga_error("util_file_parallel_copy",0);
+      }
+    }
+	
+    if (fin) (void) fclose(fin);
+    if (fout) (void) fclose(fout);
 }
 
 #ifdef CRAY
