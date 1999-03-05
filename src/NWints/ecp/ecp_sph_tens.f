1ec751e21cc09b4585b3e78a75f1ec7f27c91f25
Kenneth Dyall <dyall@schrodinger.com>
1999-03-05 22:32:49 +0000

    code for zero distance case introduced

--- src/NWints/ecp/ecp_sph_tens.f
+++ src/NWints/ecp/ecp_sph_tens.f
@@ -1,4 +1,4 @@
-C $Id: ecp_sph_tens.f,v 1.4 1997-09-08 18:13:12 mg201 Exp $
+C $Id: ecp_sph_tens.f,v 1.5 1999-03-05 22:32:49 mg201 Exp $
 ************************************************************************
 *                                                                      *
       subroutine ecp_sph_tens (l,n_n,n_t,R,X,Y,Z,xn,yn,zn,tmp,G_kq,
@@ -24,49 +24,53 @@ C $Id: ecp_sph_tens.f,v 1.4 1997-09-08 18:13:12 mg201 Exp $
       integer i,j,k,l,m,ind_c,ind_s,n_n,n_t,mmc,mms
       integer lcsco
       double precision X,Y,Z,R,xr,yr,zr,xn(0:l),yn(0:l),zn(0:l),
-     &    tmp(n_t),G_kq(n_n)
+     &    tmp(n_t),G_kq(n_n),tooclose
       double precision csco(lcsco)
+      data close/1.0d-14/
 *
 *   Set up monomials in X, Y and Z
 *
-      if (R .eq. 0.0d00) call errquit ('Zero distance in ecp_sph_tens',
-     &    99)
-      xn(0) = 1.0d00
-      yn(0) = 1.0d00
-      zn(0) = 1.0d00
-      xr = -X/R
-      yr = -Y/R
-      zr = -Z/R
-      do i = 1,l
-        xn(i) = xn(i-1)*xr
-        yn(i) = yn(i-1)*yr
-        zn(i) = zn(i-1)*zr
-      end do
+      if (R .lt. tooclose) then
+        call dfill(n_n,0.0d00,G_kq,1)
+        G_kq(1) = 1.0d00
+      else
+        xn(0) = 1.0d00
+        yn(0) = 1.0d00
+        zn(0) = 1.0d00
+        xr = -X/R
+        yr = -Y/R
+        zr = -Z/R
+        do i = 1,l
+          xn(i) = xn(i-1)*xr
+          yn(i) = yn(i-1)*yr
+          zn(i) = zn(i-1)*zr
+        end do
 *
 *   Loop over angular momenta on new centre
 *
-      ind_s = 1
-      do m = 0,l
+        ind_s = 1
+        do m = 0,l
 *
 *     Loop over cartesian components on new centre
 *
-        ind_c = 0
-        do i = m,0,-1
-          do j = m-i,0,-1
-            k = m-i-j
-            ind_c = ind_c+1
-            tmp(ind_c) = xn(i)*yn(j)*zn(k)
+          ind_c = 0
+          do i = m,0,-1
+            do j = m-i,0,-1
+              k = m-i-j
+              ind_c = ind_c+1
+              tmp(ind_c) = xn(i)*yn(j)*zn(k)
+            end do
           end do
-        end do
 *
 *     Transform cartesians to sphericals
 *
-        mmc = (m+1)*(m+2)/2
-        mms = 2*m+1
-        call ecp_cstrans (m,mmc,1,m,m,k,tmp,mmc,G_kq(ind_s),mms,
-     &      csco,lcsco,csco,1,-1,1)
-        ind_s = ind_s+mms
-      end do
+          mmc = (m+1)*(m+2)/2
+          mms = 2*m+1
+          call ecp_cstrans (m,mmc,1,m,m,k,tmp,mmc,G_kq(ind_s),mms,
+     &        csco,lcsco,csco,1,-1,1)
+          ind_s = ind_s+mms
+        end do
+      end if
 *
       return
       end
