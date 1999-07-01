ec863ef20cc32e497b2509a48c977b29a05126a4
Ricky Kendall <kendallra@ornl.gov>
1999-07-01 22:21:49 +0000

    added a logical copy

---
+++ src/util/lcopy.f
@@ -0,0 +1,19 @@
+      Subroutine lcopy(n,src,ix,dest,iy)
+*
+* $Id#
+*
+      implicit none
+      integer n, ix, iy
+      logical src(*), dest(*)
+      integer i,ii,jj
+
+      ii = 1
+      jj = 1
+      do i=1,n
+        dest(jj) = src(ii)
+        ii = ii + ix
+        jj = jj + iy
+      enddo
+      return
+      end
+
