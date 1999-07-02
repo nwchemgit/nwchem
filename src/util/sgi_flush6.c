4c8f74bc25388ee66eefc6311c747369a371b581
Ricky Kendall <kendallra@ornl.gov>
1999-07-02 00:30:12 +0000

    routine to flush stdout

---
+++ src/util/sgi_flush6.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void sgi_flush6_(void)
+{
+  int return_code;
+  return_code = fflush(stdout);
+  if (return_code == 0) return;
+  (void) perror("Error detected in sgi_flush6:");
+}
