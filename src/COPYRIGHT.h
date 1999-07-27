5f2460e1a1583d0b4e68e4758ce65019198be00b
Theresa Windus <twindus@iastate.edu>
1999-07-27 17:07:17 +0000

    Add 1999 to copyright info.

--- src/COPYRIGHT.h
+++ src/COPYRIGHT.h
@@ -1,5 +1,5 @@
 /**\
-COPYRIGHT (C) 1994, 1995, 1996, 1997, 1998
+COPYRIGHT (C) 1994, 1995, 1996, 1997, 1998, 1999
     Pacific Northwest National Laboratory, 
     Battelle Memorial Institute.  
 --------------------------------------------
