ad674776c2c11432a61382eb73bbf593e4423ffa
Tjerk Straatsma <straatsmatp@ornl.gov>
1999-09-20 22:02:14 +0000

    mods:wq

--- src/java/Graph.java
+++ src/java/Graph.java
@@ -1,15 +1,17 @@
-// $Id: Graph.java,v 1.1 1999-08-10 14:34:40 d3j191 Exp $
-import ptplot.*;
+// $Id: Graph.java,v 1.2 1999-09-20 22:02:14 d3j191 Exp $
+import ptolemy.plot.*;
 import java.awt.*;
 import java.util.*;
 import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
 
 public class Graph extends Plot{
 
   public boolean first = true, refill = true;
   public double xmin, xmax, ymin, ymax, dx, dy;
 
-  public void addData(int set, double xval, double yval, boolean connect){
+  public void addData(int set, double xval, double yval, boolean connect, boolean doRefill){
     if(first){
       xmin=xval;
       xmax=xval;
@@ -24,17 +26,17 @@ public class Graph extends Plot{
       if(yval>ymax) { ymax=yval+0.2*dy; refill=true; };
     };
     addPoint(set,xval,yval,connect);
-    if(refill) {
+    if(refill && doRefill) {
       setXRange(xmin,xmax);
       setYRange(ymin,ymax);
-      drawPlot(getGraphics(),true);
+      _drawPlot(getGraphics(),true);
     };
     first=false;
     refill=false;
   }
 
   public void removeSet(int dataset){
-    Vector pts = _points[dataset];
+    Vector pts = (Vector)_points.elementAt(dataset);
     pts.removeAllElements();
     _xTop = - Double.MAX_VALUE;
     _yTop = - Double.MAX_VALUE;
--- src/java/whichjava.java
+++ src/java/whichjava.java
@@ -1,11 +1,10 @@
-// $Id: whichjava.java,v 1.1 1999-08-10 14:34:42 d3j191 Exp $
+// $Id: whichjava.java,v 1.2 1999-09-20 22:02:14 d3j191 Exp $
 
 public class whichjava implements Runnable {
   public static void main(String[] args){
     System.out.println("The Java Version in your path is " + System.getProperty("java.version"));
     if (System.getProperty("java.version").indexOf("1.2")>=0){System.exit((int)0);};
-    if (System.getProperty("java.version").indexOf("1.1")>=0){System.exit((int)0);};
-    System.out.println (" This code requires Java 1.1 or greater"); System.exit((int)1);
+    System.out.println (" This code requires Java 1.2 or greater"); System.exit((int)1);
   }
 
   public void init(){};
