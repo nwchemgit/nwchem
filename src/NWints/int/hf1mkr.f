004dd4ecc874076a46ef9c9caeca7e69fc15dab6
Kenneth Dyall <dyall@schrodinger.com>
1999-10-26 21:51:31 +0000

    Add code for finite nucleus, update comments

--- src/NWints/int/hf1mkr.f
+++ src/NWints/int/hf1mkr.f
@@ -1,35 +1,33 @@
-      Subroutine hf1mkr(Axyz,Bxyz,Cxyz,zan,ncenters,
+      Subroutine hf1mkr(Axyz,Bxyz,Cxyz,zan,exinv,ncenters,
      &                  alpha,Pxyz,RS,PC,ff,R,
      &                  R0,R0C,IJK,NPP,Lp,Lp3,CENTER)
-c $Id: hf1mkr.f,v 1.3 1994-05-27 16:01:08 d3e129 Exp $
+c $Id: hf1mkr.f,v 1.4 1999-10-26 21:51:31 mg201 Exp $
 
-      Implicit real*8 (a-h,o-z)
-      Implicit integer (i-n)
-
+      Implicit none
+c::passed
+      Integer ncenters                  ! number of nuclei
+      Integer NPP                       ! number of primitive products
+      Integer Lp                        ! angular momentum sum La+Lb+MXD
+      Integer Lp3                       ! number of angular momentum functions
+      Double precision Axyz(3),Bxyz(3)  ! Cartesian coordinates of centers 
+      Double precision Cxyz(3,ncenters) ! Nuclear cartesian coordinates
+      Double precision zan(ncenters)    ! Nuclear charges
+      Double precision exinv(ncenters)  ! Nuclear inverse exponents
+      Double precision alpha(2,NPP)     ! basis function exponents copy
+      Double precision Pxyz(3,NPP)      ! coordinates of product center
+      Double precision RS(NPP)          ! scale factor
+      Double precision PC(NPP,3)        ! distance from product center to nucleus
+      Double precision ff(2,NPP)        ! recursion factors
+      Double precision R(NPP,0:Lp,Lp3)  ! R/R000j/RIKJj functions
+      Double precision R0(NPP,Lp3)      ! R integrals
+      Double precision R0C(ncenters,NPP,Lp3) ! R integrals per center
+      integer IJK(0:Lp,0:Lp,0:Lp)       ! index array 
       Logical CENTER
-
+c::local
+      integer ic,j,m,n
+      Double precision a,b,f1,f2,alpha_t,beta,PCx,PCy,PCz
+      Double precision PI, PI4          ! pi, 4/pi
       Parameter (PI=3.1415926535898D0,PI4=4.D0/PI)
-
-c--> Cartesian Coordinates
-
-      Dimension Axyz(3),Bxyz(3)
-
-c--> Nuclear Cartesian Coordinates & Charges
-
-      Dimension Cxyz(3,ncenters),zan(ncenters)
-
-c--> Exponents
-
-      Dimension alpha(2,NPP)
-
-c--> Auxiliary Function Integrals & Index
-
-      Dimension R0(NPP,Lp3),R0C(ncenters,NPP,Lp3),IJK(0:Lp,0:Lp,0:Lp)
-
-c--> Scratch Space
-
-      Dimension Pxyz(3,NPP),RS(NPP),PC(NPP,3),ff(2,NPP),R(NPP,0:Lp,Lp3)
-
 c
 c Define the auxiliary function integrals necessary to compute the nuclear
 c attraction integrals (NAIs). These integrals are scaled by an appropriate
@@ -53,28 +51,29 @@ c            a + b       \  a + b      /
 c
 c******************************************************************************
 
-      do 100 mp = 1,NPP
-        do 100 j = 1,Lp3
-          R0(mp,j) = 0.D0
-  100 continue
+      do 101 m = 1,NPP
+       do 100 j = 1,Lp3
+        R0(m,j) = 0.D0
+  100  continue
+  101 continue
 
-      do 110 mp = 1,NPP
+      do 110 m = 1,NPP
 
 c Define the center "P".
 
-       a = alpha(1,mp)
-       b = alpha(2,mp)
+       a = alpha(1,m)
+       b = alpha(2,m)
 
        f1 = a/(a+b)
        f2 = b/(a+b)
 
-       Pxyz(1,mp) = f1*Axyz(1) + f2*Bxyz(1)
-       Pxyz(2,mp) = f1*Axyz(2) + f2*Bxyz(2)
-       Pxyz(3,mp) = f1*Axyz(3) + f2*Bxyz(3)
+       Pxyz(1,m) = f1*Axyz(1) + f2*Bxyz(1)
+       Pxyz(2,m) = f1*Axyz(2) + f2*Bxyz(2)
+       Pxyz(3,m) = f1*Axyz(3) + f2*Bxyz(3)
 
 c Define the scaling factor.
 
-       RS(mp) = sqrt((a+b)*PI4)
+       RS(m) = sqrt((a+b)*PI4)
 
   110 continue
 
@@ -82,12 +81,15 @@ c Sum over all centers.
 
       do 150 ic = 1,ncenters
 
+       beta = exinv(ic)
+
 c Define factors necessary to compute incomplete gamma function and the
 c auxiliary functions.
 
        do 120 m = 1,NPP
 
         alpha_t = alpha(1,m) + alpha(2,m)
+        alpha_t = alpha_t/(1.0d0+alpha_t*beta)
 
         ff(1,m) = RS(m)
         ff(2,m) = -2.D0*alpha_t
@@ -107,6 +109,17 @@ c auxiliary functions.
 c Evaluate the incomplete gamma function.
 
        call igamma(R,NPP,Lp)
+
+c Scale for finite nuclear size if necessary
+
+       if (beta .ne. 0.0D0) then
+        do 125 m = 1,NPP
+         alpha_t = sqrt(1.0d0+(alpha(1,m)+alpha(2,m))*beta)
+         do 126 j=0,Lp
+          R(m,j,1) = R(m,j,1)/alpha_t
+  126    continue
+  125   continue
+       end if
 
 c Define the initial auxiliary functions (i.e., R000j, j=1,Lr).
 
