ec01471e0b1a8ba194f7b75c1ad624bfdbf65a8c
Benny Johnson <johnson@quantumsimulations.com>
1999-11-13 03:20:15 +0000

    Added WIN32 case

--- src/util/util_sleep.c
+++ src/util/util_sleep.c
@@ -1,15 +1,29 @@
 /*
- $Id: util_sleep.c,v 1.3 1997-10-31 20:45:39 d3e129 Exp $
+ $Id: util_sleep.c,v 1.4 1999-11-13 03:20:15 bjohnson Exp $
  */
 
-#ifdef CRAY
+#if defined(CRAY)
+
 void UTIL_SLEEP(long *t)
 {}
+
+#elif defined(WIN32)
+
+#include "winutil.h"
+#include "typesf2c.h"
+void FATR UTIL_SLEEP(long *t) 
+{
+  unsigned s = *t;
+  sleep(s);
+}
+
 #else
+
 #include <unistd.h>
 void util_sleep_(long *t) 
 {
   unsigned s = *t;
   sleep(s);
 }
+
 #endif
