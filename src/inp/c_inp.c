096a34d0b424dfe2f332abda5da295ad69ec2a1f
Benny Johnson <johnson@quantumsimulations.com>
1999-11-13 02:22:25 +0000

    Added USE_FCD to handle FORTRAN/C string interfacing for WIN32

--- src/inp/c_inp.c
+++ src/inp/c_inp.c
@@ -1,16 +1,29 @@
 /*
- $Id: c_inp.c,v 1.5 1999-06-01 20:48:19 d3h325 Exp $
+ $Id: c_inp.c,v 1.6 1999-11-13 02:22:25 bjohnson Exp $
  */
 
+#ifdef CRAY
+#include <fortran.h>
+#endif
 #include "typesf2c.h"
 /* routine to convert a fortran string to a C string: */
 /* Fortran callable version of f2cstring in global directory */
 
+#if defined(CRAY) || defined(USE_FCD)
+void FATR C_CNVT(clen, flen, cfcd, ffcd)
+Integer *clen, *flen;
+_fcd cfcd;
+_fcd ffcd;
+{
+    char *cstr = _fcdtocp(cfcd);
+    char *fstr = _fcdtocp(ffcd);
+#else
 void c_cnvt_(clen, flen, cstr, fstr)
 Integer *clen, *flen;
 char *cstr;
 char *fstr;
 {
+#endif
     int flenl, clenl;
 
     flenl = *flen; clenl = *clen;
