32d7eac828d56e0eb6ee2dc2953682c4113a24da
Benny Johnson <johnson@quantumsimulations.com>
1999-11-13 03:09:57 +0000

    Commented out for WIN32 (temporarily?)

--- src/selci/cputm.c
+++ src/selci/cputm.c
@@ -1,17 +1,23 @@
 /*
- $Id: cputm.c,v 1.4 1997-10-31 23:41:58 d3e129 Exp $
+ $Id: cputm.c,v 1.5 1999-11-13 03:09:57 bjohnson Exp $
  */
 
 #include <sys/types.h>
+#ifdef WIN32
+#include "typesf2c.h"
+#else
 #include <sys/time.h>
+#endif
 
-#if defined(CRAY)
-void CPUTM(ai)
+#if defined(CRAY) || defined(WIN32)
+void FATR CPUTM(ai)
 #else
 void cputm_(ai)
 #endif
 int *ai;
 {
+#ifndef WIN32
+  /* !!! Comment out function for WIN32 just so we can keep going */
 struct timeval tp;
 struct timezone tzp;
 int i;
@@ -26,4 +32,7 @@ int i;
 	i = i*100;
 	i += (tp.tv_usec / 10000);
 	*ai = i;
+#else
+	printf("selci/cputm.c not implemented yet for WIN32\n");
+#endif
 }
