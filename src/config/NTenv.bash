e2b417d8d2d11444d2bf511f0216d20b4644377f
Robert Harrison <robert.harrison@stonybrook.edu>
1999-11-15 21:10:08 +0000

    useful scripts to setup compilation environment

---
+++ src/config/NTenv.bash
@@ -0,0 +1,13 @@
+NWCHEM_TOP_WIN32="E:\users\d3g681\nwchem"; export NWCHEM_TOP_WIN32
+MPI_INCLUDE="E:\Wmpi\Include"; export MPI_INCLUDE
+MPI_LIB="E:\Wmpi\Console"; export MPI_LIB
+
+PATH=${PATH}:/Program\ Files/Microsoft\ Visual\ Studio/Common/Msdev98/BIN:/Program\ Files/Microsoft\ Visual\ Studio/Common/VC98/BIN:/Program\ Files/Microsoft\ Visual\ Studio/Common/DF98/BIN
+
+INCLUDE=C:/Program\ Files/Microsoft\ Visual\ Studio/VC98/ATL/INCLUDE\;C:/Program\ Files/Microsoft\ Visual\ Studio/VC98/INCLUDE\;C:/Program\ Files/Microsoft\ Visual\ Studio/VC98/MFC/INCLUDE
+export INCLUDE
+
+LIB=C:/Program\ Files/Microsoft\ Visual\ Studio/VC98/LIB\;C:/Program\ Files/Microsoft\ Visual\ Studio/DF98/IMSL/LIB\;C:/Program\ Files/Microsoft\ Visual\ Studio/DF98/LIB
+export LIB
+
+
---
+++ src/config/NTenv.bat
@@ -0,0 +1,4 @@
+set NWCHEM_TOP_WIN32=E:\users\d3g681\nwchem
+set MPI_INCLUDE=E:\Wmpi\Include 
+set MPI_LIB=E:\Wmpi\Console 
+
