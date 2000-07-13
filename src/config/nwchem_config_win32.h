fe5e1ca123c0618ad71e17a3f8e575d059fd2540
Edoardo Apra <edoardo.apra@gmail.com>
2000-07-13 23:24:11 +0000

    WIN32: added MD stuff

--- src/config/nwchem_config_win32.h
+++ src/config/nwchem_config_win32.h
@@ -1,20 +1,15 @@
-#
-#	$Id: nwchem_config_win32.h,v 1.2 1999-11-19 00:30:03 d3h325 Exp $
-#
+# This configuration generated automatically on cowboys at Wed Jul 12 14:26:50 PDT 2000
+# Request modules from user: all
 
-# comment out tools temporarily to get build working
-#NW_CORE_SUBDIRS = tools basis geom inp input \
 NW_CORE_SUBDIRS = basis geom inp input \
 	pstat rtdb task symmetry util peigs lapack
 
-NW_MODULE_SUBDIRS = NWints atomscf ddscf gradients moints nwdft rimp2 \
-	stepper driver dftgrad cphf ccsd vib mcscf esp hessian selci dplot \
-	mp2_grad property
+NW_MODULE_SUBDIRS = NWints atomscf ddscf gradients moints nwdft rimp2 stepper driver dftgrad cphf ccsd vib mcscf prepar nwargos esp hessian selci dplot mp2_grad property fft analyz nwmd cafe space 
 
 UTIL_LIBS = util.lib pario.lib global.lib ma.lib peigs.lib linalg.lib \
-	tcgmsg-mpi.lib armci.lib cvwmpi.lib wsock32.lib lapack.lib smaths.lib
+tcgmsg-mpi.lib armci.lib cvwmpi.lib wsock32.lib lapack.lib $(LINK_F90)
 
-LIBS = nwctask.lib ccsd.lib mcscf.lib selci.lib mp2.lib moints.lib \
-	stepper.lib driver.lib dftgrad.lib nwdft.lib gradients.lib cphf.lib \
-	esp.lib ddscf.lib guess.lib hessian.lib vib.lib util.lib rimp2.lib \
-	property.lib nwints.lib dplot.lib $(UTIL_LIBS)
+LIBS = nwctask.lib ccsd.lib mcscf.lib selci.lib mp2.lib moints.lib stepper.lib driver.lib dftgrad.lib nwdft.lib gradients.lib cphf.lib esp.lib ddscf.lib guess.lib hessian.lib vib.lib util.lib rimp2.lib property.lib nwints.lib prepar.lib nwargos.lib nwmd.lib cafe.lib space.lib analyze.lib pfft.lib dplot.lib $(UTIL_LIBS)
+
+#EXCLUDED_SUBDIRS = develop ideaz scfaux hessian plane_wave oimp2 gapss pspw rimp2_grad nbo python vscf uccsdt drdy
+#CONFIG_LIBS = 
