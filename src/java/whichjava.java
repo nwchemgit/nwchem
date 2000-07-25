b4bc3ef81ea53e6a14ccf8b6ed284f85ab2cc696
Tjerk Straatsma <straatsmatp@ornl.gov>
2000-07-25 19:50:51 +0000

    Java 1.3

--- src/java/whichjava.java
+++ src/java/whichjava.java
@@ -1,9 +1,10 @@
-// $Id: whichjava.java,v 1.2 1999-09-20 22:02:14 d3j191 Exp $
+// $Id: whichjava.java,v 1.3 2000-07-25 19:50:51 d3j191 Exp $
 
 public class whichjava implements Runnable {
   public static void main(String[] args){
     System.out.println("The Java Version in your path is " + System.getProperty("java.version"));
     if (System.getProperty("java.version").indexOf("1.2")>=0){System.exit((int)0);};
+    if (System.getProperty("java.version").indexOf("1.3")>=0){System.exit((int)0);};
     System.out.println (" This code requires Java 1.2 or greater"); System.exit((int)1);
   }
 
