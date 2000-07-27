2c97bf9c79da03cd64f53551e51459e6119d60e5
Benny Johnson <johnson@quantumsimulations.com>
2000-07-27 15:51:10 +0000

    Several new functionalities

--- src/config/NTmakelib.h
+++ src/config/NTmakelib.h
@@ -1,34 +1,58 @@
 #
-#	$Id: NTmakelib.h,v 1.3 2000-02-08 22:05:42 bjohnson Exp $
+#	$Id: NTmakelib.h,v 1.4 2000-07-27 15:51:10 bjohnson Exp $
 #
 
 LIBRARY_PATH = $(LIB_DISTRIB)\$(LIBRARY)
 OBJS = $(OBJ_OPTIMIZE) $(OBJ)
 
+!IF EXIST ($(CNFDIR)\win32\file_exists.exe)
 # !!! Use extra program to see if archive exists
-!IF [ $(SRCDIR)\config\file_exists.exe $(LIBRARY_PATH) ]
+!IF [ $(CNFDIR)\win32\file_exists.exe $(LIBRARY_PATH) ]
 LIBRARY_PATH_IF_EXISTS = $(LIBRARY_PATH)
 !ELSE
 LIBRARY_PATH_IF_EXISTS =
 !ENDIF
+!ELSE
+LIBRARY_PATH_IF_EXISTS = path_error
+!ENDIF
 
-$(LIBRARY_PATH): $(OBJDIR) $(LIB_DISTRIB) $(OBJS)
-	$(AR) @<<
-	$(ARFLAGS) $(LIBRARY_PATH_IF_EXISTS) $(OBJS)
-<<
+#
+# Use pseudotarget for library since that is the only way to ensure
+# in general that subdirs will be checked properly.
+#
+library: $(OBJDIR) $(LIB_DISTRIB) $(LIBRARY_PATH)
 !IFDEF SUBDIRS
 	@nmake -nologo foreach_subdir
 !ENDIF
 
+#
+# The use of the $? macro below assumes that any object that needs to
+# be placed into the library is newer than the library. This could fail
+# in principle if the library file is touched (e.g. by another build
+# updating the same library) after a needed object file has been built
+# but before the following rule is executed. This seems very unlikely,
+# but if it proves to be a problem simply replace $? with $(OBJS). The
+# drawback to this is that it causes a "Replacing ..." message to be
+# printed for every object file, not just the ones out of date.
+#
+$(LIBRARY_PATH): $(OBJS)
+	$(AR) @<<
+	$(ARFLAGS) $(LIBRARY_PATH_IF_EXISTS) $?
+<<
+
 WIN32.stamp: $(HEADERS)
 !IFDEF HEADERS
-	!copy $** $(INCDIR)
+	!copy $? $(INCDIR)
 !ENDIF
 	@if exist WIN32.stamp erase WIN32.stamp
 	@echo "" > WIN32.stamp
 !IFDEF SUBDIRS
 	@nmake -nologo SUBDIR_TARGET=WIN32.stamp foreach_subdir
 !ENDIF
+
+#MakeFile: GNUmakefile
+#	@echo Trying to make $@
+#	$(CNFDIR)\win32\convmake.exe
 
 "$(LIB_DISTRIB)" :
 	@if not exist "$(LIB_DISTRIB)/$(NULL)" mkdir "$(LIB_DISTRIB)"
@@ -64,3 +88,58 @@ subdir_target:
 	@echo Making $(SUBDIR_TARGET) in $(TARGET_DIR)
 	@cd $(TARGET_DIR)
 	@nmake -nologo $(SUBDIR_TARGET)
+
+#
+# Rules for automatic generation of MakeFile's from GNUmakefile's
+# whenever possible
+#
+
+#
+# The generation of MakeFile's in subdirs must be carried out in two
+# steps.  We must ensure that the parent dir's MakeFile exists because
+# it is used to determine its own subdirs.  Therefore the first target
+# simply generates the MakeFile's for the current dir's subdirs before
+# the second target steps down into the subdirs and fires up their
+# Makefile's to generate MakeFile's in their subdirs recursively.
+#
+MakeFiles: $(SUBDIRS)
+!IFDEF SUBDIRS
+	!@nmake -nologo TARGET_DIR=$** MakeFile_1
+	!@nmake -nologo TARGET_DIR=$** MakeFile_2
+!ENDIF
+
+MakeFile_1:
+	@echo Generating MakeFile in $(TARGET_DIR)
+	@cd $(TARGET_DIR)
+	@nmake -nologo /F $(CNFDIR)\win32\MakeFile CNFDIR=$(CNFDIR) MakeFile
+
+Makefile_2:
+	@cd $(TARGET_DIR)
+	@nmake -nologo MakeFiles
+
+#
+# To allow 'nmake MakeFile' to be invoked directly in an individual dir,
+# such as after modifying the GNUmakefile by hand.
+#
+MakeFile: GNUmakefile
+	@nmake -nologo /F $(CNFDIR)\win32\MakeFile CNFDIR=$(CNFDIR) MakeFile
+
+#
+# Clean the auto-generated MakeFile's - the dir tree must be traversed
+# depth-first to do this!
+#
+clean_MakeFiles: $(SUBDIRS)
+!IFDEF SUBDIRS
+	!@nmake -nologo TARGET_DIR=$** clean_subdir_MakeFiles
+!ENDIF
+	@$(CNFDIR)\win32\cleanmake.exe $(LVL)
+
+clean_subdir_MakeFiles:
+	@cd $(TARGET_DIR)
+	@nmake -nologo LVL="$(LVL)\$(TARGET_DIR)" clean_MakeFiles
+
+#
+# Make Visual Studio wrapper project for a MakeFile
+#
+VS_wrapper:
+	@$(CNFDIR)\win32\wrapdsp.exe $(CNFDIR)\win32\wrap.dsp $(TARGET_DIR)
