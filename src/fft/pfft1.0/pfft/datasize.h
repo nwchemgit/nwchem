1f808c9baceace6112af2fb860cd6c753d494a6b
Edoardo Apra <edoardo.apra@gmail.com>
2000-08-29 23:18:46 +0000

    added EXT_INT (flag common to all 64-bit platforms in $NWCHEM_TOP/src/config/makefile.h)

--- src/fft/pfft1.0/pfft/datasize.h
+++ src/fft/pfft1.0/pfft/datasize.h
@@ -1,5 +1,5 @@
 *
-* $Id: datasize.h,v 1.6 2000-07-10 16:59:15 d3g270 Exp $
+* $Id: datasize.h,v 1.7 2000-08-29 23:18:46 edo Exp $
 *
 *======================================================================
 *
@@ -37,7 +37,7 @@
 
       INTEGER             NBYTEI, NBYTED
 
-#if defined(KSR) || defined(SGI) || defined(CRAY_T3E) || defined(DECOSF)
+#if defined(KSR) || defined(SGI) || defined(CRAY_T3E) || defined(DECOSF)|| defined(EXT_INT)
       PARAMETER(          NBYTED = 8,
      $                    NBYTEI = 8 )
 #else
