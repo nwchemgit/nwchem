9d1026714e1c3ae62684c536968fb1d0fd62dc58
Tjerk Straatsma <straatsmatp@ornl.gov>
2000-08-29 15:55:39 +0000

    task_shell on CRAY

--- src/util/util_system.c
+++ src/util/util_system.c
@@ -1,5 +1,5 @@
 /*
- $Id: util_system.c,v 1.6 2000-01-17 21:50:22 windus Exp $
+ $Id: util_system.c,v 1.7 2000-08-29 15:55:39 d3j191 Exp $
  */
 
 #include <stdio.h>
@@ -48,9 +48,7 @@ Integer util_system_(const char *input, int lin)
     if (!fortchar_to_string(input, lin, in, sizeof(in)))
 	ga_error("util_system: fortchar_to_string failed for in",0);
 
-#if defined(CRAY)
-    return 1;			/* Does not work on the Cray? */
-#elif defined(LINUX)
+#if defined(LINUX)
 	Siginit = signal(SIGCHLD,SIG_IGN);
 	i = system(in);
 	Siginit = signal(SIGCHLD,Siginit);
