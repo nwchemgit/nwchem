203ac711b2f8b3204f448ae7ef822e5219b8ae4f
Robert Harrison <robert.harrison@stonybrook.edu>
2000-10-12 22:07:17 +0000

    Fortran wrapper for memcpy

---
+++ src/util/util_memcpy.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "typesf2c.h"
+
+/* A fortran interface to memcpy since on at least one machine
+   (LINUX) dcopy() breaks when copying -ve integers even if
+   everything is correctly aligned. So you cannot copy common
+   blocks or packed buffers, without doing the right thing  */
+
+#if defined(CRAY) || defined(WIN32)
+#define util_memcpy_ UTIL_MEMCPY
+#endif
+
+void FATR util_memcpy_(void *dest, void *src, Integer *n) 
+{
+  memcpy(dest, src, (size_t) *n);
+}
+
