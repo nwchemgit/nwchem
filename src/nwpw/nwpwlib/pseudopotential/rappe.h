4dd355703b67beca1d56bcfe7fa43168c37ebaf0
Eric Bylaska <Eric.Bylaska@pnnl.gov>
2002-01-21 01:22:04 +0000

    rappe pseudopotentials ... EJB

---
+++ src/nwpw/nwpwlib/pseudopotential/rappe.h
@@ -0,0 +1,14 @@
+/*
+ $Id: rappe.h,v 1.1 2002-01-21 01:22:04 bylaska Exp $
+*/
+#ifndef _RAPPE_H_
+#define _RAPPE_H_
+/* rappe.h -
+   Author - Eric Bylaska
+
+*/
+
+void	Suggested_Param_Rappe();
+void	solve_Rappe();
+
+#endif
