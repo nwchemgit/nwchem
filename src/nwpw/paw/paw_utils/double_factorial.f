f21466ba69b58d1ffb1de6e786245a5c0a1faa9a
Marat Valiev <marat.valiev@pnnl.gov>
2002-08-07 23:50:07 +0000

    ...

---
+++ src/nwpw/paw/paw_utils/double_factorial.f
@@ -0,0 +1,14 @@
+      double precision function double_factorial(n)
+
+      integer n
+      integer, parameter :: n11(-1:16)=(/1,1,1,2,3,8,15,48,105,384,945,
+     >                                   3840,10395,46080,135135,645120,
+     >                                   2027025,10321920/)
+                                         
+      if( n.ge.(-1) .and. n.le.16) then
+        double_factorial = n11(n)
+      else
+        call errquit("too big parameter in double_factorial",1)
+      end if
+
+	end
