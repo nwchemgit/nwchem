ced0572eb6120afcd1270b3bdce469289d0eb30d
Marat Valiev <marat.valiev@pnnl.gov>
2003-01-24 19:35:44 +0000

    ..

--- src/nwpw/paw/makefile.h
+++ src/nwpw/paw/makefile.h
@@ -1,8 +1,8 @@
-#FC=ifc -w -132
-#.SUFFIXES:
-#
-#.SUFFIXES: .o .f90
-#
-#.f90.o :
-#	$(FC) $(F90FLAGS) $(LIB_INCLUDES)  -I$(TOPDIR)/src/include -c $<
+FC=ifc -w -132
+.SUFFIXES:
+
+.SUFFIXES: .o .f90
+
+.f90.o :
+	$(FC) $(F90FLAGS) $(LIB_INCLUDES)  -I$(TOPDIR)/src/include -c $<
 
