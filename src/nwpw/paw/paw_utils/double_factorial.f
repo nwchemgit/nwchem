66f322baef4aef778125224b00290a218ee1bafa
Eric Bylaska <Eric.Bylaska@pnnl.gov>
2003-01-31 02:01:13 +0000

    F90-->F77 conversion

--- src/nwpw/paw/paw_utils/double_factorial.f
+++ src/nwpw/paw/paw_utils/double_factorial.f
@@ -1,9 +1,10 @@
       double precision function double_factorial(n)
 
       integer n
-      integer, parameter :: n11(-1:16)=(/1,1,1,2,3,8,15,48,105,384,945,
-     >                                   3840,10395,46080,135135,645120,
-     >                                   2027025,10321920/)
+      integer n11(-1:16)
+      data (n11(i), i=-1,16) /1,1,1,2,3,8,15,48,105,384,945,
+     >                        3840,10395,46080,135135,645120,
+     >                        2027025,10321920/
                                          
       if( n.ge.(-1) .and. n.le.16) then
         double_factorial = n11(n)
