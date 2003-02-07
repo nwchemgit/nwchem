dc8bc7cbba2a326fc310b9892104d84f52e8b59b
Edoardo Apra <edoardo.apra@gmail.com>
2003-02-07 02:45:01 +0000

    MakeFIle back to 2 vs ago added tests to doqm.bat

--- QA/doqm.bat
+++ QA/doqm.bat
@@ -1,30 +1,39 @@
-csh runtests.unix auh2o autosym dft_he2+ h2mp2 h2o hess_h2o prop_h2o pyqa
-csh runtests.unix rimp2_ne sadsmall scf_feco5 small_intchk tagcheck testtab
-csh runtests.unix h2o_dk u_sodft cosmo_h2o ch5n_nbo h2s_finite startag
-csh runtests.unix neda_nbo
+tcsh runtests.unix auh2o autosym dft_he2+ h2mp2 h2o hess_h2o prop_h2o pyqa
+tcsh runtests.unix rimp2_ne sadsmall scf_feco5 small_intchk tagcheck testtab
+tcsh runtests.unix h2o_dk u_sodft cosmo_h2o ch5n_nbo h2s_finite startag
+tcsh runtests.unix cosmo_trichloroethene esp dplot
+tcsh runtests.unix neda_nbo
 
 REM ---   small tests that should fail!
 echo " "
 echo "The oh2 test is testing the perl parsing script and SHOULD fail"
-csh runtests.unix oh2
+tcsh runtests.unix oh2
 REM ---   medium tests
-csh runtests.unix dft_feco5 
-csh runtests.unix dft_ozone 
-csh runtests.unix dft_siosi3
-csh runtests.unix dielsalder
-csh runtests.unix grad_ozone
-csh runtests.unix hess_c6h6
-csh runtests.unix intchk
-csh runtests.unix sadbig
-csh runtests.unix br2_dk
-csh runtests.unix uo2_sodft
-csh runtests.unix uo2_sodft_grad
-csh runtests.unix si2cl6_gc
+tcsh runtests.unix dft_feco5 
+tcsh runtests.unix dft_ozone 
+tcsh runtests.unix dft_siosi3
+tcsh runtests.unix dft_x
+tcsh runtests.unix dielsalder
+tcsh runtests.unix grad_ozone
+tcsh runtests.unix hess_c6h6
+tcsh runtests.unix intchk
+tcsh runtests.unix sadbig
+tcsh runtests.unix br2_dk
+tcsh runtests.unix uo2_sodft
+tcsh runtests.unix uo2_sodft_grad
+tcsh runtests.unix si2cl6_gc
+tcsh runtests.unix pspw
+tcsh runtests.unix pspw_SiC
+tcsh runtests.unix pspw_md
+tcsh runtests.unix pspw_polarizability
+tcsh runtests.unix tddft_h2o
+tcsh runtests.unix tddft_n2+
+tcsh runtests.unix tce_h2o
 REM ---   long  may not run on workstations
-csh runtests.unix aump2 
-csh runtests.unix uoverlap
-csh runtests.unix grad_nh3_trimer 
-csh runtests.unix hess_nh3
-csh runtests.unix hess_nh3_dimer
-csh runtests.unix mp2_si2h6
-csh runtests.unix pbo_nesc1e
+tcsh runtests.unix aump2 
+tcsh runtests.unix uoverlap
+tcsh runtests.unix grad_nh3_trimer 
+tcsh runtests.unix hess_nh3
+tcsh runtests.unix hess_nh3_dimer
+tcsh runtests.unix mp2_si2h6
+tcsh runtests.unix pbo_nesc1e
