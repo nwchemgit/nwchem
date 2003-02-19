84774705f1421ac2d10d95be816a9008db30ee0c
So Hirata <sohirata@illinois.edu>
2003-02-19 18:41:18 +0000

    Several large files generated by TCE have been split into smaller ones with each containing just one subroutine, with a python code splitfile.py.

---
+++ src/tce/splitfiles.py
@@ -0,0 +1,30 @@
+# Usage: python splitfiles.py < inputfile.F
+# (c) All rights reserved by Battelle & Pacific Northwest Nat'l Lab (2002)
+ 
+import string
+import copy
+import sys
+
+source = sys.stdin.readlines()
+
+if (not source):
+   print "Usage: python splitfiles.py < inputfile.F"
+
+nfiles = 0
+for line in source:
+   if (string.find(line,"SUBROUTINE") != -1):
+      if (nfiles):
+         file = open(filename+".F","w")
+         for newline in filecontent:
+            file.write(newline)
+      filename = string.split(line[string.find(line,"SUBROUTINE")+11:],"(")[0]
+      print filename+".o\\"
+      nfiles = nfiles + 1
+      filecontent = [line]
+   else:
+      filecontent.append(line)
+# don't forget to dump the last subroutine
+file = open(filename+".F","w")
+for newline in filecontent:
+   file.write(newline)
+print "Number of files generated:",nfiles
