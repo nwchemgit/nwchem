6afb37f6696d820e2276886ac9ac78e1ff5713b2
Bert de Jong <wadejong@lbl.gov>
2003-04-11 00:29:05 +0000

    Bug fixes to make selci work on a 64 bit platform.

--- src/selci/gettim.f
+++ src/selci/gettim.f
@@ -1,12 +1,12 @@
       subroutine selci_gettim(cpud,elapsd)
 *
-* $Id: gettim.f,v 1.4 1997-10-31 23:42:03 d3e129 Exp $
+* $Id: gettim.f,v 1.5 2003-04-11 00:29:05 bert Exp $
 *
       implicit double precision (a-h,o-z)
       real*4 elast,timediff
       external util_cpusec
-      integer last,ienter
-      save ienter,last,elast
+      integer last,ienter,now
+      save ienter,last,elast,now
       data ienter/0/
       if(ienter.eq.0) then
         ienter=1
