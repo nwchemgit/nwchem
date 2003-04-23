cb3c14481d92d02a886843aa8771e8470c9a8c63
Edoardo Apra <edoardo.apra@gmail.com>
2003-04-23 01:37:07 +0000

    ia64: added line to cause sigfpe for each fpswa

--- src/util/linux_setfpucw.c
+++ src/util/linux_setfpucw.c
@@ -1,5 +1,5 @@
 /*
- $Id: linux_setfpucw.c,v 1.4 2003-04-22 01:55:50 edo Exp $
+ $Id: linux_setfpucw.c,v 1.5 2003-04-23 01:37:07 edo Exp $
  */
 
 #ifdef CYGNUS
@@ -10,16 +10,18 @@ void linux_trapfpe_(void) { }
 #include <fenv.h>
 #include <stdio.h>
 #include <linux/prctl.h>
-# define FE_NONIEEE_ENV ((__const fenv_t *) 0xc009a04d0270037fUL)               
-/* http://devresource.hp.com/devresource/Docs/TechTips/ia64linuxTips.html#ia64lptip2 */
+//#include </usr/src/linux-2.4.19-hp2_pnnl6_Lv15irqstkM/include/linux/prctl.h>
+/* grabbed from ftp://linux.hpl.hp.com/pub/linux-ia64/prctl-1.3.tar.gz  */
 void linux_trapfpe_(void) { 
 int retval;
-//    fesetenv (FE_NONIEEE_ENV);
-  //fedisableexcept (FE_ALL_EXCEPT); 
-//__asm__ __volatile__ (";; mov.m ar.fpsr=%0;;" :: "r"(st));
 /* this causes a Sigbus for each unaligned access */
     retval = prctl(PR_SET_UNALIGN,  PR_UNALIGN_SIGBUS);
 	if (retval == -1) fprintf(stderr, "Failed to sigbus align \n");
+#ifdef PR_SET_FPEMU
+/* this causes a SigFPE for each fpswa */
+    retval = prctl(PR_SET_FPEMU,  PR_FPEMU_SIGFPE);
+	if (retval == -1) fprintf(stderr, "Failed to sigfpe fpswa \n");
+#endif
 }
 
 #else
