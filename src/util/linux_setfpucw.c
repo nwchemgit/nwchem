4b2b80044c343cf3f556532227d1e8ea7dece8b0
Edoardo Apra <edoardo.apra@gmail.com>
2003-07-01 18:18:18 +0000

    LINUX FPU control cleaned-up following fenv.h header file added silencing of fp assist fault under ia64

--- src/util/linux_setfpucw.c
+++ src/util/linux_setfpucw.c
@@ -1,47 +1,37 @@
 /*
- $Id: linux_setfpucw.c,v 1.5 2003-04-23 01:37:07 edo Exp $
+ $Id: linux_setfpucw.c,v 1.6 2003-07-01 18:18:18 edo Exp $
  */
-
-#ifdef CYGNUS
-
-/* Make this a dummy routine under CYGNUS since fpu_control.h doesn't exist */
-void linux_trapfpe_(void) { }
-#elif defined(LINUXIA64)
+#ifdef __CYGWIN__
+#include <mingw/fenv.h>
+#else
 #include <fenv.h>
+#endif
+#define FTZ 0x8000
+#define FP_QUIET
+
+void linux_trapfpe_(void) { 
+#ifdef LINUXIA64
 #include <stdio.h>
 #include <linux/prctl.h>
 //#include </usr/src/linux-2.4.19-hp2_pnnl6_Lv15irqstkM/include/linux/prctl.h>
 /* grabbed from ftp://linux.hpl.hp.com/pub/linux-ia64/prctl-1.3.tar.gz  */
-void linux_trapfpe_(void) { 
 int retval;
 /* this causes a Sigbus for each unaligned access */
     retval = prctl(PR_SET_UNALIGN,  PR_UNALIGN_SIGBUS);
 	if (retval == -1) fprintf(stderr, "Failed to sigbus align \n");
 #ifdef PR_SET_FPEMU
+#ifdef FP_QUIET
+/* this causes no print for each fpswa */
+    retval = prctl(PR_SET_FPEMU,  PR_FPEMU_NOPRINT);
+#else
 /* this causes a SigFPE for each fpswa */
     retval = prctl(PR_SET_FPEMU,  PR_FPEMU_SIGFPE);
-	if (retval == -1) fprintf(stderr, "Failed to sigfpe fpswa \n");
 #endif
-}
+        if (retval == -1) fprintf(stderr, "Failed to sigfpe fpswa \n");
+#endif
 
 #else
-
-/* Regular LINUX case*/
-#include <fpu_control.h>
-#include <math.h>
-#include <stdio.h>
-/* static void __attribute__ ((constructor)) trapfpe (void)*/
-  void  linux_trapfpe_ (void)
- {
-   /* fpu_control_t cw = 
-    (_FPU_DEFAULT & ~_FPU_EXTENDED) | _FPU_DOUBLE; _FPU_SETCW(cw);*/
- fpu_control_t cw = 
-#ifdef IFCLINUX
- _FPU_DEFAULT & ~(_FPU_MASK_ZM |_FPU_MASK_OM | _FPU_RC_ZERO); _FPU_SETCW(cw);
-#else
- _FPU_DEFAULT & ~(_FPU_MASK_ZM |_FPU_MASK_OM | _FPU_RC_ZERO); _FPU_SETCW(cw);
- // _FPU_DEFAULT & ~(_FPU_MASK_IM |_FPU_MASK_ZM |_FPU_MASK_OM | _FPU_RC_ZERO); _FPU_SETCW(cw);
+            feenableexcept(FE_OVERFLOW | FE_DIVBYZERO);
 #endif
  }
 
-#endif
