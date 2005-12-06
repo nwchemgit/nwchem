86a49d63ea8d68af9a2d878ad20f48d9f2ba298d
Edoardo Apra <edoardo.apra@gmail.com>
2005-12-06 16:59:50 +0000

    added wrappers for cholesky & inverse

---
+++ src/64to32blas/ypotri.f
@@ -0,0 +1,19 @@
+      SUBROUTINE YPOTRI( UPLO, N, A, LDA, INFO )
+c
+* $Id: ypotri.f,v 1.1 2005-12-06 16:59:50 edo Exp $
+c
+      CHARACTER          UPLO
+      INTEGER            INFO, LDA, N
+*     ..
+*     .. Array Arguments ..
+      DOUBLE PRECISION   A( LDA, * )
+c
+      integer*4 n4,lda4,info4
+c
+      n4=n
+      lda4=lda
+c
+      call DPOTRI( UPLO, N4, A, LDA4, INFO4 )
+      info=info4
+      return
+      end
