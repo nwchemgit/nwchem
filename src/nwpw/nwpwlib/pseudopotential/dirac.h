cb3a6ad8cc475cd4a7dddb2fd65a993eb661e10d
Pat Nichols <pnichols@ucar.edu>
2007-04-04 21:48:18 +0000

    pjn    This Dirac Solver header file.

---
+++ src/nwpw/nwpwlib/pseudopotential/dirac.h
@@ -0,0 +1,8 @@
+#ifndef _DIRAC_H_
+#define _DIRAC_H_
+/* dirac.c */
+extern void R_Dirac (int n, int l, int s2, double Z, const double *v,
+		     int *mch, double *Eig, double *u, double *uprime);
+extern void R_Dirac_Fixed_E (int n, int l, int s2, double Z, const double *v,
+			     int match, double E, double *u, double *uprime);
+#endif
