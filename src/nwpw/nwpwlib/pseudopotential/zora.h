d35b6e3edb3a6dd71d5e784f8b7b84d9b74ff15b
Pat Nichols <pnichols@ucar.edu>
2008-02-21 18:14:23 +0000

    pjn   adding zora solvers...

---
+++ src/nwpw/nwpwlib/pseudopotential/zora.h
@@ -0,0 +1,8 @@
+#ifndef _ZORA_H_
+#define _ZORA_H_
+/* dirac.c */
+extern void R_ZORA (int n, int l, int s2, double Z, const double *v,
+                         int *mch, double *Eig, double *u, double *uprime);
+extern void R_ZORA_Fixed_E (int n, int l, int s2, double Z, const double *v,
+                                 int match, double E, double *u, double *uprime);
+#endif
