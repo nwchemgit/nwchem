2ec5f9789d6acaaf93360bcb4d841c6398e74fa6
Niri Govind <niri.govind@pnl.gov>
2009-09-23 20:41:46 +0000

    changes corresponding to previous checkin

--- src/config/nwchem_config_win32.h
+++ src/config/nwchem_config_win32.h
@@ -9,7 +9,7 @@ PYTHON_SUBDIR = python
 NW_CORE_SUBDIRS = basis geom inp input \
 	pstat rtdb task symmetry util peigs lapack blas
 
-NW_MODULE_SUBDIRS = NWints atomscf ddscf gradients moints nwdft rimp2 stepper driver dftgrad cphf ccsd vib mcscf prepar  esp hessian selci dplot mp2_grad property nwpw fft analyz nwmd cafe space drdy qmmm qmd  etrans tce tddft cons qhop $(PYTHON_SUBDIR)
+NW_MODULE_SUBDIRS = NWints atomscf ddscf gradients moints nwdft rimp2 stepper driver dftgrad cphf ccsd vib mcscf prepar  esp hessian selci dplot mp2_grad property nwpw fft analyz nwmd cafe space drdy qmmm qmd  etrans tce cons qhop $(PYTHON_SUBDIR)
 #NW_MODULE_SUBDIRS = NWints atomscf ddscf gradients moints nwdft rimp2 stepper driver dftgrad cphf ccsd vib mcscf prepar  esp hessian selci dplot mp2_grad property nwpw fft analyz nwmd cafe space drdy qmmm qmd etrans $(PYTHON_SUBDIR)
 
 !IFNDEF LINK_F90
@@ -28,7 +28,7 @@ UTIL_LIBS = nwcutil.lib $(PYTHON_NWLIB) pario.lib global.lib ma.lib peigs.lib \
 tcgmsg-mpi.lib armci.lib $(CXMLLIB) lapack.lib blas.lib $(NWCHEM_EXTRA_LIBS) wsock32.lib \
 $(LINK_F90)
 
-LIBS = nwctask.lib ccsd.lib mcscf.lib selci.lib mp2.lib moints.lib stepper.lib driver.lib dftgrad.lib nwdft.lib gradients.lib cphf.lib esp.lib ddscf.lib guess.lib hessian.lib vib.lib nwcutil.lib rimp2.lib tce.lib tddft.lib cons.lib property.lib nwints.lib prepar.lib nwmd.lib paw.lib pspw.lib nwpw.lib band.lib nwpwlib.lib cafe.lib space.lib analyze.lib qhop.lib pfft.lib dplot.lib drdy.lib qmmm.lib qmd.lib  etrans.lib  $(UTIL_LIBS)
+LIBS = nwctask.lib ccsd.lib mcscf.lib selci.lib mp2.lib moints.lib stepper.lib driver.lib dftgrad.lib nwdft.lib gradients.lib cphf.lib esp.lib ddscf.lib guess.lib hessian.lib vib.lib nwcutil.lib rimp2.lib tce.lib cons.lib property.lib nwints.lib prepar.lib nwmd.lib paw.lib pspw.lib nwpw.lib band.lib nwpwlib.lib cafe.lib space.lib analyze.lib qhop.lib pfft.lib dplot.lib drdy.lib qmmm.lib qmd.lib  etrans.lib  $(UTIL_LIBS)
 EXCLUDED_SUBDIRS = nwargos
 #EXCLUDED_SUBDIRS = develop nwargos plane_wave oimp2 rimp2_grad python vscf uccsdt 
 #CONFIG_LIBS =
