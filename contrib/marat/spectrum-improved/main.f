98f91d0c66c9837577cd323c7ff3c750d01db803
Marat Valiev <marat.valiev@pnnl.gov>
2010-07-13 22:39:35 +0000

    enabling comments and better output file

--- contrib/marat/spectrum-improved/main.f
+++ contrib/marat/spectrum-improved/main.f
@@ -19,31 +19,33 @@ c
        end if 
        n = 0
 10     continue
+       buffer = " "
        read(10,'(A180)',ERR=30,END=30) buffer
-       n = n+1
-       if(n.gt.nmax) then
-         write(6,*) "increase nmax"
-         return
+       if(buffer.ne." ".and.buffer(1:1).ne."#") then
+         n = n+1
+         if(n.gt.nmax) then
+           write(6,*) "increase nmax"
+           return
+         end if
+         read(buffer,*) lmax(n),f(n),sigma(n)
        end if
-       read(buffer,*) lmax(n),f(n),sigma(n)
        goto 10
 30     close(10)
        l1=lmax(1)
        l2=lmax(1)
        do i=1,n
-         write(*,*) i,lmax(i),f(i),sigma(i)
+         write(*,1000) lmax(i),f(i),sigma(i)
          call integrate(lmax(i),sigma(i),sum(i))
-         write(*,*) "integral equals to",sum(i)
          e(i)=f(i)/(0.043*sum(i))
-         write(*,*) "e=",e(i)
+         write(*,1001) e(i)
          tmp = lmax(i)-5*sigma(i)
          if(tmp.lt.l1) l1=tmp
          tmp = lmax(i)+5*sigma(i)
          if(tmp.gt.l2) l2=tmp
        end do
-
-       write(*,*) "proposed boundaries",l1,l2
-       call plot(n,l1,l2,lmax,sigma,e)
+       call plot(n,l1,l2,f,lmax,sigma,e)
+1000   FORMAT("Root :", 3G12.6)
+1001   FORMAT("Max. extinction :", G12.6)
        end
 c
        subroutine integrate(lmax,sigma,sum)
@@ -58,7 +60,6 @@ c
        dx = 0.1d0
        dl=5.0d0*sigma
        n = int(2*dl/dx)
-       write(*,*) "number of points",n
        x = lmax-dl
        f = exp(-((lmax-x)/sigma)**2)/x**2
        sum = 0.5d0*f
@@ -74,53 +75,39 @@ c
        return
        
        end
-
-       subroutine plot(n,l1,l2,lmax,sigma,e)
+c
+       subroutine plot(n,l1,l2,f,lmax,sigma,e)
        implicit none
        integer n
        double precision l1,l2
-       double precision lmax(n),sigma(n),e(n)
+       double precision f(n),lmax(n),sigma(n),e(n)
 c       
        integer i
        integer k,kmax
-       double precision f,tmp
+       double precision s,tmp
        double precision x,dx
        double precision dl
 
+       open(11,file="spectrum.out",
+     $            form='formatted',status='unknown')
+c
+       do i=1,n
+         write(11,1000) lmax(i),f(i),sigma(i),e(i)
+       end do
+1000   FORMAT("# ",4G12.6)
+c       
        dx = 2.0d0
        kmax = int((l2-l1)/dx)
-       write(*,*) "number of points",kmax
        x = l1
        do k=1,kmax-1
          x=x+dx
-         f = 0.0d0
+         s = 0.0d0
          do i=1,n
-           f =f+e(i)*exp(-((lmax(i)-x)/sigma(i))**2)
+           s =s+e(i)*exp(-((lmax(i)-x)/sigma(i))**2)
          end do
-         write(33,*) x,f
+         write(11,*) x,s
        end do
-       return
-       
-       end
-       subroutine plot1(lmax,sigma,e)
-       implicit none
-       double precision lmax,sigma,e
-c       
-       integer i,n
-       double precision f,tmp
-       double precision x,dx
-       double precision dl
-
-       dx = 0.1d0
-       dl=5.0d0*sigma
-       n = int(2*dl/dx)
-       write(*,*) "number of points",n
-       x = lmax-dl
-       do i=1,n-1
-         x=x+dx
-         f = e*exp(-((lmax-x)/sigma)**2)
-         write(33,*) x,f
-       end do
+       close(11)
        return
        
        end
