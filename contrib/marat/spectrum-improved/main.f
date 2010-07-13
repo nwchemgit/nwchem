15301d8b55773a478c21cc9039ca16484669f4b8
Marat Valiev <marat.valiev@pnnl.gov>
2010-07-13 22:16:12 +0000

    changes to accomodate multiple bands

--- contrib/marat/spectrum-improved/main.f
+++ contrib/marat/spectrum-improved/main.f
@@ -1,26 +1,49 @@
        program main
        implicit none
-       double precision lmax,sigma,sum
-       double precision f,e
        logical ofile
+       integer nmax
+       parameter(nmax=20)
+       double precision lmax(nmax),sigma(nmax),sum(nmax)
+       double precision f(nmax),e(nmax)
+       character*(180) buffer
+       integer i,n
+       double precision l1,l2,tmp
 c
        inquire(file="spectrum.dat",exist=ofile)
        if(ofile) then
           open(10,file="spectrum.dat",
      $            form='formatted',status='old')
-c         lattice vectors a,b,c
-          read(10,*) lmax
-          read(10,*) sigma
-          read(10,*) f
-          close(10)
        else
-       write(6,*) "Please provide spectrum.dat file in the format"
+         write(6,*) "Please provide spectrum.dat file in the format"
+         return
        end if 
-       call integrate(lmax,sigma,sum)
-       write(*,*) "integral equals to",sum
-       e=f/(0.043*sum)
-       write(*,*) "e=",e
-       call plot(lmax,sigma,e)
+       n = 0
+10     continue
+       read(10,'(A180)',ERR=30,END=30) buffer
+       n = n+1
+       if(n.gt.nmax) then
+         write(6,*) "increase nmax"
+         return
+       end if
+       read(buffer,*) lmax(n),f(n),sigma(n)
+       goto 10
+30     close(10)
+       l1=lmax(1)
+       l2=lmax(1)
+       do i=1,n
+         write(*,*) i,lmax(i),f(i),sigma(i)
+         call integrate(lmax(i),sigma(i),sum(i))
+         write(*,*) "integral equals to",sum(i)
+         e(i)=f(i)/(0.043*sum(i))
+         write(*,*) "e=",e(i)
+         tmp = lmax(i)-5*sigma(i)
+         if(tmp.lt.l1) l1=tmp
+         tmp = lmax(i)+5*sigma(i)
+         if(tmp.gt.l2) l2=tmp
+       end do
+
+       write(*,*) "proposed boundaries",l1,l2
+       call plot(n,l1,l2,lmax,sigma,e)
        end
 c
        subroutine integrate(lmax,sigma,sum)
@@ -52,7 +75,34 @@ c
        
        end
 
-       subroutine plot(lmax,sigma,e)
+       subroutine plot(n,l1,l2,lmax,sigma,e)
+       implicit none
+       integer n
+       double precision l1,l2
+       double precision lmax(n),sigma(n),e(n)
+c       
+       integer i
+       integer k,kmax
+       double precision f,tmp
+       double precision x,dx
+       double precision dl
+
+       dx = 2.0d0
+       kmax = int((l2-l1)/dx)
+       write(*,*) "number of points",kmax
+       x = l1
+       do k=1,kmax-1
+         x=x+dx
+         f = 0.0d0
+         do i=1,n
+           f =f+e(i)*exp(-((lmax(i)-x)/sigma(i))**2)
+         end do
+         write(33,*) x,f
+       end do
+       return
+       
+       end
+       subroutine plot1(lmax,sigma,e)
        implicit none
        double precision lmax,sigma,e
 c       
