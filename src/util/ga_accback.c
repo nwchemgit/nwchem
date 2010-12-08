cfe10ca087ba065cbd6001478d151fd181475620
Huub Van Dam <hvandam@bnl.gov>
2010-12-08 07:19:25 +0000

    HvD: Bruce Palmer requested that ga_access_callback_release be made independent of nga_access_ptr as he has eliminated that routine from the global arrays.
    Doing this requires the introduction of the macro's COPYINDEX_F2C to translate
    the global array indeces between Fortran and C conventions. These macro's 
    behave differently depending on whether the preprocessor flag -DUSE_FAPI is
    presented or not. This can cause problems as this flag is likely to be managed
    correctly within the global array library itself, but will be unknown to the
    nwchem make procedures. So as long USE_FAPI is never used we will be fine,
    otherwise almost every QA example will fail. At least that makes it clear that
    there is an issue.

--- src/util/ga_accback.c
+++ src/util/ga_accback.c
@@ -11,6 +11,32 @@
 #ifdef WIN32
 #include "typesf2c.h"
 #endif
+
+#ifdef USE_FAPI
+#  define COPYC2F(carr, farr, n){\
+   int i; for(i=0; i< (n); i++)(farr)[i]=(Integer)(carr)[i];} 
+#  define COPYF2C(farr, carr, n){\
+   int i; for(i=0; i< (n); i++)(carr)[i]=(int)(farr)[i];} 
+#  define COPYF2C_64(farr, carr, n){\
+   int i; for(i=0; i< (n); i++)(carr)[i]=(int64_t)(farr)[i];} 
+#  define COPYINDEX_F2C     COPYF2C
+#  define COPYINDEX_F2C_64  COPYF2C_64
+#  define COPYINDEX_C2F     COPYC2F
+#else
+#  define COPYC2F(carr, farr, n){\
+   int i; for(i=0; i< (n); i++)(farr)[n-i-1]=(Integer)(carr)[i];} 
+#  define COPYF2C(farr, carr, n){\
+   int i; for(i=0; i< (n); i++)(carr)[n-i-1]=(int)(farr)[i];} 
+#  define COPYF2C_64(farr, carr, n){\
+   int i; for(i=0; i< (n); i++)(carr)[n-i-1]=(int64_t)(farr)[i];} 
+#  define COPYINDEX_C2F(carr, farr, n){\
+   int i; for(i=0; i< (n); i++)(farr)[n-i-1]=(Integer)(carr)[i]+1;}
+#  define COPYINDEX_F2C(farr, carr, n){\
+   int i; for(i=0; i< (n); i++)(carr)[n-i-1]=(int)(farr)[i] -1;}
+#  define COPYINDEX_F2C_64(farr, carr, n){\
+   int i; for(i=0; i< (n); i++)(carr)[n-i-1]=(int64_t)(farr)[i] -1;}
+#define BASE_0
+#endif
 			     
 /*\ PROVIDE ACCESS TO A PATCH OF A GLOBAL ARRAY WITH CALLBACK AND RELEASE
 \*/
@@ -23,27 +49,29 @@ void FATR ga_access_callback_release_(g_a, ilo, ihi, jlo, jhi,
 			 void *, void *, void *, void *, void *, void *, void *);
      void *arg1, *arg2, *arg3, *arg4, *arg5, *arg6, *arg7;
 {
-  Integer lo[2], hi[2],ndim=GA_Ndim(*g_a), ld[2], result;
-  int alo[2], ahi[2];
+  Integer ndim=GA_Ndim(*g_a), lo[2], hi[2], ld[2],
+          result; /* Fortran variables */
+  int alo[2], ahi[2], ald[2], ag_a; /* variables for the C-interfaces */
   void *ptr;
 
   if(ndim != 2) 
     GA_Error("ga_access: 2D API cannot be used for array dimension",ndim);
 
+  ag_a=*g_a;
   lo[0]=*ilo;
   lo[1]=*jlo;
   hi[0]=*ihi;
   hi[1]=*jhi;
-  nga_access_ptr(g_a,lo,hi,&ptr,ld);
+  COPYINDEX_F2C(lo,alo,ndim);
+  COPYINDEX_F2C(hi,ahi,ndim);
+  NGA_Access(ag_a,alo,ahi,&ptr,ald); /* This routine sets ald[] */
+  ld[0]=ald[0];
+  ld[1]=ald[1];
   result = callback(g_a,ilo,ihi,jlo,jhi,ptr,&ld[0],arg1,arg2,arg3,arg4,arg5,arg6,arg7);
-  alo[0]=lo[0];
-  alo[1]=lo[1];
-  ahi[0]=hi[0];
-  ahi[1]=hi[1];
   if (result) {
-    NGA_Release_update(*g_a, alo, ahi);
+    NGA_Release_update(ag_a, alo, ahi);
   } else {
-    NGA_Release(*g_a, alo, ahi);
+    NGA_Release(ag_a, alo, ahi);
   }
 } 
 
