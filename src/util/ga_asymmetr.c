3ca80173019642917671a5ce9e02ece3a6fce82b
Huub Van Dam <hvandam@bnl.gov>
2010-12-11 04:55:49 +0000

    HvD: Another routine in which ga_access_ptr was used. As ga_access_ptr is now deprecated in the GAs this needed to go. As this routine is implemented in C I have adapted in such a way as to only use the C interface of the GAs, rather than the eclectic mix of routines from the C and Fortran interfaces. Obviously this led to some extra changes as the order of the array dimensions in C and Fortran are different.

--- src/util/ga_asymmetr.c
+++ src/util/ga_asymmetr.c
@@ -12,18 +12,18 @@
 #include "macdecls.h"
 
 void FATR
-gai_subtr(Integer *lo, Integer *hi, void *a, void *b, DoublePrecision alpha,
-        Integer type, Integer nelem, Integer ndim) {
+gai_subtr(int *lo, int *hi, void *a, void *b, DoublePrecision alpha,
+          int type, Integer nelem, int ndim) {
 
   Integer i, j, m=0;
   Integer nrow, ncol, indexA=0, indexB=0;
   DoublePrecision *A = (DoublePrecision *)a, *B = (DoublePrecision*)b;
   Integer offset1=1, offset2=1;
 
-  nrow = hi[ndim-2] - lo[ndim-2] + 1;
-  ncol = hi[ndim-1] - lo[ndim-1] + 1;
+  nrow = hi[1] - lo[1] + 1;
+  ncol = hi[0] - lo[0] + 1;
 
-  for(i=0; i<ndim-2; i++) {
+  for(i=2; i<ndim; i++) {
     offset1 *= hi[i] - lo[i] + 1;
     offset2 *= hi[i] - lo[i] + 1;
   }
@@ -41,14 +41,16 @@ void FATR
 ga_antisymmetrize_(Integer *g_a) {
   
   DoublePrecision alpha = 0.5;
-  Integer i, me = GA_Nodeid();
+  int i, me = GA_Nodeid();
   extern void * FATR ga_malloc(Integer nelem, int type, char *name);
   extern void FATR ga_free(void *ptr);
-  void FATR gai_add(Integer *lo, Integer *hi, void *a, void *b, DoublePrecision alpha, Integer type, Integer nelem, Integer ndim);
+  void FATR gai_subtr(int *lo, int *hi, void *a, void *b, DoublePrecision alpha,
+                      int type, Integer nelem, int ndim);
 
-  Integer alo[GA_MAX_DIM], ahi[GA_MAX_DIM], lda[GA_MAX_DIM], nelem=1;
-  Integer blo[GA_MAX_DIM], bhi[GA_MAX_DIM], ldb[GA_MAX_DIM];
-  Integer ndim, dims[GA_MAX_DIM], type;
+  int alo[GA_MAX_DIM], ahi[GA_MAX_DIM], lda[GA_MAX_DIM];
+  int blo[GA_MAX_DIM], bhi[GA_MAX_DIM], ldb[GA_MAX_DIM];
+  int ndim, dims[GA_MAX_DIM], type;
+  Integer nelem=1;
   Logical have_data;
   void *a_ptr, *b_ptr;
 
@@ -56,41 +58,41 @@ ga_antisymmetrize_(Integer *g_a) {
 
 
   
-  nga_inquire_(g_a, &type, &ndim, dims);
+  NGA_Inquire((int)(*g_a), &type, &ndim, dims);
   
-  if (dims[ndim-1] != dims[ndim-2]) 
+  if (dims[0] != dims[1]) 
     GA_Error("ga_sym: can only sym square matrix", 0L);
   
   /* Find the local distribution */
-  nga_distribution_(g_a, &me, alo, ahi);
+  NGA_Distribution((int)(*g_a), me, alo, ahi);
  
  
-  have_data = ahi[0]>0;
-  for(i=1; i<ndim; i++) have_data = have_data && ahi[i]>0;
+  have_data = ahi[0]>=0;
+  for(i=1; i<ndim; i++) have_data = have_data && ahi[i]>=0;
   
   if(have_data) {
-    nga_access_ptr(g_a, alo, ahi, &a_ptr, lda); 
+    NGA_Access((int)(*g_a), alo, ahi, &a_ptr, lda); 
     
     for(i=0; i<ndim; i++) nelem *= ahi[i]-alo[i] +1;
-    b_ptr = (void *) ga_malloc(nelem, MT_F_DBL, "v");
+    b_ptr = (void *) ga_malloc(nelem, MT_C_DBL, "v");
     
-    for(i=0; i<ndim-2; i++) {bhi[i]=ahi[i]; blo[i]=alo[i]; }
+    for(i=2; i<ndim; i++) {bhi[i]=ahi[i]; blo[i]=alo[i]; }
     
     /* switch rows and cols */
-    blo[ndim-1]=alo[ndim-2];
-    bhi[ndim-1]=ahi[ndim-2];
-    blo[ndim-2]=alo[ndim-1];
-    bhi[ndim-2]=ahi[ndim-1];
+    blo[1]=alo[0];
+    bhi[1]=ahi[0];
+    blo[0]=alo[1];
+    bhi[0]=ahi[1];
 
     for (i=0; i < ndim-1; i++) 
-      ldb[i] = bhi[i] - blo[i] + 1; 
-    nga_get_(g_a, blo, bhi, b_ptr, ldb);
+      ldb[i] = bhi[i+1] - blo[i+1] + 1; 
+    NGA_Get((int)(*g_a), blo, bhi, b_ptr, ldb);
   }
   GA_Sync(); 
 
   if(have_data) {
-    gai_subtr(alo, ahi, a_ptr, b_ptr, alpha, type, nelem, ndim);
-    nga_release_update_(g_a, alo, ahi);
+    gai_subtr_new(alo, ahi, a_ptr, b_ptr, alpha, type, nelem, ndim);
+    NGA_Release_update((int)(*g_a), alo, ahi);
     ga_free(b_ptr);
   }
   GA_Sync();
