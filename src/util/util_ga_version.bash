a6d7e25a044f6ad8a651abac57adfe239482b395
Huub Van Dam <hvandam@bnl.gov>
2012-05-21 23:35:32 +0000

    HvD: The util_version.F file has a couple of problems: 1. It is so big that some compilers choke on it. 2. You need to run nwchem with a specific input to get the information out. 3. When we get the information we would have to reconstruct the matching    collection of 17,000 files which is impractical. However, using svnversion we can get the revision number of NWChem and GA. This is a single number per repository that takes only one line in the output to print. So with 2 lines in the output we know all there is to know, and we do not need a massive file to print out all revision numbers. If this works reliably we can phase the big old util_version stuff out.

---
+++ src/util/util_ga_version.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# This script works out the revision number of the GA source
+# code. It writes the resulting data in suboutine that can be used
+# to query this information. As NWChem can build with different 
+# versions of GA the script takes one command line argument to
+# figure out of which GA directory it has to establish the revision
+# number.
+#
+# We need 2 things for this operation to complete successfully:
+# 1. svnversion needs to be available on this machine
+# 2. the .svn directories need to be present in this source code
+# If both these requirements are satisfied we will always overwrite
+# the revision information with a current version.
+# If either of these requirements is not satisfied we still need to
+# make sure there is a valid version subroutine to ensure the code
+# will compile. If such a routine already exists we do nothing as 
+# we lack the tools to do better than whatever is in that routine
+# already. If such a routine does not exist we create one but
+# setting the version number to blank (this is the best we can do).
+#
+# First find out where this script actually lives so we can create
+# the appropriate Fortran file in the right location.
+#
+if [ -f "$0" ] ; then
+   # The first item on the command line is an actual file so the 
+   # script must have been specified including the path.
+   path="`dirname \"$0\"`"
+else
+   # The first item on the command line is not a file so script
+   # it must have been found in PATH.
+   path="`which \"$0\"`"
+   path="`dirname \"$path\"`"
+fi
+my_svnversion=`which svnversion`
+ga_dir="$1"
+cd "$path"
+if [ -f ${my_svnversion} ] ; then
+  # svnversion exists, but does .svn?
+  if [ -d "../tools/${ga_dir}/.svn" ] ; then
+    # .svn exists too
+    revision=`${my_svnversion} "../tools/${ga_dir}"`
+    echo "      subroutine util_ga_version(garev)" > util_ga_version.F
+    echo "      implicit none" >> util_ga_version.F
+    echo "      character*(*) garev" >> util_ga_version.F
+    echo "      garev=\"${revision}\"" >> util_ga_version.F
+    echo "      end" >> util_ga_version.F
+  else
+    if [ ! -f util_ga_version.F ] ; then
+      echo "      subroutine util_ga_version(garev)" > util_ga_version.F
+      echo "      implicit none" >> util_ga_version.F
+      echo "      character*(*) garev" >> util_ga_version.F
+      echo "      garev=\"N/A\"" >> util_ga_version.F
+      echo "      end" >> util_ga_version.F
+    fi
+  fi
+else
+  if [ ! -f util_ga_version.F ] ; then
+    echo "      subroutine util_ga_version(garev)" > util_ga_version.F
+    echo "      implicit none" >> util_ga_version.F
+    echo "      character*(*) garev" >> util_ga_version.F
+    echo "      garev=\"N/A\"" >> util_ga_version.F
+    echo "      end" >> util_ga_version.F
+  fi
+fi
---
+++ src/util/util_nwchem_version.bash
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# This script works out the revision number of the NWChem source
+# code. It writes the resulting data in suboutine that can be used
+# to query this information.
+#
+# We need 2 things for this operation to complete successfully:
+# 1. svnversion needs to be available on this machine
+# 2. the .svn directories need to be present in this source code
+# If both these requirements are satisfied we will always overwrite
+# the revision information with a current version.
+# If either of these requirements is not satisfied we still need to
+# make sure there is a valid version subroutine to ensure the code
+# will compile. If such a routine already exists we do nothing as 
+# we lack the tools to do better than whatever is in that routine
+# already. If such a routine does not exist we create one but
+# setting the version number to blank (this is the best we can do).
+#
+# First find out where this script actually lives so we can create
+# the appropriate Fortran file in the right location.
+#
+if [ -f "$0" ] ; then
+   # The first item on the command line is an actual file so the 
+   # script must have been specified including the path.
+   path="`dirname \"$0\"`"
+else
+   # The first item on the command line is not a file so script
+   # it must have been found in PATH.
+   path="`which \"$0\"`"
+   path="`dirname \"$path\"`"
+fi
+my_svnversion=`which svnversion`
+cd "$path"
+if [ -f ${my_svnversion} ] ; then
+  # svnversion exists, but does .svn?
+  if [ -d ../../.svn ] ; then
+    # .svn exists too
+    revision=`${my_svnversion} ../..`
+    echo "      subroutine util_nwchem_version(nwrev)" > util_nwchem_version.F
+    echo "      implicit none" >> util_nwchem_version.F
+    echo "      character*(*) nwrev" >> util_nwchem_version.F
+    echo "      nwrev=\"${revision}\"" >> util_nwchem_version.F
+    echo "      end" >> util_nwchem_version.F
+  else
+    if [ ! -f util_nwchem_version.F ] ; then
+      echo "      subroutine util_nwchem_version(nwrev)" > util_nwchem_version.F
+      echo "      implicit none" >> util_nwchem_version.F
+      echo "      character*(*) nwrev" >> util_nwchem_version.F
+      echo "      nwrev=\"N/A\"" >> util_nwchem_version.F
+      echo "      end" >> util_nwchem_version.F
+    fi
+  fi
+else
+  if [ ! -f util_nwchem_version.F ] ; then
+    echo "      subroutine util_nwchem_version(nwrev)" > util_nwchem_version.F
+    echo "      implicit none" >> util_nwchem_version.F
+    echo "      character*(*) nwrev" >> util_nwchem_version.F
+    echo "      nwrev=\"N/A\"" >> util_nwchem_version.F
+    echo "      end" >> util_nwchem_version.F
+  fi
+fi
