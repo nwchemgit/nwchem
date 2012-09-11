ae99a51d8f3d23b15466368e2faf8c1201f5be0d
Huub Van Dam <hvandam@bnl.gov>
2012-09-11 19:11:24 +0000

    HvD: Added some general documentation on the task module and polished a few things on the main task driver and task_input. Also moved the defgroup declaration from task_energy to the new documentation file task_dox.c. The latter just contains documentation and no code. It is just there to separate general documentation from the source code to minimize clutter.

---
+++ src/task/task_dox.c
@@ -0,0 +1,17 @@
+/* This file contains Doxygen documentation only, and no code at all.
+   It provides general information about the task module.
+*/
+
+/**
+\defgroup task Task interface
+\brief Interface to control the main tasks
+
+In NWChem the overall work of a job is broken up into tasks. 
+In general, each task is defined by an energy expression and a quantity to
+calculate with it. For example, `task scf optimize` calculates a minimum energy
+molecular structure using the Hartree-Fock energy expression.
+
+The routines in this module implement how tasks are actually accomplished.
+I.e. the task in the above example is implemented in the function 
+`task_optimize`.
+*/
