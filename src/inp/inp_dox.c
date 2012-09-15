e1530b6087774c1a273ab4cc84e8623b211cf80e
Huub Van Dam <hvandam@bnl.gov>
2012-09-15 07:49:28 +0000

    HvD: Added some introductory statements on the Free Format Input module.

---
+++ src/inp/inp_dox.c
@@ -0,0 +1,20 @@
+/* This file contains only Doxygen documentation. It does not contain any
+   actual source code at all.
+*/
+
+/**
+\defgroup inp Free format input module
+\brief Notes on the input parsing routines
+
+The input parser processes the user's input file and translates the
+information into a form meaningful to the main program and the driver routines
+for specific tasks.  The parser translates input following the 
+rules for free-format input specified in the NWChem Users Manual.  The
+following sections present detailed descriptions of the functions
+used by the input parser, and the conventional form of the processed
+input.
+
+All input routines must be declared in the header file `inp.fh`.
+
+
+*/
