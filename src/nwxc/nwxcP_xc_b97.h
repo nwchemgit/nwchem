f4180864ef5bde37b1f909b82422dfc30ddd1bc7
Huub Van Dam <hvandam@bnl.gov>
2013-12-17 18:28:47 +0000

    HvD: Adding the Becke97 family of functionals.

---
+++ src/nwxc/nwxcP_xc_b97.h
@@ -0,0 +1,51 @@
+
+#define _FCLDA_ELEMENTS 9
+#define _FXC_NUMDERI 36
+
+
+#define _FXC_E       0
+
+#define _FXC_RA      1
+#define _FXC_RB      2
+
+#define _FXC_GAA     3
+#define _FXC_GAB     4
+#define _FXC_GBB     5
+
+#define _FXC_RARA    6
+#define _FXC_RBRB    7
+#define _FXC_RARB    8
+
+#define _FXC_GAAGAA  9
+#define _FXC_GAAGAB 10
+#define _FXC_GAAGBB 11
+#define _FXC_GBBGAB 12
+#define _FXC_GBBGBB 13
+#define _FXC_GABGAB 14
+
+#define _FXC_RAGAA  15
+#define _FXC_RAGAB  16
+#define _FXC_RAGBB  17
+#define _FXC_RBGAA  18
+#define _FXC_RBGAB  19
+#define _FXC_RBGBB  20
+
+#define _FXC_TA     21
+#define _FXC_TB     22
+
+#define _FXC_TATA   23
+#define _FXC_TATB   24
+#define _FXC_TBTB   25
+
+#define _FXC_TARA   26
+#define _FXC_TARB   27
+#define _FXC_TBRA   28
+#define _FXC_TBRB   29
+
+#define _FXC_TAGAA  30
+#define _FXC_TAGAB  31
+#define _FXC_TAGBB  32
+#define _FXC_TBGAA  33
+#define _FXC_TBGAB  34
+#define _FXC_TBGBB  35
+
