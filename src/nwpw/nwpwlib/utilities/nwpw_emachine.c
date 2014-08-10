1bad798a3cebb4e04abdd6761b2f9cb3b291b39c
Eric Bylaska <Eric.Bylaska@pnnl.gov>
2014-08-10 14:23:22 +0000

    ...EJB

--- src/nwpw/nwpwlib/utilities/nwpw_emachine.c
+++ src/nwpw/nwpwlib/utilities/nwpw_emachine.c
@@ -445,6 +445,448 @@ static void nwpw_gencode(Integer code[], double fconst[], int ln, char expr[])
 
 /*************************  parser routines *******************************/
 
+/*************************  emachine routines *******************************/
+
+
+/* registers */
+static Integer i,j,k,ii,jj,kk;
+
+
+/********************************************
+ *                                          *
+ *                nwpw_emachine             *
+ *                                          *
+ ********************************************/
+
+double nwpw_emachine(int ln, Integer code[], double fconst[], Integer nion, double rion[])
+{
+   Integer op,arg1,arg2,arg3,arg4,ln1,ln2;
+   double f,a,b;
+
+   f = 0.0;
+   op  = code[5*ln];
+   ln1 = code[5*ln+1];
+   ln2 = arg1 = code[5*ln+2];
+   arg2       = code[5*ln+3];
+   arg3       = code[5*ln+4];
+
+   if (arg1==(-9999)) arg1 = nion;
+   if (arg1==(-8888)) arg1 = i;
+   if (arg1==(-7777)) arg1 = j;
+   if (arg1==(-6666)) arg1 = k;
+   if (arg1==(-5555)) arg1 = ii;
+   if (arg1==(-4444)) arg1 = jj;
+   if (arg1==(-3333)) arg1 = kk;
+   if (arg2==(-9999)) arg2 = nion;
+   if (arg2==(-8888)) arg2 = i;
+   if (arg2==(-7777)) arg2 = j;
+   if (arg2==(-6666)) arg2 = k;
+   if (arg2==(-5555)) arg2 = ii;
+   if (arg2==(-4444)) arg2 = jj;
+   if (arg2==(-3333)) arg2 = kk;
+   if (arg3==(-9999)) arg3 = nion;
+   if (arg3==(-8888)) arg3 = i;
+   if (arg3==(-7777)) arg3 = j;
+   if (arg3==(-6666)) arg3 = k;
+   if (arg3==(-5555)) arg3 = ii;
+   if (arg3==(-4444)) arg3 = jj;
+   if (arg3==(-3333)) arg3 = kk;
+   
+   f = 0.0;
+   switch (op) {
+    
+      case -1: /* + */
+         f = nwpw_emachine(ln1,code,fconst,nion,rion) + nwpw_emachine(ln2,code,fconst,nion,rion);
+         break;
+
+      case -2: /* - */
+         f = nwpw_emachine(ln1,code,fconst,nion,rion) - nwpw_emachine(ln2,code,fconst,nion,rion);
+         break;
+
+      case -3: /* * */
+         f = nwpw_emachine(ln1,code,fconst,nion,rion) * nwpw_emachine(ln2,code,fconst,nion,rion);
+         break;
+
+      case -4: /* / */
+         f = nwpw_emachine(ln1,code,fconst,nion,rion) / nwpw_emachine(ln2,code,fconst,nion,rion);
+         break;
+
+      case -5: /* ** or ^ */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion);
+         b = nwpw_emachine(ln2,code,fconst,nion,rion);
+         f = pow(a,b);
+         break;
+
+      case -6: /* abs */
+         f = fabs(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -7: /* exp */
+         f = exp(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -8: /* log */
+         f = log(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -9: /* sqrt */
+         f = sqrt(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -10: /* sinh */
+         f = sinh(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -11: /* cosh */
+         f = cosh(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -12: /* tanh */
+         f = tanh(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -13: /* sin */
+         f = sin(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -14: /* cos */
+         f = cos(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -15: /* tan */
+         f = tan(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -16: /* asin */
+         f = asin(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -17: /* acos */
+         f = acos(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -18: /* atan */
+         f = atan(nwpw_emachine(ln1,code,fconst,nion,rion));
+         break;
+
+      case -19: /* sum(i=arg1,arg2:arg3) */
+         for (i=arg1; i<=arg2; i+=arg3) f += nwpw_emachine(ln1,code,fconst,nion,rion);
+         break;
+
+      case -20: /* sum(j=arg1,arg2:arg3) */
+         for (j=arg1; j<=arg2; j+=arg3) f += nwpw_emachine(ln1,code,fconst,nion,rion);
+         break;
+
+      case -21: /* sum(k=arg1,arg2:arg3) */
+         for (k=arg1; k<=arg2; k+=arg3) f += nwpw_emachine(ln1,code,fconst,nion,rion);
+         break;
+
+      case -22: /* sum(ii=arg1,arg2:arg3) */
+         for (ii=arg1; ii<=arg2; ii+=arg3) f += nwpw_emachine(ln1,code,fconst,nion,rion);
+         break;
+
+      case -23: /* sum(jj=arg1,arg2:arg3) */
+         for (jj=arg1; jj<=arg2; jj+=arg3) f += nwpw_emachine(ln1,code,fconst,nion,rion);
+         break;
+
+      case -24: /* sum(kk=arg1,arg2:arg3) */
+         for (kk=arg1; kk<=arg2; kk+=arg3) f += nwpw_emachine(ln1,code,fconst,nion,rion);
+         break;
+
+      case -25: /* fconst(arg1) */
+         f = fconst[arg1];
+         break;
+
+      case -30: /* rion(arg1,arg2) */
+         f = rion[(arg1-1)+3*(arg2-1)];
+         break;
+
+
+      case -40: /* i */
+         f = ((double) i);
+         break;
+
+      case -41: /* j */
+         f = ((double) j);
+         break;
+
+      case -42: /* k */
+         f = ((double) k);
+         break;
+
+      case -43: /* ii */
+         f = ((double) ii);
+         break;
+
+      case -44: /* jj */
+         f = ((double) jj);
+         break;
+
+      case -45: /* kk */
+         f = ((double) kk);
+         break;
+
+       default:
+          f = 0.0;
+          
+   }
+
+   return f;
+}
+
+
+
+/********************************************
+ *                                          *
+ *                nwpw_fmachine             *
+ *                                          *
+ ********************************************/
+
+double nwpw_fmachine(Integer i0, Integer ii0, Integer ln, Integer code[], double fconst[], Integer nion, double rion[])
+{
+   Integer op,arg1,arg2,arg3,arg4,ln1,ln2;
+   double df,a,b,c,d,bb,aa,fac1,fac2;
+
+   df = 0.0;
+   op  = code[5*ln];
+   ln1 = code[5*ln+1];
+   ln2 = arg1 = code[5*ln+2];
+   arg2       = code[5*ln+3];
+   arg3       = code[5*ln+4];
+
+   if (arg1==(-9999)) arg1 = nion;
+   if (arg1==(-8888)) arg1 = i;
+   if (arg1==(-7777)) arg1 = j;
+   if (arg1==(-6666)) arg1 = k;
+   if (arg1==(-5555)) arg1 = ii;
+   if (arg1==(-4444)) arg1 = jj;
+   if (arg1==(-3333)) arg1 = kk;
+   if (arg2==(-9999)) arg2 = nion;
+   if (arg2==(-8888)) arg2 = i;
+   if (arg2==(-7777)) arg2 = j;
+   if (arg2==(-6666)) arg2 = k;
+   if (arg2==(-5555)) arg2 = ii;
+   if (arg2==(-4444)) arg2 = jj;
+   if (arg2==(-3333)) arg2 = kk;
+   if (arg3==(-9999)) arg3 = nion;
+   if (arg3==(-8888)) arg3 = i;
+   if (arg3==(-7777)) arg3 = j;
+   if (arg3==(-6666)) arg3 = k;
+   if (arg3==(-5555)) arg3 = ii;
+   if (arg3==(-4444)) arg3 = jj;
+   if (arg3==(-3333)) arg3 = kk;
+
+   df = 0.0;
+   switch (op) {
+    
+      case -1: /* + */
+         df = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion) + nwpw_fmachine(i0,ii0,ln2,code,fconst,nion,rion);
+         break;
+
+      case -2: /* - */
+         df = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion) - nwpw_fmachine(i0,ii0,ln2,code,fconst,nion,rion);
+         break;
+
+      case -3: /* * */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion);
+         b = nwpw_emachine(ln2,code,fconst,nion,rion);
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         d = nwpw_fmachine(i0,ii0,ln2,code,fconst,nion,rion);
+         df = c*b + a*d;
+         printf("* df=%le a=%le b=%le c=%le d=%le\n\n",df,a,b,c,d);
+         break;
+
+      case -4: /* / */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion);
+         b = nwpw_emachine(ln2,code,fconst,nion,rion);
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         d = nwpw_fmachine(i0,ii0,ln2,code,fconst,nion,rion);
+         bb = b*b; if (bb<1.0e-8) bb = 1.0e-8;
+         df = (b*c - a*d)/(bb);
+         break;
+
+      case -5: /* ** or ^ */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion);
+         b = nwpw_emachine(ln2,code,fconst,nion,rion);
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         d = nwpw_fmachine(i0,ii0,ln2,code,fconst,nion,rion);
+         fac1 = pow(a,b-1.0);
+         if (fabs(d)>1.0e-10)
+            fac2 = (b*c + a*log(a)*d);
+         else
+            fac2 = b*c;
+         df = fac1*fac2;
+         printf("** df=%le a=%le b=%le c=%le d=%le fac1=%le fac2=%le\n",df,a,b,c,d,fac1,fac2);
+         break;
+
+      case -6: /* abs */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion);
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         aa = fabs(a); if (aa<1.0e-8) aa = 1.0e-8;
+         df = (a*c)/aa;
+         break;
+
+      case -7: /* exp */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion);
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         df = exp(a)*c;
+         break;
+
+      case -8: /* log */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         if (fabs(a)<1.0e-8)
+         {
+            if (a<0.0) 
+               a = -1.0e-8;
+            else
+               a = 1.0e-8;
+         }
+         df = c/a;
+         break;
+
+      case -9: /* sqrt */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         printf("sqrt a=%le c=%le\n",a,c);
+         df = 0.5*c/sqrt(a);
+         break;
+
+      case -10: /* sinh */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         df = c*cosh(a);
+         break;
+
+      case -11: /* cosh */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         df = c*sinh(a);
+         break;
+
+      case -12: /* tanh */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         b = cosh(a);
+         if (fabs(b)<1.0e-8) b = 1.0e-8;
+         df = c/(b*b);
+         break;
+
+      case -13: /* sin */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         df = c*cos(a);
+         break;
+
+      case -14: /* cos */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         df = -c*sin(a);
+         break;
+
+      case -15: /* tan */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         b = cos(a);
+         if (fabs(b)<1.0e-8) b = 1.0e-8;
+         df = c/(b*b);
+         break;
+
+      case -16: /* asin */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         b = 1.0-a*a;
+         if (b<1.0e-8) b = 1.0e-8;
+         b = sqrt(b);
+         df = c/b;
+         break;
+
+      case -17: /* acos */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         b = 1.0-a*a;
+         if (b<1.0e-8) b = 1.0e-8;
+         b = sqrt(b);
+         df = -c/b;
+         break;
+
+      case -18: /* atan */
+         a = nwpw_emachine(ln1,code,fconst,nion,rion); 
+         c = nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         df = c/(1+a*a);
+         break;
+
+      case -19: /* sum(i=arg1,arg2:arg3) */
+         for (i=arg1; i<=arg2; i+=arg3) df += nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         break;
+
+      case -20: /* sum(j=arg1,arg2:arg3) */
+         for (j=arg1; j<=arg2; j+=arg3) df += nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         break;
+
+      case -21: /* sum(k=arg1,arg2:arg3) */
+         for (k=arg1; k<=arg2; k+=arg3) df += nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         break;
+
+      case -22: /* sum(ii=arg1,arg2:arg3) */
+         for (ii=arg1; ii<=arg2; ii+=arg3) df += nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         break;
+
+      case -23: /* sum(jj=arg1,arg2:arg3) */
+         for (jj=arg1; jj<=arg2; jj+=arg3) df += nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         break;
+
+      case -24: /* sum(kk=arg1,arg2:arg3) */
+         for (kk=arg1; kk<=arg2; kk+=arg3) df += nwpw_fmachine(i0,ii0,ln1,code,fconst,nion,rion);
+         break;
+
+      case -25: /* fconst(arg1) */
+         df = 0.0;
+         break;
+
+      case -30: /* rion(arg1,arg2) */
+         if ((arg1==i0) && (arg2==ii0))
+            df = 1.0;
+         else 
+            df = 0.0;
+         break;
+
+      case -40: /* i */
+         df = 0.0;
+         break;
+
+      case -41: /* j */
+         df = 0.0;
+         break;
+
+      case -42: /* k */
+         df = 0.0;
+         break;
+
+      case -43: /* ii */
+         df = 0.0;
+         break;
+      
+      case -44: /* jj */
+         df = 0.0;
+         break;
+
+      case -45: /* kk */
+         df = 0.0;
+         break;
+
+
+       default:
+          df = 0.0;
+          
+   }
+
+   return df;
+}
+
+
+/*************************  emachine routines *******************************/
+
 
 
 /********************************************************
@@ -497,4 +939,93 @@ double fconst0[];
    *nf0 = nf;
 
 }
+
+
+
+/********************************************************
+ *                                                      *
+ *               nwpw_emachine_f                        *
+ *                                                      *
+ ********************************************************/
+
+#if (defined(CRAY) &&!defined(__crayx1)) || defined(CRAY_T3D) || defined(WIN32)
+#define nwpw_emachine_f_ nwpw_emachine_f
+#endif
+
+double FATR nwpw_emachine_f_
+#if defined(USE_FCD)
+(
+ Integer *nc0,
+ Integer code0[],
+ Integer *nf0,
+ double fconst0[],
+ Integer *nion0,
+ double  *rion0[])
+{
+
+#else
+(nc0,code0,nf0,fconst0,nion0,rion0)
+Integer *nc0;
+Integer code0[];
+Integer *nf0;
+double fconst0[];
+Integer *nion0;
+double rion0[];
+{
+
+#endif
+   double f;
+
+   f = nwpw_emachine(0,code0,fconst0,*nion0,rion0);
+
+   return f;
+
+}
+
+
+
+/********************************************************
+ *                                                      *
+ *               nwpw_emachine_df                       *
+ *                                                      *
+ ********************************************************/
+
+#if (defined(CRAY) &&!defined(__crayx1)) || defined(CRAY_T3D) || defined(WIN32)
+#define nwpw_emachine_df_ nwpw_emachine_df
+#endif
+
+double FATR nwpw_emachine_df_
+#if defined(USE_FCD)
+(
+ Integer *i0,
+ Integer *ii0,
+ Integer *nc0,
+ Integer code0[],
+ Integer *nf0,
+ double fconst0[],
+ Integer *nion0,
+ double  *rion0[])
+{
+
+#else
+(i0,ii0,nc0,code0,nf0,fconst0,nion0,rion0)
+Integer *i0;
+Integer *ii0;
+Integer *nc0;
+Integer code0[];
+Integer *nf0;
+double fconst0[];
+Integer *nion0;
+double rion0[];
+{
+
+#endif
+   double df;
+
+   df = nwpw_fmachine(*i0,*ii0,0,code0,fconst0,*nion0,rion0);
+
+   return df;
+
+}
+
 
