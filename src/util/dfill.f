2933be9c57959b48b9a567bcb93340d694c8544c
Edoardo Apra <edoardo.apra@gmail.com>
2015-02-12 17:15:55 +0000

    got rid of cppisms

--- src/util/dfill.f
+++ src/util/dfill.f
@@ -7,18 +7,6 @@ C$Id$
 c
 c     initialise double precision array to scalar value
 c
-#if 1
          call dcopy(n,val,0,a,ia)
-#else
-      if (ia.eq.1) then
-         do 10 i = 1, n
-            a(i) = val
- 10      continue
-      else
-         do 20 i = 1,(n-1)*ia+1,ia
-            a(i) = val
- 20      continue
-      endif
-#endif
 c
       end
