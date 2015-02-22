ed0d75fdbb7d573b5a5f978b3de0f3dc0a3349a8
Jeff Hammond <jeff.r.hammond@intel.com>
2015-02-22 04:49:27 +0000

    whitespace only

--- src/tce/sort/new_sort4.f
+++ src/tce/sort/new_sort4.f
@@ -16,76 +16,76 @@ c
       jd(3) = c
       jd(4) = d
       if (l.eq.4) then
-      do j1 = 1,a
+       do j1 = 1,a
+        do j2 = 1,b
+         do j3 = 1,c
+          id(1) = j1
+          id(2) = j2
+          id(3) = j3
+          !id(4) = j4 ! 1234 are only used once and we know l=4, hence ijk must not be 4
+          iax = d*(j3-1+c*(j2-1+b*(j1-1)))
+          ibx = d*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          do j4 = 1,d
+           ia = j4+iax
+           ib = j4+ibx
+           s(ib) = u(ia) * f
+          enddo
+         enddo
+        enddo
+       enddo
+      else if (l.eq.3) then
+       do j1 = 1,a
+        do j2 = 1,b
+         do j4 = 1,d
+          id(1) = j1
+          id(2) = j2
+          !id(3) = j3
+          id(4) = j4
+          iax = -1+c*(j2-1+b*(j1-1))
+          ibx = c*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          do j3 = 1,c
+           ia = j4+d*(j3+iax)
+           ib = j3+ibx
+           s(ib) = u(ia) * f
+          enddo
+         enddo
+        enddo
+       enddo
+      else if (l.eq.2) then
+       do j1 = 1,a
+        do j3 = 1,c
+         do j4 = 1,d
+          id(1) = j1
+          !id(2) = j2
+          id(3) = j3
+          id(4) = j4
+          iax = -1+b*(j1-1)
+          ibx = b*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          do j2 = 1,b
+           ia = j4+d*(j3-1+c*(j2+iax))
+           ib = j2+ibx
+           s(ib) = u(ia) * f
+          enddo
+         enddo
+        enddo
+       enddo
+      else ! l.eq.1
        do j2 = 1,b
         do j3 = 1,c
-         id(1) = j1
-         id(2) = j2
-         id(3) = j3
-         !id(4) = j4 ! 1234 are only used once and we know l=4, hence ijk must not be 4
-         iax = d*(j3-1+c*(j2-1+b*(j1-1)))
-         ibx = d*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
          do j4 = 1,d
-          ia = j4+iax
-          ib = j4+ibx
-          s(ib) = u(ia) * f
+          !id(1) = j1
+          id(2) = j2
+          id(3) = j3
+          id(4) = j4
+          ibx = a*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          do j1 = 1,a
+           ia = j4+d*(j3-1+c*(j2-1+b*(j1-1)))
+           ib = j1+ibx
+           s(ib) = u(ia) * f
+          enddo
          enddo
         enddo
        enddo
-      enddo
-      else if (l.eq.3) then
-      do j1 = 1,a
-       do j2 = 1,b
-        do j4 = 1,d
-         id(1) = j1
-         id(2) = j2
-         !id(3) = j3
-         id(4) = j4
-         iax = -1+c*(j2-1+b*(j1-1))
-         ibx = c*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
-         do j3 = 1,c
-          ia = j4+d*(j3+iax)
-          ib = j3+ibx
-          s(ib) = u(ia) * f
-         enddo
-        enddo
-       enddo
-      enddo
-      else if (l.eq.2) then
-      do j1 = 1,a
-       do j3 = 1,c
-        do j4 = 1,d
-         id(1) = j1
-         !id(2) = j2
-         id(3) = j3
-         id(4) = j4
-         iax = -1+b*(j1-1)
-         ibx = b*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
-         do j2 = 1,b
-          ia = j4+d*(j3-1+c*(j2+iax))
-          ib = j2+ibx
-          s(ib) = u(ia) * f
-         enddo
-        enddo
-       enddo
-      enddo
-      else ! l.eq.1
-      do j2 = 1,b
-       do j3 = 1,c
-        do j4 = 1,d
-         !id(1) = j1
-         id(2) = j2
-         id(3) = j3
-         id(4) = j4
-         ibx = a*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
-         do j1 = 1,a
-          ia = j4+d*(j3-1+c*(j2-1+b*(j1-1)))
-          ib = j1+ibx
-          s(ib) = u(ia) * f
-         enddo
-        enddo
-       enddo
-      enddo
       endif
       return
       end
@@ -104,76 +104,76 @@ c
       jd(3) = c
       jd(4) = d
       if (l.eq.4) then
-      do j1 = 1,a
+       do j1 = 1,a
+        do j2 = 1,b
+         do j3 = 1,c
+          id(1) = j1
+          id(2) = j2
+          id(3) = j3
+          !id(4) = j4 ! 1234 are only used once and we know l=4, hence ijk must not be 4
+          iax = d*(j3-1+c*(j2-1+b*(j1-1)))
+          ibx = d*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          do j4 = 1,d
+           ia = j4+iax
+           ib = j4+ibx
+           s(ib) = s(ib) + u(ia) * f
+          enddo
+         enddo
+        enddo
+       enddo
+      else if (l.eq.3) then
+       do j1 = 1,a
+        do j2 = 1,b
+         do j4 = 1,d
+          id(1) = j1
+          id(2) = j2
+          !id(3) = j3
+          id(4) = j4
+          iax = -1+c*(j2-1+b*(j1-1))
+          ibx = c*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          do j3 = 1,c
+           ia = j4+d*(j3+iax)
+           ib = j3+ibx
+           s(ib) = s(ib) + u(ia) * f
+          enddo
+         enddo
+        enddo
+       enddo
+      else if (l.eq.2) then
+       do j1 = 1,a
+        do j3 = 1,c
+         do j4 = 1,d
+          id(1) = j1
+          !id(2) = j2
+          id(3) = j3
+          id(4) = j4
+          iax = -1+b*(j1-1)
+          ibx = b*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          do j2 = 1,b
+           ia = j4+d*(j3-1+c*(j2+iax))
+           ib = j2+ibx
+           s(ib) = s(ib) + u(ia) * f
+          enddo
+         enddo
+        enddo
+       enddo
+      else ! l.eq.1
        do j2 = 1,b
         do j3 = 1,c
-         id(1) = j1
-         id(2) = j2
-         id(3) = j3
-         !id(4) = j4 ! 1234 are only used once and we know l=4, hence ijk must not be 4
-         iax = d*(j3-1+c*(j2-1+b*(j1-1)))
-         ibx = d*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
          do j4 = 1,d
-          ia = j4+iax
-          ib = j4+ibx
-          s(ib) = s(ib) + u(ia) * f
+          !id(1) = j1
+          id(2) = j2
+          id(3) = j3
+          id(4) = j4
+          ibx = a*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
+          do j1 = 1,a
+           ia = j4+d*(j3-1+c*(j2-1+b*(j1-1)))
+           ib = j1+ibx
+           s(ib) = s(ib) + u(ia) * f
+          enddo
          enddo
         enddo
        enddo
-      enddo
-      else if (l.eq.3) then
-      do j1 = 1,a
-       do j2 = 1,b
-        do j4 = 1,d
-         id(1) = j1
-         id(2) = j2
-         !id(3) = j3
-         id(4) = j4
-         iax = -1+c*(j2-1+b*(j1-1))
-         ibx = c*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
-         do j3 = 1,c
-          ia = j4+d*(j3+iax)
-          ib = j3+ibx
-          s(ib) = s(ib) + u(ia) * f
-         enddo
-        enddo
-       enddo
-      enddo
-      else if (l.eq.2) then
-      do j1 = 1,a
-       do j3 = 1,c
-        do j4 = 1,d
-         id(1) = j1
-         !id(2) = j2
-         id(3) = j3
-         id(4) = j4
-         iax = -1+b*(j1-1)
-         ibx = b*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
-         do j2 = 1,b
-          ia = j4+d*(j3-1+c*(j2+iax))
-          ib = j2+ibx
-          s(ib) = s(ib) + u(ia) * f
-         enddo
-        enddo
-       enddo
-      enddo
-      else ! l.eq.1
-      do j2 = 1,b
-       do j3 = 1,c
-        do j4 = 1,d
-         !id(1) = j1
-         id(2) = j2
-         id(3) = j3
-         id(4) = j4
-         ibx = a*(id(k)-1+jd(k)*(id(j)-1+jd(j)*(id(i)-1)))
-         do j1 = 1,a
-          ia = j4+d*(j3-1+c*(j2-1+b*(j1-1)))
-          ib = j1+ibx
-          s(ib) = s(ib) + u(ia) * f
-         enddo
-        enddo
-       enddo
-      enddo
       endif
       return
       end
