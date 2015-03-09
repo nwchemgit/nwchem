20243d5b041e30656718b2a885c62ff0938d40cb
Huub Van Dam <hvandam@bnl.gov>
2015-03-09 23:07:47 +0000

    HvD: Documented the preferred way to initialize an array to point coders in the right direction.

--- src/util/dfill.f
+++ src/util/dfill.f
@@ -1,3 +1,9 @@
+C>
+C> \brief Routine to initialize an array (Deprecated)
+C>
+C> The DCOPY call that implements this routine is the preferred
+C> initialization mechanism for performance reasons.
+C>
       subroutine dfill(n,val,a,ia)
 C$Id$
       implicit none
