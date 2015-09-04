fc36252f0a3b83c8df93dfc8aa1aba5e4862be85
Edoardo Apra <edoardo.apra@gmail.com>
2015-09-04 23:23:30 +0000

    missing routines

---
+++ src/util/zfill.f
@@ -0,0 +1,18 @@
+C>
+C> \brief Routine to initialize an array (Deprecated)
+C>
+C> The DCOPY call that implements this routine is the preferred
+C> initialization mechanism for performance reasons.
+C>
+      subroutine zfill(n,val,a,ia)
+C$Id: dfill.f 26932 2015-03-09 23:07:47Z d3y133 $
+      implicit none
+      integer n, ia
+      double complex val, a(*)
+      integer i
+c
+c     initialise double precision array to scalar value
+c
+         call zcopy(n,val,0,a,ia)
+c
+      end
