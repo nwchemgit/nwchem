ef311e719841030a3f31b0a302c6ddfc077b23fb
Edoardo Apra <edoardo.apra@gmail.com>
2015-09-11 22:38:48 +0000

    mingw

--- src/util/linux_cpu.c
+++ src/util/linux_cpu.c
@@ -5,7 +5,11 @@
 #include <sys/time.h>
 #include <unistd.h>
 #include <stdio.h>
-#if !defined(__MINGW32__)
+#if defined(__MINGW32__)
+double linux_cputime_(void){
+  return ((double) clock()) / ((double) CLOCKS_PER_SEC);
+}
+#else
 #include <sys/resource.h>
 
 double linux_cputime_(void)
@@ -15,10 +19,5 @@ double linux_cputime_(void)
    (void) getrusage (RUSAGE_SELF, &rusage_out);
 
   return ((double)rusage_out.ru_utime.tv_usec* 1E-6 + (double)(rusage_out.ru_utime.tv_sec));
-}
-#else
-double linux_cputime_(void)
-{
-  return 0.;
 }
 #endif
