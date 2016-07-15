7692e4186212470ec1739a22b2ab7d391bfebb5f
Eric Bylaska <Eric.Bylaska@pnnl.gov>
2016-07-15 18:17:53 +0000

    ...EJB

--- src/nwpw/nwpwlib/TMatrix/test/test.f
+++ src/nwpw/nwpwlib/TMatrix/test/test.f
@@ -1,7 +1,7 @@
       program testab
       implicit none
       integer npack,N
-      parameter (npack=20000,N=256)
+      parameter (npack=12000,N=256)
       real*8 A(npack,N)
       real*8 C(npack,N)
       real*8 B(N,N)
@@ -25,34 +25,48 @@
       call current_second(t0)
       write(*,*) "hera"
 
-!$OMP PARALLEL private(it,tid,nthr,Itid,Mtid,r)
+!$OMP PARALLEL private(it,tid,nthr,Itid,Mtid,r) shared(A,B,C)
       tid  = omp_get_thread_num()
       nthr = omp_get_num_threads()
-
-      do i=1,10
-         write(*,*) "into dgemm"
-         M = npack
-         r    = mod(M,nthr)
-         if (tid.lt.r) then
-             Itid = tid*(M/nthr+1) + 1
-             Mtid = M/nthr + 1
-         else
-             Itid = r + tid*(M/nthr) + 1
-             Mtid = M/nthr
-         end if
-
-         call DGEMM('N','N',npack,N,N,
-     >           1.0d0,
-     >           A(Itid,1),npack,
-     >           B,N,
-     >           0.0d0,
-     >           C(Itid,1),npack)
-
-         write(*,*) "out dgemm"
+      write(*,*) "into dgemm",tid,nthr
+      do it=1,10
+         call  dgemmtest(npack,N,N,A,B,C)
       end do
 !$OMP END PARALLEL
       call current_second(t1)
       write(*,*) "time=",t1-t0
       return
       end
+
+      subroutine dgemmtest(M,N,K,A,B,C)
+      implicit none
+      integer M,N,K
+      real*8 A(*)
+      real*8 B(*)
+      real*8 C(*)
+      integer r,tid,nthr,Itid,Mtid
+      integer  omp_get_num_threads, omp_get_thread_num
+      external omp_get_num_threads, omp_get_thread_num
+
+      tid  = omp_get_thread_num()
+      nthr = omp_get_num_threads()
+      r    = mod(M,nthr)
+      if (tid.lt.r) then
+          !Itid = tid*(M/nthr+1) + 1
+          Itid = tid*(M/nthr+1)*N + 1
+          Mtid = M/nthr + 1
+      else
+          !Itid = r + tid*(M/nthr) + 1
+          Itid = (r + tid*(M/nthr))*N + 1
+          Mtid = M/nthr
+      end if
+      write(*,*) tid,"  Itid,Mtid,N,K=",Itid,Mtid,N,K
+      call DGEMM('N','N',Mtid,N,K,
+     >        1.0d0,
+     >        A(Itid),Mtid,
+     >        B,K,
+     >        0.0d0,
+     >        C(Itid),Mtid)
      
+       return
+       end
