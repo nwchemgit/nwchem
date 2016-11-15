fe5a9a4343f381a02184a79d4d5673f532ec1fe2
Jeff Hammond <jeff.r.hammond@intel.com>
2016-11-15 22:38:34 +0000

    switch to modern comment style for VIM readability

--- src/ccsd/geaxpy.f
+++ src/ccsd/geaxpy.f
@@ -1,39 +1,39 @@
-C:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-C NAME
-C     GEAXPY -- Matrix Y <-- a*X + Y
-C
-C REVISION
-C     $Id$
-C
-C SYNOPSIS
+!:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+! NAME
+!     GEAXPY -- Matrix Y <-- a*X + Y
+!
+! REVISION
+!     $Id$
+!
+! SYNOPSIS
       Subroutine GEAXPY(M, N, Alpha, X, LDX, Y, LDY)
       Implicit NONE
       Integer M, N, LDX, LDY
       Double precision Alpha
       Double precision X(LDX, N), Y(LDY, N)
-C
-C ARGUMENTS
-C     M       Row dimension of X, Y [IN]
-C     N       Column dimension of X, Y [IN]
-C     Alpha   Scale factor for X [IN]
-C     X       MxN matrix [IN]
-C     LDX     Leading dimension of X [IN]
-C     Y       Result matrix [INOUT]
-C     LDY     Leading dimension of Y [IN]
-C
-C DESCRIPTION
-C     A simple convenience routine to simplify the addition of two
-C     matrices which may have different leading dimensions (which makes
-C     use of DAXPY unsafe).
-C
-C     Implemented as DAXPY calls on each column of X/Y.  Easy.
-C:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-C LOCAL VARIABLES
+!
+! ARGUMENTS
+!     M       Row dimension of X, Y [IN]
+!     N       Column dimension of X, Y [IN]
+!     Alpha   Scale factor for X [IN]
+!     X       MxN matrix [IN]
+!     LDX     Leading dimension of X [IN]
+!     Y       Result matrix [INOUT]
+!     LDY     Leading dimension of Y [IN]
+!
+! DESCRIPTION
+!     A simple convenience routine to simplify the addition of two
+!     matrices which may have different leading dimensions (which makes
+!     use of DAXPY unsafe).
+!
+!     Implemented as DAXPY calls on each column of X/Y.  Easy.
+!:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+! LOCAL VARIABLES
       Integer J
-C
+!
       Do J = 1, N
          Call daxpy(M, Alpha, X(1, J), 1, Y(1, J), 1)
       EndDo
-C
+!
       Return
       End
