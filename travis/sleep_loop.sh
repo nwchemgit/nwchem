8383eeeec82b9db7212fa5108806e2a3e5dd2ec0
edoapra <edoardo.apra@gmail.com>
2017-11-22 23:37:39 -0800

    bash arguments

--- travis/sleep_loop.sh
+++ travis/sleep_loop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "starting sleep_loop.sh for command: " $*
-$* >& make.log &
+echo "starting sleep_loop.sh for command: " ${@}
+${@} >& make.log &
 pid=$!
 echo "sleep_loop got pid" $pid
 while :
 do
-    sleep 60
+    sleep 60s
     if kill -0 "$pid" 2>/dev/null; then
 	echo ' ==== ' `date` ' ==== '
 	tail -1 make.log
