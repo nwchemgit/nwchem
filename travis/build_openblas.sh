6d8a65c0965b95562b05811b6901d62b14110b7d
edoapra <edoardo.apra@gmail.com>
2017-11-22 18:40:59 -0800

    added qas

--- travis/build_openblas.sh
+++ travis/build_openblas.sh
@@ -2,6 +2,6 @@
 cd $TRAVIS_BUILD_DIR
 git clone -b release-0.2.21 https://github.com/xianyi/OpenBLAS.git
 cd OpenBLAS
-make -j3 PREFIX=$TRAVIS_BUILD_DIR NO_CBLAS=1 NO_LAPACKE=1 DEBUG=1 NUM_THREADS=1 all >& openblas.log
+make -j3 PREFIX=$TRAVIS_BUILD_DIR  USE_THREAD=0 NO_CBLAS=1 NO_LAPACKE=1 DEBUG=1 NUM_THREADS=1 all >& openblas.log
 make PREFIX=$TRAVIS_BUILD_DIR install
 tail -4 openblas.log
