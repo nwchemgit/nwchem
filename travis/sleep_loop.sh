96f669a2430f4ff2bef7b599d35cbed5353ed6b8
edoapra <edoardo.apra@gmail.com>
2017-11-22 21:14:28 -0800

    sleep_loop.sh changes

--- travis/sleep_loop.sh
+++ travis/sleep_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -f
-$* &
+$* >& make.log &
 pid=$!
 echo "TT got pid" $pid
 while :
@@ -7,6 +7,7 @@ do
     sleep 60
     if kill -0 "$pid" 2>/dev/null; then
 	echo ' ==== ' `date` ' ==== '
+	tail -1 log
     elif wait "$pid"; then
 	break          #Abandon the loop.
     fi
