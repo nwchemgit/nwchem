20198c2e48d7d8f8aa066ce553525010f3b39d10
edoapra <edoardo.apra@gmail.com>
2017-11-22 17:12:34 -0800

    64_to_32 takes a loong time in tce

--- travis/build_openblas.sh
+++ travis/build_openblas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 cd $TRAVIS_BUILD_DIR
-git clone https://github.com/xianyi/OpenBLAS.git
+git clone -b release-0.2.21 https://github.com/xianyi/OpenBLAS.git
 cd OpenBLAS
 make -j3 PREFIX=$TRAVIS_BUILD_DIR NO_CBLAS=1 NO_LAPACKE=1 DEBUG=1 NUM_THREADS=1 all >& openblas.log
 make PREFIX=$TRAVIS_BUILD_DIR install
