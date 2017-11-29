e9468bcc73373e8a427fd39c6e3f9e43abf6d952
edoapra <edoardo.apra@gmail.com>
2017-11-28 18:54:32 -0800

    sleep_loop.sh for building scalapack

--- travis/build_scalapack.sh
+++ travis/build_scalapack.sh
@@ -4,7 +4,6 @@ git clone https://github.com/scibuilder/scalapack.git
 mkdir -p scalapack/build
 cd scalapack/build
 cmake ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTEST_SCALAPACK=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON  -DBLAS_blas_LIBRARY="-L$TRAVIS_BUILD_DIR/lib -lopenblas" -DLAPACK_lapack_LIBRARY="-L$TRAVIS_BUILD_DIR/lib -lopenblas"  -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR
-make V=0 -j3 >& scalap.log
-make install
-tail -20 scalap.log
+$TRAVIS_BUILD_DIR/travis/sleep_loop.sh make V=0 -j3 
+$TRAVIS_BUILD_DIR/travis/sleep_loop.sh make install
 ls -lrt $TRAVIS_BUILD_DIR/lib 
