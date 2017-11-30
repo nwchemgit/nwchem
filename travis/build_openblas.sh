982661fed7a29df901f89cd77191ce6e6f9695f7
Edoardo Aprà <edoardo.apra@gmail.com>
2017-11-29 21:53:16 -0800

    no sleep_loop for make install (too fast?)

--- travis/build_openblas.sh
+++ travis/build_openblas.sh
@@ -3,5 +3,5 @@ cd $TRAVIS_BUILD_DIR
 git clone -b release-0.2.21 https://github.com/xianyi/OpenBLAS.git
 cd OpenBLAS
 $TRAVIS_BUILD_DIR/travis/sleep_loop.sh make -j3 PREFIX=$TRAVIS_BUILD_DIR  USE_THREAD=0 NO_CBLAS=1 NO_LAPACKE=1 DEBUG=1 NUM_THREADS=1 all 
-$TRAVIS_BUILD_DIR/travis/sleep_loop.sh make PREFIX=$TRAVIS_BUILD_DIR install
+ make PREFIX=$TRAVIS_BUILD_DIR install
 #tail -4 openblas.log
