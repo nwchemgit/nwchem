1bbbc9b99371d0291c34544a9939ecb00244ee61
edoapra <edoardo.apra@gmail.com>
2017-11-30 17:43:29 -0800

    on mac we use brew to get scalapack and blas

--- travis/build_scalapack.sh
+++ travis/build_scalapack.sh
@@ -3,7 +3,7 @@ cd $TRAVIS_BUILD_DIR
 git clone https://github.com/scibuilder/scalapack.git
 mkdir -p scalapack/build
 cd scalapack/build
-cmake -Wno-dev ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTEST_SCALAPACK=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON  -DBLAS_blas_LIBRARY="-L$TRAVIS_BUILD_DIR/lib -lopenblas" -DLAPACK_lapack_LIBRARY="-L$TRAVIS_BUILD_DIR/lib -lopenblas"  -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR
+cmake -Wno-dev ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTEST_SCALAPACK=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=ON  -DBLAS_blas_LIBRARY="-L$TRAVIS_BUILD_DIR/lib -lopenblas" -DLAPACK_lapack_LIBRARY="-L$TRAVIS_BUILD_DIR/lib/ -lopenblas"  -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR
 $TRAVIS_BUILD_DIR/travis/sleep_loop.sh make V=0 -j3 
 $TRAVIS_BUILD_DIR/travis/sleep_loop.sh make install
 ls -lrt $TRAVIS_BUILD_DIR/lib 
--- travis/nwchem.bashrc
+++ travis/nwchem.bashrc
@@ -1,10 +1,17 @@
 #- env: == default ==
+os=`uname`
 export NWCHEM_TOP=$TRAVIS_BUILD_DIR
 export USE_MPI=y
 export USE_64TO32=y
-export BLASOPT="-L$TRAVIS_BUILD_DIR/lib -lopenblas"
+ if [[ "$os" == "Darwin" ]]; then 
+export BLASOPT="-lopenblas"
+export SCALAPACK=" -lscalapack -lopenblas"
+fi
+if [[ "$os" == "Linux" ]]; then 
+   export BLASOPT="-L$TRAVIS_BUILD_DIR/lib -lopenblas"
+   export SCALAPACK="-L$TRAVIS_BUILD_DIR/lib  -lscalapack -lopenblas"
+fi
 export BLAS_SIZE=4
-export SCALAPACK="-L$TRAVIS_BUILD_DIR/lib  -lscalapack -lopenblas"
 export SCALAPACK_SIZE=4
 export USE_PYTHONCONFIG=y
 export PYTHONVERSION=2.7
