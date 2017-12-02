f52e865b0a9dd25a131b58d603274088501e6179
Edoardo Aprà <edoardo.apra@gmail.com>
2017-12-02 10:51:04 -0800

    tce specific qas

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -f
 # source env. variables
  source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
- cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 dft_siosi3
- cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs  2 h2o_opt dft_he2+ cosmo_h2o_dft tddft_h2o h2o2-response
+ if [[ "$NWCHEM_MODULES" == "tce" ]]; then
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_n2
+ else
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 dft_siosi3
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs  2 h2o_opt dft_he2+ cosmo_h2o_dft tddft_h2o h2o2-response
+ fi
