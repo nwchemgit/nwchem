7631486078bf7efff1633bed12f2c6313e612e58
Edoardo Aprà <edoardo.apra@gmail.com>
2017-12-03 14:09:41 -0800

    added tce and pspw tests

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -2,8 +2,9 @@
 # source env. variables
  source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
  if [[ "$NWCHEM_MODULES" == "tce" ]]; then
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_n2
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc tce_ipccsd_f2
  else
    cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 dft_siosi3
    cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs  2 h2o_opt dft_he2+ cosmo_h2o_dft tddft_h2o h2o2-response
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs  2 pspw 
  fi
