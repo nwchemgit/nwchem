3837e43da2324916a735aaa02f4b4cdcb563209a
edoapra <edoardo.apra@gmail.com>
2017-12-04 15:17:39 -0800

    debu mpipr failures; added USE_SLEEPLOOP env. variable in runtests.mpi.unix

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -6,10 +6,11 @@
    nprocs=3
  fi
  if [[ "$NWCHEM_MODULES" == "tce" ]]; then
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs tce_ipccsd_f2 tce_eaccsd_ozone
+   cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc
+   cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs tce_ipccsd_f2 tce_eaccsd_ozone
  else
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs dft_siosi3
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs h2o_opt dft_he2+ cosmo_h2o_dft tddft_h2o h2o2-response
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs pspw pspw_md
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs dft_he2+ 
+   cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs h2o_opt cosmo_h2o_dft tddft_h2o h2o2-response
+   cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs dft_siosi3
+   cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs pspw pspw_md
  fi
