7cedc6cbc9c9774af124075e9309b8321096f2d2
NWChem: Open Source High-Performance Computational Chemistry <33767317+nwchemgit@users.noreply.github.com>
2017-12-04 00:09:05 -0800

    mpi-pr bits

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -f
 # source env. variables
  source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
+ nprocs=2
+ if [[ "$ARMCI_NETWORK" == "MPI-PR" ]]; then
+   nprocs=3
+ fi
  if [[ "$NWCHEM_MODULES" == "tce" ]]; then
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_ipccsd_f2 tce_eaccsd_ozone
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs tce_ipccsd_f2 tce_eaccsd_ozone
  else
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 dft_siosi3
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs  2 h2o_opt dft_he2+ cosmo_h2o_dft tddft_h2o h2o2-response
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs  2 pspw pspw_md
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs dft_siosi3
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs h2o_opt dft_he2+ cosmo_h2o_dft tddft_h2o h2o2-response
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs pspw pspw_md
  fi
