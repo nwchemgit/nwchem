6a3abe81f5aa780fe5d56918005ddc8dd0dc60e1
Edoardo Aprà <edoardo.apra@gmail.com>
2017-12-03 16:56:02 -0800

    diffs

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -3,8 +3,9 @@
  source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
  if [[ "$NWCHEM_MODULES" == "tce" ]]; then
    cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc
-   cd $NWCHEM_TOP/QA/testoutputs && diff tce_h2o_eomcc.out.nwparse tce_h2o_eomcc.out.nwparse
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix  tce_ipccsd_f2
+   echo "diff output for tce_h2o_eomcc"
+   diff $NWCHEM_TOP/QA/testoutputs/tce_h2o_eomcc.out.nwparse $NWCHEM_TOP/QA/testoutputstce_h2o_eomcc.out.nwparse
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_ipccsd_f2
    cd $NWCHEM_TOP/QA/testoutputs && diff tce_ipccsd_f2.out.nwparse tce_ipccsd_f2.out.nwparse
  else
    cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 dft_siosi3
