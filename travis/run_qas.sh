b3b5138e7861faf1f2c8d68ac667629e56bf06cb
Edoardo Aprà <edoardo.apra@gmail.com>
2017-12-03 16:13:46 -0800

    more tce bits

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -3,11 +3,9 @@
  source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
  if [[ "$NWCHEM_MODULES" == "tce" ]]; then
    cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc
-   cd $NWCHEM_TOP/QA/testoutputs
-   diff tce_h2o_eomcc.o*nw*
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 tce_ipccsd_f2
-   diff tce_ipccsd_f2.o*nw*
-
+   cd $NWCHEM_TOP/QA/testoutputs && diff tce_h2o_eomcc.out.nwparse tce_h2o_eomcc.out.nwparse
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix  tce_ipccsd_f2
+   cd $NWCHEM_TOP/QA/testoutputs && diff tce_ipccsd_f2.out.nwparse tce_ipccsd_f2.out.nwparse
  else
    cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs 2 dft_siosi3
    cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs  2 h2o_opt dft_he2+ cosmo_h2o_dft tddft_h2o h2o2-response
