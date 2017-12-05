9bb6faf4b9822b19c66834b358ae5f18c9c942e7
Edoardo Aprà <edoardo.apra@gmail.com>
2017-12-04 23:43:45 -0800

    more ugly hacks [ci skip]

--- QA/sleep_loopqa.sh
+++ QA/sleep_loopqa.sh
@@ -2,9 +2,18 @@
 #echo "starting sleep_loop.sh for command: " ${@}
 #"${@}"  &
 narg="${#}"
-if [ "$narg" -eq 7 ]; then
+if [ "$narg" -eq 10 ]; then
+ outfile=../testoutputs/$10
+ $1 $2 $3 $4 $5 $6 $7 $8 $9 >& $outfile &
+elif [ "$narg" -eq 9 ]; then
+ outfile=../testoutputs/$9
+ $1 $2 $3 $4 $5 $6 $7 $8    >& $outfile &
+elif [ "$narg" -eq 8 ]; then
+ outfile=../testoutputs/$8
+ $1 $2 $3 $4 $5 $6 $7       >& $outfile &
+elif [ "$narg" -eq 7 ]; then
  outfile=../testoutputs/$7
- $1 $2 $3 $4 $5 $6  >& $outfile &
+ $1 $2 $3 $4 $5 $6          >& $outfile &
 else
  outfile=../testoutputs/$6
  $1 $2 $3 $4 $5     >& $outfile &
