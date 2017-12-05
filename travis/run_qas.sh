b0d450b354edbbfe09a0782506c8357692fd1ab6
edoapra <edoardo.apra@gmail.com>
2017-12-04 16:45:31 -0800

    added prop_mep_gcube

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -9,7 +9,7 @@
    cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc
    cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs tce_ipccsd_f2 tce_eaccsd_ozone
  else
-   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs dft_he2+ 
+   cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs dft_he2+ prop_mep_gcube
    cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs h2o_opt cosmo_h2o_dft tddft_h2o h2o2-response
    cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs dft_siosi3
    cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs pspw pspw_md
