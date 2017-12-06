9d4379d8f68e26c5c93e88ba4d120f1f0a919ad5
Edoardo Aprà <edoardo.apra@gmail.com>
2017-12-05 22:08:11 -0800

    macos brew has scalapack only for ompi [ci skip]

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew cask uninstall oclint || true  ; fi
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then brew install gcc "$MPI_IMPL" scalapack openblas || true  ; fi
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
+  brew cask uninstall oclint || true  
+  brew install gcc "$MPI_IMPL"  openblas ||true
+  if [[ "$MPI_IMPL" == "openmpi" ]]; then
+     brew install scalapack
+  fi
+fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
     if [[ "$MPI_IMPL" == "openmpi" ]]; then
 	mpi_bin="openmpi-bin" ; mpi_libdev="libopenmpi-dev"
