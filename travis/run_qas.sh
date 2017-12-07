8c0facdebdc8bd96dde643b6eae2e1688558b50c
edoapra <edoardo.apra@gmail.com>
2017-12-06 19:54:41 -0800

    large qas only run when needed

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -1,17 +1,37 @@
 #!/bin/bash -f
 # source env. variables
  source $TRAVIS_BUILD_DIR/travis/nwchem.bashrc
- nprocs=2
  os=`uname`
- if [[ "$ARMCI_NETWORK" == "MPI-PR" ]]; then
-     nprocs=3
-    if [[ "$os" == "Darwin" && "$MPI_IMPL" == "openmpi" ]]; then 
-	export MPIRUN_NPOPT="-mca mpi_yield_when_idle 0 --oversubscribe -np "
-    fi
- fi
+ nprocs=2
+ do_largeqas=1
  if [[ "$os" == "Linux" && "$MPI_IMPL" == "mpich" ]]; then
     export MPIRUN_PATH=/usr/bin/mpirun.mpich
  fi
+ case "$ARMCI_NETWORK" in
+    MPI-PR)
+        nprocs=3
+        case "$os" in
+            Darwin)
+                do_largeqas=0
+		case "$MPI_IMPL" in
+		    openmpi)
+			export MPIRUN_NPOPT="-mca mpi_yield_when_idle 0 --oversubscribe -np "
+			;;
+		esac
+            ;;
+        esac
+	;;
+    MPI-MT)
+        do_largeqas=0
+        ;;
+    MPI3)
+        case "$os" in
+            Darwin)
+                do_largeqas=0
+		;;
+	esac
+	;;
+ esac
  if [[ "$NWCHEM_MODULES" == "tce" ]]; then
    cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs tce_n2 tce_ccsd_t_h2o tce_h2o_eomcc
    cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs tce_ipccsd_f2 tce_eaccsd_ozone
@@ -19,7 +39,7 @@
      cd $NWCHEM_TOP/QA && ./runtests.mpi.unix procs $nprocs dft_he2+ prop_mep_gcube
      cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs cosmo_h2o_dft  
      cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs pspw 
-     if [[ "$ARMCI_NETWORK" != "MPI-PT" ]] || [[ "$os" == "Darwin" && "$ARMCI_NETWORK" != "MPI-PR" ]]; then
+     if  [[ "$do_largeqas" == 1 ]]; then
        cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs dft_siosi3 h2o_opt
        cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs tddft_h2o h2o2-response
        cd $NWCHEM_TOP/QA && USE_SLEEPLOOP=1 ./runtests.mpi.unix procs $nprocs pspw_md
