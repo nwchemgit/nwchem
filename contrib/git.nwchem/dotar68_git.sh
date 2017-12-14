1816c11981512ae4eafcbad3cc996886f5d813f7
edoapra <edoardo.apra@gmail.com>
2017-12-14 15:40:08 -0800

    update [ci skip]

--- contrib/git.nwchem/dotar68_git.sh
+++ contrib/git.nwchem/dotar68_git.sh
@@ -2,8 +2,8 @@
 rm -rf temp.`date +%Y%m%d`
 mkdir -p temp.`date +%Y%m%d`
 cd temp.`date +%Y%m%d`
-NWVER='6.8-beta3'
-git clone -b release-6-8 https://github.com/nwchemgit/nwchem nwchem-6.8
+NWVER='6.8-release'
+git clone  -b release-6-8 https://github.com/nwchemgit/nwchem nwchem-6.8
 cd nwchem-6.8/src/tools
 ######REV68="$(svn info -r HEAD |grep Revision: |cut -c11-)"
 rm -f *tar*gz
@@ -21,11 +21,11 @@ echo 'revision ' $REV68
 pwd
 rm -f *md5 *tar*
 tar --exclude=".git" -czvf nwchem-"${NWVER}".revision-"${REV68}"-src.`date +%Y-%m-%d`.tar.gz nwchem-6.8/*
-md5sum nwchem-"${NWVER}".revision-"${REV68}"-src.`date +%Y-%m-%d`.tar.gz > nwchem-6.8.revision"${REV68}"-src.`date +%Y-%m-%d`.tar.gz.md5
+md5sum nwchem-"${NWVER}".revision-"${REV68}"-src.`date +%Y-%m-%d`.tar.gz > nwchem-"${NWVER}".revision"${REV68}"-src.`date +%Y-%m-%d`.tar.gz.md5
 tar --exclude=".git" -cjvf nwchem-"${NWVER}".revision-"${REV68}"-src.`date +%Y-%m-%d`.tar.bz2 nwchem-6.8/*
-md5sum nwchem-"${NWVER}".revision-"${REV68}"-src.`date +%Y-%m-%d`.tar.bz2 >  nwchem-6.8.revision"${REV68}"-src.`date +%Y-%m-%d`.tar.bz2.md5
+md5sum nwchem-"${NWVER}".revision-"${REV68}"-src.`date +%Y-%m-%d`.tar.bz2 >  nwchem-"${NWVER}".revision"${REV68}"-src.`date +%Y-%m-%d`.tar.bz2.md5
 tar --exclude=".git" -cjvf nwchem-"${NWVER}".revision-"${REV68}"-srconly.`date +%Y-%m-%d`.tar.bz2 nwchem-6.8/src/*
-md5sum nwchem-"${NWVER}".revision-"${REV68}"-srconly.`date +%Y-%m-%d`.tar.bz2>  nwchem-6.8.revision"${REV68}"-srconly.`date +%Y-%m-%d`.tar.bz2.md5
+md5sum nwchem-"${NWVER}".revision-"${REV68}"-srconly.`date +%Y-%m-%d`.tar.bz2>  nwchem-"${NWVER}".revision"${REV68}"-srconly.`date +%Y-%m-%d`.tar.bz2.md5
 tar --exclude=".git" --exclude="src" -cjvf nwchem-"${NWVER}".revision-"${REV68}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2 nwchem-6.8/*
-md5sum nwchem-"${NWVER}".revision-"${REV68}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2 > nwchem-6.8.revision"${REV68}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2.md5
+md5sum nwchem-"${NWVER}".revision-"${REV68}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2 > nwchem-"${NWVER}".revision"${REV68}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2.md5
 echo 'upload to http://192.101.105.206/'
