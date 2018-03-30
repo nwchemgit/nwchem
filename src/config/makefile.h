7587f4ffc8ece12a835b056624baae3555bdd01a
edoapra <edoardo.apra@gmail.com>
2018-03-29 19:30:29 -0700

    back to ifeq on USE_ARUR

--- src/config/makefile.h
+++ src/config/makefile.h
@@ -260,8 +260,8 @@ endif
 #      USE_ARUR = $(shell rm -f aru.tmp;ar -U > aru.tmp 2>&1; head -1 aru.tmp| awk ' /no\ operation/ {print "Y";exit};{print "N"}'; rm -f aru.tmp)
   USE_ARUR = $(shell rm -f aru.tmp;ar --help  > aru.tmp 2>&1; grep U aru.tmp| awk ' /ctual\ timest/ {print "Y";exit};'; rm -f aru.tmp)
   
-      ifdef USE_ARUR
-        ARFLAGS = rU
+      ifeq ($(USE_ARUR), "Y")
+        ARFLAGS = rU 
       endif
 
 # strip long paths when FC and/or CC are set from user
