f32bf073696ff83be613bf2711b41ef1d5d09c3e
Jeff Hammond <jeff.r.hammond@intel.com>
2018-09-20 01:54:19 -0500

    more work on test_cbody

--- src/ccsd/test_cbody.c
+++ src/ccsd/test_cbody.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MIN(x,y) (x<y ? x : y)
+#define MAX(x,y) (x>y ? x : y)
+
+#ifdef _OPENMP
+#include <omp.h>
+#else
+#warning No timer!
+double omp_get_wtime() { return 0.0; }
+#endif
+
 void ccsd_trpdrv_omp_cbody_(double * restrict f1n, double * restrict f1t,
                             double * restrict f2n, double * restrict f2t,
                             double * restrict f3n, double * restrict f3t,
@@ -20,7 +30,7 @@ double * make_array(int n)
 {
     double * a = malloc(n*sizeof(double));
     for (int i=0; i<n; i++) {
-        a[i] = 1.0/(10.0+i);
+        a[i] = 1.0/(100.0+i);
     }
     return a;
 }
@@ -28,14 +38,18 @@ double * make_array(int n)
 int main(int argc, char* argv[])
 {
     int ncor, nocc, nvir;
+    int maxiter = 100;
 
-    if (argc!=3) {
-        printf("Usage: ./test_cbody nocc nvir\n");
+    if (argc<3) {
+        printf("Usage: ./test_cbody nocc nvir [maxiter]\n");
         return argc;
     } else {
         ncor = 0;
         nocc = atoi(argv[1]);
         nvir = atoi(argv[2]);
+        if (argc>3) {
+            maxiter = atoi(argv[3]);
+        }
     }
 
     if (nocc<1 || nvir<1) {
@@ -43,7 +57,7 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    printf("Test driver for cbody with nocc=%d, nvir=%d\n", nocc, nvir);
+    printf("Test driver for cbody with nocc=%d, nvir=%d, maxiter=%d\n", nocc, nvir, maxiter);
 
     const int nbf = ncor + nocc + nvir;
     const int lnvv = nvir * nvir;
@@ -88,13 +102,73 @@ int main(int argc, char* argv[])
     double * t1v1   = make_array(lnvv);
     double * t1v2   = make_array(lnvv);
 
+    double * timers = calloc(nocc*nocc*nocc*nocc,sizeof(double));
+
     double emp4=0.0, emp5=0.0;
     int a=1, i=1, j=1, k=1, klo=1;
 
-    ccsd_trpdrv_omp_cbody_(f1n, f1t, f2n, f2t, f3n, f3t, f4n, f4t, eorb,
-                           &ncor, &nocc, &nvir, &emp4, &emp5, &a, &i, &j, &k, &klo,
-                           Tij, Tkj, Tia, Tka, Xia, Xka, Jia, Jka, Kia, Kka, Jij, Jkj, Kij, Kkj,
-                           dintc1, dintx1, t1v1, dintc2, dintx2, t1v2);
+    int iter = 0;
+
+    for (int klo=1; klo<=nocc; klo+=kchunk) {
+        const int khi = MIN(nocc, klo+kchunk-1);
+        int a=1;
+        for (int j=1; j<=nocc; j++) {
+            for (int i=1; i<=nocc; i++) {
+                for (int k=klo; k<=MIN(khi,i); k++) {
+                    double t0 = omp_get_wtime();
+                    ccsd_trpdrv_omp_cbody_(f1n, f1t, f2n, f2t, f3n, f3t, f4n, f4t, eorb,
+                                           &ncor, &nocc, &nvir, &emp4, &emp5, &a, &i, &j, &k, &klo,
+                                           Tij, Tkj, Tia, Tka, Xia, Xka, Jia, Jka, Kia, Kka, Jij, Jkj, Kij, Kkj,
+                                           dintc1, dintx1, t1v1, dintc2, dintx2, t1v2);
+                    double t1 = omp_get_wtime();
+                    timers[iter] = (t1-t0);
+                    iter++;
+                    if (iter==maxiter) {
+                        printf("Stopping after %d iterations...\n", iter);
+                        goto maxed_out;
+                    }
+
+                    /* prevent NAN for large maxiter... */
+                    if (emp4 >  1000.0) emp4 -= 1000.0;
+                    if (emp4 < -1000.0) emp4 += 1000.0;
+                    if (emp5 >  1000.0) emp5 -= 1000.0;
+                    if (emp5 < -1000.0) emp5 += 1000.0;
+                }
+            }
+        }
+    }
+
+maxed_out:
+    printf("");
+
+    double tsum =  0.0;
+    double tmax = -1.0e10;
+    double tmin =  1.0e10;
+    for (int i=0; i<iter; i++) {
+        //printf("timers[%d] = %f\n", i, timers[i]);
+        tsum += timers[i];
+        tmax  = MAX(tmax,timers[i]);
+        tmin  = MIN(tmin,timers[i]);
+    }
+    double tavg = tsum / iter;
+    printf("TIMING: min=%f, max=%f, avg=%f\n", tmin, tmax, tavg);
+
+    double dgemm_flops = ((8.0*nvir)*nvir)*(nvir+nocc);
+    double dgemm_mops  = 8.0*(4.0*nvir*nvir + 2.0*nvir*nocc);
+
+    /* The inner loop of tengy touches 24 f[1234][nt] elements and 8 other arrays...
+     * We will just assume flops=mops... */
+    double tengy_ops = ((1.0*nvir)*nvir)*(24+8);
+
+    printf("OPS: dgemm_flops=%10.3e dgemm_mops=%10.3e tengy_ops=%10.3e\n",
+            dgemm_flops, dgemm_mops, tengy_ops);
+    printf("PERF: GF/s=%10.3e GB/s=%10.3e\n",
+            1.0e-9*(dgemm_flops+tengy_ops)/tavg, 8.0e-9*(dgemm_mops+tengy_ops)/tavg);
+
+    printf("These are meaningless but should be constant with fixed (nocc,nvir,maxiter):\n");
+    printf("emp4=%f emp5=%f\n", emp4, emp5);
+
+    printf("SUCCESS\n");
 
     return 0;
 }
