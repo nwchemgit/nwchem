0e011cc45f77f32c23531351f873027549e43abd
Eric Hermes <ehermes@sandia.gov>
2018-10-08 10:59:30 -0700

    Initial Python2/3 support

--- src/config/makefile.h
+++ src/config/makefile.h
@@ -1324,7 +1324,12 @@ ifeq ($(BUILDING_PYTHON),python)
 #   EXTRA_LIBS += -L/home/edo/tcltk/lib/LINUX -ltk8.3 -ltcl8.3 -L/usr/X11R6/lib -lX11 -ldl
 # needed if python was built with pthread support
   ifneq ($(GOTMINGW32),1)
+   PYMAJOR = $(echo $PYTHONVERSION | cut -d. -f1)
+   ifeq (${PYMAJOR},3)
+   EXTRA_LIBS += $(shell $(PYTHONHOME)/bin/python3-config --libs)  -lnwcutil
+   else
    EXTRA_LIBS += $(shell $(PYTHONHOME)/bin/python-config --libs)  -lnwcutil
+   endif
   endif
 endif
 
@@ -2388,7 +2393,12 @@ ifeq ($(_CPU),$(findstring $(_CPU), ppc64 ppc64le))
 
      ifeq ($(BUILDING_PYTHON),python)
 #   EXTRA_LIBS += -ltk -ltcl -L/usr/X11R6/lib -lX11 -ldl
+   PYMAJOR = $(echo $PYTHONVERSION | cut -d. -f1)
+   ifeq (${PYMAJOR},3)
+   EXTRA_LIBS += -lnwcutil $(shell $(PYTHONHOME)/bin/python3-config --libs) #-lz
+   else
    EXTRA_LIBS += -lnwcutil $(shell $(PYTHONHOME)/bin/python-config --libs) #-lz
+   endif
   LDOPTIONS += -Wl,--export-dynamic 
      endif
 ifeq ($(NWCHEM_TARGET),CATAMOUNT)
@@ -2558,7 +2568,12 @@ errorpython2:
 endif
 #
 ifdef USE_PYTHONCONFIG
+PYMAJOR = $(echo $PYTHONVERSION | cut -d. -f1)
+ifeq (${PYMAJOR},3)
+EXTRA_LIBS += $(shell $(PYTHONHOME)/bin/python3-config --ldflags)
+else
 EXTRA_LIBS += $(shell $(PYTHONHOME)/bin/python-config --ldflags)
+endif
 else
 ifndef PYTHONLIBTYPE
 	PYTHONLIBTYPE=a
