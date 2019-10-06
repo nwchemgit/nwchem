7f4f0b9437a9cc6f2823deb1339039ff49ae53a9
edoapra <edoardo.apra@gmail.com>
2019-10-05 17:19:34 -0700

    added BUILD_OPENBLAS and BUILD_SCALAPACK to download and build libraries [ci skip]

---
+++ src/libext/openblas/build_openblas.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+VERSION=0.3.7
+rm -rf OpenBLAS*
+curl -LJO https://github.com/xianyi/OpenBLAS/archive/v${VERSION}.tar.gz
+tar xzf OpenBLAS-${VERSION}.tar.gz
+ln -sf OpenBLAS-${VERSION} OpenBLAS
+cd OpenBLAS-${VERSION}
+if [[ ! -z "${USE_64TO32}" ]]; then
+  sixty4_int=0
+else
+  sixty4_int=1
+fi
+make  INTERFACE64="$sixty4_int" USE_THREAD=0 NO_CBLAS=1 NO_LAPACKE=1 DEBUG=0 NUM_THREADS=1 libs netlib
+mkdir -p ../../lib
+cp libopenbla*.* ../../lib
+#make PREFIX=. install
+#tail -4 openblas.log
