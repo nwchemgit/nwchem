3c53a012668da725cecbd6d83540968f184ebe4a
edoapra <edoardo.apra@gmail.com>
2019-10-07 16:13:14 -0700

    xlf changes

--- src/libext/scalapack/build_scalapa.sh
+++ src/libext/scalapack/build_scalapa.sh
@@ -7,7 +7,9 @@ if [[ -z "$USE_64TO32"   ]] ; then
     echo USE_64TO32 must be set
     exit 1
 fi
-os=`uname`
+if [[ ! -z "$BUILD_OPENBLAS"   ]] ; then
+    BLASOPT="-L../../../build -lopenblas"
+fi
 #git clone https://github.com/scibuilder/scalapack.git
 #svn co --non-interactive --trust-server-cert https://icl.utk.edu/svn/scalapack-dev/scalapack/trunk/ scalapack
 rm -rf scalapack*
@@ -17,7 +19,10 @@ tar xzf scalapack.tgz
 ln -sf scalapack-${VERSION} scalapack
 mkdir -p scalapack/build
 cd scalapack/build
-cmake -Wno-dev ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTEST_SCALAPACK=OFF  -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF  -DBLAS_openblas_LIBRARY=$BLASOPT
+if  [ ${FC} == xlf ] || [ ${FC} == xlf_r ] || [ ${FC} == xlf90 ]|| [ ${FC} == xlf90_r ]; then
+    Fortran_FLAGS=" -qintsize=4 -qextname "
+fi
+FFLAGS="$Fortran_FLAGS" cmake -Wno-dev ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_Fortran_FLAGS="$Fortran_FLAGS" -DTEST_SCALAPACK=OFF  -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF  -DBLAS_openblas_LIBRARY="$BLASOPT"  -DBLAS_LIBRARIES="$BLASOPT"  -DLAPACK_openblas_LIBRARY="$BLASOPT"  -DLAPACK_LIBRARIES="$BLASOPT" 
 make V=0 -j3 scalapack/fast
 mkdir -p ../../../lib
 cp lib/libscalapack.a ../../../lib
