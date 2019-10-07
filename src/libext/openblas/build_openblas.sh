23a18beb3e2c59f77c9a6c8a40c4b91f5f604df7
edoapra <edoardo.apra@gmail.com>
2019-10-07 16:13:14 -0700

    xlf changes

--- src/libext/openblas/build_openblas.sh
+++ src/libext/openblas/build_openblas.sh
@@ -5,7 +5,7 @@ curl -LJO https://github.com/xianyi/OpenBLAS/archive/v${VERSION}.tar.gz
 tar xzf OpenBLAS-${VERSION}.tar.gz
 ln -sf OpenBLAS-${VERSION} OpenBLAS
 cd OpenBLAS-${VERSION}
-if [[ ! -z "${USE_64TO32}" ]]; then
+if [[ "${BLAS_SIZE}" == "8" ]]; then
   sixty4_int=0
 else
   sixty4_int=1
@@ -16,8 +16,11 @@ if [[ "${NWCHEM_TARGET}" == "LINUX" ]]; then
 else
   binary=64
 fi
-make  INTERFACE64="$sixty4_int" BINARY="$binary" USE_THREAD=0 NO_CBLAS=1 NO_LAPACKE=1 DEBUG=0 NUM_THREADS=1 libs netlib
+if [ ${FC} == xlf ] || [ ${FC} == xlf_r ] || [ ${FC} == xlf90 ]|| [ ${FC} == xlf90_r ]; then
+ make CC=gcc FC="xlf -qextname"  INTERFACE64="$sixty4_int" BINARY="$binary" USE_THREAD=0 NO_CBLAS=1 NO_LAPACKE=1 DEBUG=0 NUM_THREADS=1 libs netlib
+else
+ make  INTERFACE64="$sixty4_int" BINARY="$binary" USE_THREAD=0 NO_CBLAS=1 NO_LAPACKE=1 DEBUG=0 NUM_THREADS=1 libs netlib
+fi
 mkdir -p ../../lib
 cp libopenbla*.* ../../lib
 #make PREFIX=. install
-#tail -4 openblas.log
