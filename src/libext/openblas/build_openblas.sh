e147b6ba1f1ce1a7df270d1131314e18e32e92e3
edoapra <edoardo.apra@gmail.com>
2019-10-07 22:47:47 -0700

    wrong blas_size assignment

--- src/libext/openblas/build_openblas.sh
+++ src/libext/openblas/build_openblas.sh
@@ -5,10 +5,10 @@ curl -LJO https://github.com/xianyi/OpenBLAS/archive/v${VERSION}.tar.gz
 tar xzf OpenBLAS-${VERSION}.tar.gz
 ln -sf OpenBLAS-${VERSION} OpenBLAS
 cd OpenBLAS-${VERSION}
-if [[ "${BLAS_SIZE}" == "8" ]]; then
-  sixty4_int=0
-else
+if [[ ${BLAS_SIZE} == 8 ]]; then
   sixty4_int=1
+else
+  sixty4_int=0
 fi
 if [[ "${NWCHEM_TARGET}" == "LINUX" ]]; then
   binary=32
@@ -16,7 +16,7 @@ if [[ "${NWCHEM_TARGET}" == "LINUX" ]]; then
 else
   binary=64
 fi
-if [ ${FC} == xlf ] || [ ${FC} == xlf_r ] || [ ${FC} == xlf90 ]|| [ ${FC} == xlf90_r ]; then
+if [[ -z ${FC} ]] &&  [[ ${FC} == xlf ]] || [[ ${FC} == xlf_r ]] || [[ ${FC} == xlf90 ]]|| [[ ${FC} == xlf90_r ]]; then
  make CC=gcc FC="xlf -qextname"  INTERFACE64="$sixty4_int" BINARY="$binary" USE_THREAD=0 NO_CBLAS=1 NO_LAPACKE=1 DEBUG=0 NUM_THREADS=1 libs netlib
 else
  make  INTERFACE64="$sixty4_int" BINARY="$binary" USE_THREAD=0 NO_CBLAS=1 NO_LAPACKE=1 DEBUG=0 NUM_THREADS=1 libs netlib
