072f294dc8ff5ab7b63b394f009421eb3b36d8b7
edoapra <edoardo.apra@gmail.com>
2019-10-25 18:27:05 -0700

    added pbe96x stub routine with errquit

---
+++ src/nwpw/pspw/lib/exchange-correlation/pbe96x.f
@@ -0,0 +1,16 @@
+      subroutine gen_PBE96_x_unrestricted(ndn,agrdn,
+     >                                xedn_pbe,fdnxdn_pbe,fdagrxdn_pbe)
+      implicit none
+      real*8 ndn,agrdn
+      real*8 xedn_pbe,fdnxdn_pbe,fdagrxdn_pbe
+      call errquit(' gen_PBE96_x_unrestricted not coded yet ',0,0)
+      return
+      end
+      subroutine gen_PBE96_x_restricted(n,agr,
+     >     xe_pbe,fdnx_pbe,fdagrx_pbe)
+      implicit none
+      real*8 n,agr
+      real*8 xe_pbe,fdnx_pbe,fdagrx_pbe
+      call errquit(' gen_PBE96_x_restricted not coded yet ',0,0)
+      return
+      end
