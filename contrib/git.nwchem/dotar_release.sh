040faafa1b32d25b849f96072f33705354f4d630
edoapra <edoardo.apra@gmail.com>
2019-11-01 23:05:42 -0700

    update [ci skip]

--- contrib/git.nwchem/dotar_release.sh
+++ contrib/git.nwchem/dotar_release.sh
@@ -1,10 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
+Vx=7
+Vy=0
+Vz=0
+NWVER="$Vx"."$Vy"."$Vz"-release
+TOPDIR=nwchem-"$Vx"."$Vy"."$Vz"
+BRANCH=release-"$Vx"-"$Vy"-"$Vz"
+# need to change BRANCH for patch releases
 rm -rf temp.`date +%Y%m%d`
 mkdir -p temp.`date +%Y%m%d`
 cd temp.`date +%Y%m%d`
-NWVER='7.0.0-release'
-TOPDIR="nwchem-7.0.0"
-git clone --depth 1 --shallow-submodules -b release-7-0-0 https://github.com/nwchemgit/nwchem $TOPDIR
+git clone --depth 1 --shallow-submodules -b $BRANCH https://github.com/nwchemgit/nwchem $TOPDIR
 cd $TOPDIR/src/tools
 rm -f *tar*gz
 ./get-tools-github
@@ -12,6 +17,7 @@ cd ../util
 ./util_ga_version.bash
 ./util_nwchem_version.bash
 cd ..
+export NWCHEM_MODULES=all\ python
 make nwchem_config NWCHEM_MODULES=all\ python
 export EACCSD=1
 export IPCCSD=1
@@ -23,17 +29,18 @@ make 64_to_32   USE_INTERNALBLAS=y
 #rm `find peigs -name peigs_stamp.*`
 cd ..
 rm -rf bin lib
-REV700="$(git describe)"
+REVGIT="$(git describe --always)"
 cd ..
-echo 'revision ' $REV700
+echo 'revision ' $REVGIT
 pwd
 rm -f *md5 *tar*
-tar --exclude=".git" -czvf nwchem-"${NWVER}".revision-"${REV700}"-src.`date +%Y-%m-%d`.tar.gz $TOPDIR/*
-md5sum nwchem-"${NWVER}".revision-"${REV700}"-src.`date +%Y-%m-%d`.tar.gz > nwchem-"${NWVER}".revision"${REV700}"-src.`date +%Y-%m-%d`.tar.gz.md5
-tar --exclude=".git" -cjvf nwchem-"${NWVER}".revision-"${REV700}"-src.`date +%Y-%m-%d`.tar.bz2 $TOPDIR/*
-md5sum nwchem-"${NWVER}".revision-"${REV700}"-src.`date +%Y-%m-%d`.tar.bz2 >  nwchem-"${NWVER}".revision"${REV700}"-src.`date +%Y-%m-%d`.tar.bz2.md5
-tar --exclude=".git" -cjvf nwchem-"${NWVER}".revision-"${REV700}"-srconly.`date +%Y-%m-%d`.tar.bz2 $TOPDIR/src/*
-md5sum nwchem-"${NWVER}".revision-"${REV700}"-srconly.`date +%Y-%m-%d`.tar.bz2>  nwchem-"${NWVER}".revision"${REV700}"-srconly.`date +%Y-%m-%d`.tar.bz2.md5
-tar --exclude=".git" --exclude="src" -cjvf nwchem-"${NWVER}".revision-"${REV700}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2 $TOPDIR/*
-md5sum nwchem-"${NWVER}".revision-"${REV700}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2 > nwchem-"${NWVER}".revision"${REV700}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2.md5
+tar --exclude=".git" -czf nwchem-"${NWVER}".revision-"${REVGIT}"-src.`date +%Y-%m-%d`.tar.gz $TOPDIR/*
+md5sum nwchem-"${NWVER}".revision-"${REVGIT}"-src.`date +%Y-%m-%d`.tar.gz > nwchem-"${NWVER}".revision"${REVGIT}"-src.`date +%Y-%m-%d`.tar.gz.md5
+tar --exclude=".git" -cjf nwchem-"${NWVER}".revision-"${REVGIT}"-src.`date +%Y-%m-%d`.tar.bz2 $TOPDIR/*
+md5sum nwchem-"${NWVER}".revision-"${REVGIT}"-src.`date +%Y-%m-%d`.tar.bz2 >  nwchem-"${NWVER}".revision"${REVGIT}"-src.`date +%Y-%m-%d`.tar.bz2.md5
+tar --exclude=".git" -cjf nwchem-"${NWVER}".revision-"${REVGIT}"-srconly.`date +%Y-%m-%d`.tar.bz2 $TOPDIR/src/*
+md5sum nwchem-"${NWVER}".revision-"${REVGIT}"-srconly.`date +%Y-%m-%d`.tar.bz2>  nwchem-"${NWVER}".revision"${REVGIT}"-srconly.`date +%Y-%m-%d`.tar.bz2.md5
+tar --exclude=".git" --exclude="src" -cjf nwchem-"${NWVER}".revision-"${REVGIT}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2 $TOPDIR/*
+md5sum nwchem-"${NWVER}".revision-"${REVGIT}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2 > nwchem-"${NWVER}".revision"${REVGIT}"-nonsrconly.`date +%Y-%m-%d`.tar.bz2.md5
+ls -lrt
 echo 'upload to http://192.101.105.206/'
