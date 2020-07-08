d79f999d4f21c5f790acde28fad9d10e0ffd454d
edoapra <edoardo.apra@gmail.com>
2020-07-08 14:57:19 -0700

    use python@3.8 on mac for travis

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
   brew cask uninstall oclint || true  
-  brew install gcc "$MPI_IMPL" openblas python@3 ||true
+  brew install gcc "$MPI_IMPL" openblas python@3.8 ||true
   if [[ "$MPI_IMPL" == "openmpi" ]]; then
      brew install scalapack
   fi
