a3b71430f66190ca5246a49032ffb5e84d0931b8
edoapra <edoardo.apra@gmail.com>
2020-12-03 14:10:51 -0800

    BUILD_OPENBLAS and BUILD_SCALAPACK on by default in travis

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then 
 #  HOMEBREW_NO_AUTO_UPDATE=1 brew cask uninstall oclint || true  
-  HOMEBREW_NO_INSTALL_CLEANUP=1  HOMEBREW_NO_AUTO_UPDATE=1 brew install gcc "$MPI_IMPL" openblas python@3.7 ||true
-  if [[ "$MPI_IMPL" == "openmpi" ]]; then
-      HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_AUTO_UPDATE=1 brew install scalapack
-  fi
+#  HOMEBREW_NO_INSTALL_CLEANUP=1  HOMEBREW_NO_AUTO_UPDATE=1 brew install gcc "$MPI_IMPL" openblas python3 ||true
+  HOMEBREW_NO_INSTALL_CLEANUP=1  HOMEBREW_NO_AUTO_UPDATE=1 brew install gcc "$MPI_IMPL" python3 ||true
+#  if [[ "$MPI_IMPL" == "openmpi" ]]; then
+#      HOMEBREW_NO_INSTALL_CLEANUP=1 HOMEBREW_NO_AUTO_UPDATE=1 brew install scalapack
+#  fi
 fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then 
     if [[ "$MPI_IMPL" == "openmpi" ]]; then
@@ -15,5 +16,6 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     fi
     cat /etc/apt/sources.list
     sudo add-apt-repository universe && sudo apt update
-    sudo apt-get -y install gfortran python3-dev python-dev cmake "$mpi_libdev" "$mpi_bin" "$scalapack_libdev"  make perl  libopenblas-dev python3 rsync
+#    sudo apt-get -y install gfortran python3-dev python-dev cmake "$mpi_libdev" "$mpi_bin" "$scalapack_libdev"  make perl  libopenblas-dev python3 rsync
+    sudo apt-get -y install gfortran python3-dev python-dev cmake "$mpi_libdev" "$mpi_bin"  make perl  python3 rsync
 fi
