6385739f9de1d5eb0aa85afbf1068c8980aef955
edoapra <edoardo.apra@gmail.com>
2020-12-23 18:49:17 -0800

    work with FC=gfortran-xy

--- src/libext/scalapack/build_scalapa.sh
+++ src/libext/scalapack/build_scalapa.sh
@@ -102,14 +102,14 @@ else
     rm -f scalapack-$COMMIT.zip
     curl -L https://github.com/Reference-ScaLAPACK/scalapack/archive/$COMMIT.zip -o scalapack-$COMMIT.zip
 fi
-unzip -q scalapack-$COMMIT.zip
+unzip -n -q scalapack-$COMMIT.zip
 ln -sf scalapack-$COMMIT scalapack
 #ln -sf scalapack-${VERSION} scalapack
 #curl -L http://www.netlib.org/scalapack/scalapack-${VERSION}.tgz -o scalapack.tgz
 #tar xzf scalapack.tgz
 cd scalapack
 if [[  -z "$USE_DCOMBSSQ" ]]; then
-    patch -p0 < ../dcombssq.patch
+    patch -p0 -s -N < ../dcombssq.patch
 fi
 #curl -LJO https://github.com/Reference-ScaLAPACK/scalapack/commit/189c84001bcd564296a475c5c757afc9f337e828.patch
 #patch -p1 < 189c84001bcd564296a475c5c757afc9f337e828.patch
@@ -131,8 +131,10 @@ GOTCLANG=$( mpicc -dM -E - </dev/null 2> /dev/null |grep __clang__|head -1|cut -
 if [[ ${GOTCLANG} == "1" ]] ; then
     C_FLAGS=" -Wno-error=implicit-function-declaration "
 fi
+
 if [[  "$SCALAPACK_SIZE" == 8 ]] ; then
-    if  [[ ${FC} == gfortran ]] || [[ ${FC} == f95 ]]  ; then
+    GFORTRAN_EXTRA=$(echo $FC | cut -c 1-8)
+    if  [[ ${FC} == gfortran ]] || [[ ${FC} == f95 ]] || [[ ${GFORTRAN_EXTRA} == gfortran ]] ; then
     Fortran_FLAGS+=" -fdefault-integer-8 "
     elif  [[ ${FC} == xlf ]] || [[ ${FC} == xlf_r ]] || [[ ${FC} == xlf90 ]]|| [[ ${FC} == xlf90_r ]]; then
     Fortran_FLAGS=" -qintsize=8 -qextname "
