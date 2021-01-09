a6c53da946a6645a88f58aea47b6bd29bb4f1108
edoapra <edoardo.apra@gmail.com>
2021-01-08 23:11:30 -0800

    ifort compiler

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -34,9 +34,22 @@ fi
     if [[ "$MPI_IMPL" == "mpich" ]]; then
         mpi_bin="mpich" ; mpi_libdev="libmpich-dev" scalapack_libdev="libscalapack-mpich-dev"
     fi
-    cat /etc/apt/sources.list
+    if [[ "$MPI_IMPL" == "intel" ]]; then
+	wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB \
+            && sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB  \
+            && rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB  \
+            && echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list \
+            && sudo add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"  \
+	    && sudo apt update
+	sudo apt-cache search intel-oneapi-mpi
+        mpi_bin="  " ; mpi_libdev="intel-oneapi-mpi-devel" scalapack_libdev="intel-oneapi-mkl"
+    fi
     sudo add-apt-repository universe && sudo apt update
 #    sudo apt-get -y install gfortran python3-dev python-dev cmake "$mpi_libdev" "$mpi_bin" "$scalapack_libdev"  make perl  libopenblas-dev python3 rsync
     sudo apt-get -y install gfortran python3-dev python-dev cmake "$mpi_libdev" "$mpi_bin"  make perl  python3 rsync
+    if [[ "$FC" == "ifort" ]]; then
+	sudo apt-get -y install intel-oneapi-ifort intel-oneapi-compiler-dpcpp-cpp-and-cpp-classic  intel-oneapi-mkl
+	sudo apt-get -y install intel-oneapi-mpi-devel
+    fi
     fi
 fi
--- travis/compile_nwchem.sh
+++ travis/compile_nwchem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "start compile"
-set -ev
+set -e
 # source env. variables
 if [[ -z "$TRAVIS_BUILD_DIR" ]] ; then
     TRAVIS_BUILD_DIR=$(pwd)
@@ -27,7 +27,19 @@ if [[ "$arch" == "aarch64" ]]; then
 	FOPT="-O1 -fno-aggressive-loop-optimizations"
     fi
 else
-    FOPT="-O2 -fno-aggressive-loop-optimizations  -ffast-math"
+    if [[ "$FC" == "ifort" ]] ; then
+	FOPT=-O2
+	export USE_FPICF=Y
+	export BLASOPT=" -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core  -lpthread -lm -ldl"
+	export LAPACK_LIB=" -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core  -lpthread -lm -ldl"
+	export SCALAPACK_LIB=" -lmkl_scalapack_ilp64 -lmkl_blacs_intelmpi_ilp64 -lpthread -lm -ldl"
+	export BLAS_SIZE=8
+	export SCALAPACK_SIZE=8
+	unset BUILD_OPENBLAS
+	unset BUILD_SCALAPACK
+    else
+	FOPT="-O2 -fno-aggressive-loop-optimizations  -ffast-math"
+    fi
 fi    
  if [[ "$os" == "Darwin" ]]; then 
    if [[ "$NWCHEM_MODULES" == "tce" ]]; then
@@ -39,7 +51,8 @@ fi
        export PATH="/usr/local/bin:$PATH"
 #       export LDFLAGS="-L/usr/local/opt/python@3.7/lib:$LDFLAGS"
    fi
-if [[ -z "$TRAVIS_HOME" ]]; then
+   if [[ -z "$TRAVIS_HOME" ]]; then
+       env
     make V=0 FOPTIMIZE="$FOPT" FDEBUG="$FDOPT"  -j3
 else
     ../travis/sleep_loop.sh make V=1 FOPTIMIZE="$FOPT" FDEBUG="$FDOPT"  -j3
