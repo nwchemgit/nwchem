d473b7f87d3ea8397cb67bc53cc0cdbafbe1a145
edoapra <edoardo.apra@gmail.com>
2021-04-13 10:04:20 -0700

    nvhpc version 21.3

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 os=`uname`
-arch=`uname -m`
 dist="ubuntu"
 if test -f "/usr/lib/os-release"; then
     dist=$(grep ID= /etc/os-release |head -1 |cut -c4-| sed 's/\"//g')
@@ -83,9 +82,15 @@ fi
     fi
     if [[ "$FC" == "nvfortran" ]]; then
 	sudo apt-get -y install lmod
-	wget https://developer.download.nvidia.com/hpc-sdk/21.2/nvhpc-21-2_21.2_amd64.deb
-	wget https://developer.download.nvidia.com/hpc-sdk/21.2/nvhpc-2021_21.2_amd64.deb
-	sudo dpkg -i nvhpc-21-2_21.2_amd64.deb nvhpc-2021_21.2_amd64.deb
+	nv_major=21
+	nv_minor=3
+	nverdot="$nv_major"."$nv_minor"
+	nverdash="$nv_major"-"$nv_minor"
+	nv_p1=nvhpc-"$nverdash"_"$nverdot"_amd64.deb
+	nv_p2=nvhpc-2021_"$nverdot"_amd64.deb
+	wget https://developer.download.nvidia.com/hpc-sdk/"$nverdot"/"$nv_p1"
+	wget https://developer.download.nvidia.com/hpc-sdk/"$nverdot"/"$nv_p2"
+	sudo dpkg -i "$nv_p1" "$nv_p2"
 	source /etc/profile.d/lmod.sh
         module use /opt/nvidia/hpc_sdk/modulefiles
 	module load nvhpc
