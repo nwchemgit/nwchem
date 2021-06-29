44e02723f028be608233bc9011c35b2405e019df
edoapra <edoardo.apra@gmail.com>
2021-06-29 16:46:38 -0700

    updates for case when FC and/or CC are not set

--- src/libext/libxc/build_libxc.sh
+++ src/libext/libxc/build_libxc.sh
@@ -3,14 +3,30 @@
 VERSION=5.1.5
 
 if [[ -f "libxc-${VERSION}.tar.gz" ]]; then
-    echo "using exisiting  libxc-${VERSION}.tar.gz"
+    echo "using existing  libxc-${VERSION}.tar.gz"
 else
     echo "downloading libxc-${VERSION}.tar.gz"
     curl -L https://gitlab.com/libxc/libxc/-/archive/${VERSION}/libxc-${VERSION}.tar.gz -o libxc-${VERSION}.tar.gz
 fi
 
 tar -xzf libxc-${VERSION}.tar.gz
-ln -s libxc-${VERSION} libxc
+ln -sf libxc-${VERSION} libxc
+
+if [[  -z "${CC}" ]]; then
+    CC=cc
+fi
+if [[  -z "${FC}" ]]; then
+#FC not defined. Look for gfortran
+    if [[ ! -x "$(command -v gfortran)" ]]; then
+	echo ' '
+	echo 'please define FC to compile libxc'
+	echo ' '
+	exit 1
+    else
+	echo 'FC not defined, defaulting FC=gfortran'
+	FC=gfortran
+    fi
+fi
 
 cd libxc
 mkdir build
