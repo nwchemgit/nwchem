04a2a6ec94c3a969655454e7ecf92d05b202a6d5
edoapra <edoardo.apra@gmail.com>
2021-10-29 13:50:59 -0700

    fixes for flake8

--- src/tce/splitfiles.py
+++ src/tce/splitfiles.py
@@ -1,33 +1,31 @@
 # Usage: python splitfiles.py < inputfile.F
 # (c) All rights reserved by Battelle & Pacific Northwest Nat'l Lab (2002)
 # $Id$
- 
-#import string
-import copy
+
 import sys
-import string
 
 source = sys.stdin.readlines()
 
 if (not source):
-   print ("Usage: python splitfiles.py < inputfile.F")
+    print("Usage: python splitfiles.py < inputfile.F")
 
 nfiles = 0
 filename = " "
+filecontent = " "
 for line in source:
    if (line.find("SUBROUTINE") != -1):
       if (nfiles):
-         file = open(filename+".F","w")
+         file = open(filename+".F", "w")
          for newline in filecontent:
             file.write(newline)
-      filename = line[line.find("SUBROUTINE")+11:].split("(",99999)[0]
-      print (filename+".o\\")
+      filename = line[line.find("SUBROUTINE")+11:].split("(", 99999)[0]
+      print(filename+".o\\")
       nfiles = nfiles + 1
       filecontent = [line]
    else:
       filecontent.append(line)
 # don't forget to dump the last subroutine
-file = open(filename+".F","w")
+file = open(filename+".F", "w")
 for newline in filecontent:
    file.write(newline)
-print ("Number of files generated:",nfiles)
+print("Number of files generated:", nfiles)
