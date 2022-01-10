cc8090ab215bd1892b807d595c6b69c0a40bdc3b
edoapra <edoardo.apra@gmail.com>
2022-01-10 13:40:10 -0800

    fix file name

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -36,7 +36,7 @@ esac
 	cd ~/Downloads
 	dir_base="18342"
 	dir_hpc="18341"
-	base="m_BaseKit_p_2022.1.92_offline"
+	base="m_BaseKit_p_2022.1.0.92_offline"
 	hpc="m_HPCKit_p_2022.1.0.86_offline"
 	curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/"$dir_base"/"$base".dmg
 	curl -LJO https://registrationcenter-download.intel.com/akdlm/irc_nas/"$dir_hpc"/"$hpc".dmg
