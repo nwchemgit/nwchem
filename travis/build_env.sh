e46f64d511693acf098c936a67e722ee01776ef4
edoapra <edoardo.apra@gmail.com>
2022-01-14 14:21:47 -0800

    extra pkg for rocm

--- travis/build_env.sh
+++ travis/build_env.sh
@@ -157,7 +157,7 @@ fi
 	rocm_version=4.5.2
 	wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key |  sudo apt-key add -
 	echo 'deb [arch=amd64] https://repo.radeon.com/rocm/apt/'$rocm_version'/ ubuntu main' | sudo tee /etc/apt/sources.list.d/rocm.list
-	sudo apt-get  update -y && sudo apt-get -y install rocm-llvm
+	sudo apt-get  update -y && sudo apt-get -y install rocm-llvm openmp-extras
 	export PATH=/opt/rocm-"$rocm_version"/bin:$PATH
 	export LD_LIBRARY_PATH=/opt/rocm-"$rocm_version"/lib:/opt/rocm-"$rocm_version"/llvm/lib:$LD_LIBRARY_PATH
 	amdflang -v
