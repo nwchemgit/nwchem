1439a93d52c61e0575b170a8f3698e9c9a1c12a8
edoapra <edoardo.apra@gmail.com>
2022-02-04 11:34:39 -0800

    fix

--- src/config/64all.sh
+++ src/config/64all.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # convert all source files
-find ${NWCHEM_TOP}/src \
-     -type d \( -path tools -o -path config  \
-  -o -path lapack  -o -path blas  -o -path 64to32blas \
-     -o -path libext -o -name libsimint_source \) -prune  \
+find ${NWCHEM_TOP}/src/NWints \
+	 -type d \( -name "blas*" \
+	 -o -name "lapack*" \
+	 -o -name "config*" \
+	 -o -name "libext*" \
+	 -o -name "tools*" \
+	 -o -name "lapack*" \
+	 -o -name "libsimint_source*" \
+	 -o -name "64to32blas*" \) -prune \
 -o -type f \( -iname "*.f"  -o  -iname "*.c"  \) \
-  | xargs -n 50 ${NWCHEM_TOP}/src/config/64_to_32 
+  | xargs -n 50 ${NWCHEM_TOP}/src/config/64_to_32
