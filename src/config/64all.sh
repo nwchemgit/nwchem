fb9d7d15a24e38da97846659bb91df6c624be70f
edoapra <edoardo.apra@gmail.com>
2022-02-04 09:21:53 -0800

    skip conversion for 64to32blas directory

--- src/config/64all.sh
+++ src/config/64all.sh
@@ -2,7 +2,7 @@
 # convert all source files
 find ${NWCHEM_TOP}/src \
      -type d \( -path tools -o -path config  \
-  -o -path lapack  -o -path blas \
+  -o -path lapack  -o -path blas  -o -path 64to32blas \
      -o -path libext -o -name libsimint_source \) -prune  \
 -o -type f \( -iname "*.f"  -o  -iname "*.c"  \) \
   | xargs -n 50 ${NWCHEM_TOP}/src/config/64_to_32 
