18ef52740375f9193f848160467cdc26def78127
Jeff Hammond <jeff.science@gmail.com>
2022-03-07 12:27:16 +0200

    fix preprocessor logic
    Signed-off-by: Jeff Hammond <jeff.science@gmail.com>

--- src/gwmol/mycpu.c
+++ src/gwmol/mycpu.c
@@ -1,5 +1,7 @@
-#if defined(__x86_64__) && ( defined (MACX) || defined (MACX64) )
+#if defined (MACX) || defined (MACX64)
+  #if defined(__x86_64__)
     #include <cpuid.h>
+  #endif
 #else
     // sched_getcpu is non-standard and may only be found in glibc,
     // so this code is not portable to Alpine Linux, BSD,
@@ -12,15 +14,17 @@ int findmycpu_ ()
 {
   int cpu = -1;
 
-#if defined(__x86_64__) && ( defined (MACX) || defined (MACX64) )
-  int cpuinfo[4];
-  __cpuid_count(1, 0, cpuinfo[0], cpuinfo[1], cpuinfo[2], cpuinfo[3]);
-  if ( (cpuinfo[3] & (1 << 9)) == 0 ) {
-    cpu = -1;
-  }
-  else {
-    cpu = (unsigned) cpuinfo[1] >> 24;
-  }
+#if defined (MACX) || defined (MACX64)
+  #if defined(__x86_64__)
+    int cpuinfo[4];
+    __cpuid_count(1, 0, cpuinfo[0], cpuinfo[1], cpuinfo[2], cpuinfo[3]);
+    if ( (cpuinfo[3] & (1 << 9)) == 0 ) {
+      cpu = -1;
+    }
+    else {
+      cpu = (unsigned) cpuinfo[1] >> 24;
+    }
+  #endif
 #else
   cpu = sched_getcpu();
 #endif
