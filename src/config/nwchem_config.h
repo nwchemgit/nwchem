611e9ea85418d0f536d1d38d7f3319ee3b93a493
Daniel Mejia-Rodriguez <daniel.mejia@pnnl.gov>
2022-04-05 16:51:51 -0400

    Input changes

--- src/config/nwchem_config.h
+++
@@ -1,6 +0,0 @@
-# This configuration generated automatically on dmejiar-VirtualBox at Fri Apr 1 06:37:31 PM EDT 2022
-# Request modules from user: smallqm xtb
-NW_MODULE_SUBDIRS = NWints atomscf ddscf gradients moints nwdft rimp2 stepper driver optim cphf vib mcscf esp hessian dplot mp2_grad qhop property solvation fft nwmd cafe space vscf qmd etrans bq cons dimqm gwmol xtb
-NW_MODULE_LIBS = -lmcscf -lmp2 -lmoints -lstepper -ldriver -loptim -lnwdft -lgradients -lcphf -lesp -lddscf -lguess -lhessian -lvib -lnwcutil -lrimp2 -lproperty -lsolvation -lnwints -lnwmd -lcafe -lspace -lqhop -lpfft -ldplot -lvscf -lqmd -letrans -lbq -lcons -ldimqm -lgwmol -lnwxtb
-EXCLUDED_SUBDIRS = develop nwxc scfaux ccsd prepar selci nwpw rimp2_grad python argos analyz diana drdy uccsdt qmmm rism tce geninterface mm perfm dntmc smd nbo dangchang leps ccca lucia rdmft fcidump
-CONFIG_LIBS = 
