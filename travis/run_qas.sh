0320ae09232cc043c2a82a458d5da67f6557d597
edoapra <edoardo.apra@gmail.com>
2022-04-07 13:34:31 -0700

    tblite test

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -126,6 +126,9 @@ fi
      else
 	 echo ' dft_input stubbed'
      fi
+     if [[ ! $(grep -i dft $TRAVIS_BUILD_DIR/src/stubs.F| awk '/xtb_input/') ]]; then
+	 cd $TRAVIS_BUILD_DIR/QA && ./runtests.mpi.unix xtb_siosi7
+     fi
      if [[ "$USE_SIMINT" != "1" ]] ; then
 # check if pspw is among modules
 	 if [[ ! $(grep -i pspw $TRAVIS_BUILD_DIR/src/stubs.F| awk '/pspw_input/') ]]; then
@@ -155,9 +158,6 @@ fi
 	     cd $TRAVIS_BUILD_DIR/QA && ./runtests.mpi.unix procs $nprocs au2-sarc-zora-mp
 	   if [[ ! -z "$USE_LIBXC" ]]; then
 	       cd $TRAVIS_BUILD_DIR/QA && ./runtests.mpi.unix procs $nprocs libxc_he2+
-	   fi
-	   if [[ ! -z "$USE_TBLITE" ]]; then
-	       cd $TRAVIS_BUILD_DIR/QA && ./runtests.mpi.unix xtb_siosi7
 	   fi
 	 fi
        if [[ ! $(grep -i mp2_input $TRAVIS_BUILD_DIR/src/stubs.F| awk '/mp2_input/') ]]; then
