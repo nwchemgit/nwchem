5427c53a0f49ac0940a2182d1cd4256f7aedcfd5
edoapra <edoardo.apra@gmail.com>
2022-04-29 10:35:28 -0700

    redirect mpich log

--- src/libext/mpich/build_mpich.sh
+++ src/libext/mpich/build_mpich.sh
@@ -16,5 +16,17 @@ unset F90FLAGS
 echo 'using FFLAGS=' $FFLAGS
 ./configure --prefix=`pwd`/../.. --enable-fortran=all --disable-shared --disable-cxx --enable-romio --with-pm=gforker --with-device=ch3:nemesis --disable-cuda --disable-opencl --enable-silent-rules  --enable-fortran=all
 mkdir -p ../../../lib
-make -j3
-make install
+echo
+echo mpich compilation in progress
+echo output redirected to libext/mpich/mpich/mpich.log
+echo
+make -j3 >& mpich.log
+if [[ "$?" != "0" ]]; then
+    tail -500 mpich.log
+    echo " "
+    echo "MPICH compilation failed"
+    echo " "
+    exit 1
+fi
+
+make install >& mpich_install.log
