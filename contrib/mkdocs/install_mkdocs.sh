bcbd99ef34fca8f39cf15c50afe23dd1e3ad8eea
edoapra <edoardo.apra@gmail.com>
2022-05-26 18:32:53 -0700

    script update [ci skip]

--- contrib/mkdocs/install_mkdocs.sh
+++ contrib/mkdocs/install_mkdocs.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 python3 -m pip install --user mkdocs
 python3 -m pip install --user mkdocs-material
-git clone https://github.com/cmitu/mkdocs-altlink-plugin
-cd mkdocs-altlink-plugin
-python3 -m pip install --user -e .
-cd ..
+python3 -m pip install git+https://github.com/cmitu/mkdocs-altlink-plugin/
 python3 -m pip install --user python-markdown-math
