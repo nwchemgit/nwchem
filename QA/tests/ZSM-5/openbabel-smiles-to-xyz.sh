0e2b4221bb0b00f8000ab79fef32d31a0f611a6f
hjjvandam <hvandam@bnl.gov>
2022-07-18 12:01:03 -0400

    Adding 1-propanol

--- QA/tests/ZSM-5/openbabel-smiles-to-xyz.sh
+++ QA/tests/ZSM-5/openbabel-smiles-to-xyz.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 obabel -h --gen3d -ismi SMILES/h2o.smi        -oxyz -Oxyz/h2o.xyz
+obabel -h --gen3d -ismi SMILES/1-propanol.smi -oxyz -Oxyz/1-propanol.xyz
 obabel -h --gen3d -ismi SMILES/2-propanol.smi -oxyz -Oxyz/2-propanol.xyz
 obabel -h --gen3d -ismi SMILES/propene.smi    -oxyz -Oxyz/propene.xyz
