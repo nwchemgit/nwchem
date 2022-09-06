63171e63b26488618e95baffa05d64cddbdc5c5c
Alex Woods <s2270946@ed.ac.uk>
2022-09-06 19:09:03 +0100

    update build

--- src/config/makefile.h
+++ src/config/makefile.h
@@ -3706,6 +3706,13 @@ ifdef TCE_CUDA
     endif
 endif
 
+ifdef TCE_OPENACC
+    DEFINES += -DTCE_OPENACC
+    ifeq ($(_FC),gfortran)
+        CORE_LIBS += -fopenacc
+    endif
+endif
+
 ifndef HIP
     HIP = hipcc
 endif
