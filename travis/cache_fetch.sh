50384aa745cda7af770f016fec2726b528886bfc
edoapra <edoardo.apra@gmail.com>
2022-09-08 19:23:47 -0700

    scripts for github actions

---
+++ travis/cache_fetch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+          ls -Rla ~/cache
+          [ -d ~/cache/libext ] && rsync -av ~/cache/libext/* src/libext/.  
+          [ -d ~/cache/simint/simint_install/lib ] && \
+          mkdir -p src/NWints/simint/libsimint_source/simint_install && \
+          rsync -av ~/cache/simint/simint_install/* src/NWints/simint/libsimint_source/simint_install/.   || true
+          [ -f ~/cache/tarballs/dftd3.tgz ] && rsync -av ~/cache/tarballs/dftd3.tgz  src/nwpw/nwpwlib/nwpwxc/dftd3.tgz  || true
+          echo "cache fetched"
