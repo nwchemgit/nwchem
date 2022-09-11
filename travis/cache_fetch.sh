b8bc56ecb9b0cf1c44f54c774148cf4d1f8a4fe8
edoapra <edoardo.apra@gmail.com>
2022-09-11 15:28:02 -0700

    cache fix

--- travis/cache_fetch.sh
+++ travis/cache_fetch.sh
@@ -5,5 +5,5 @@
           mkdir -p src/NWints/simint/libsimint_source/simint_install && \
           rsync -av ~/cache/simint/simint_install/* src/NWints/simint/libsimint_source/simint_install/.   || true
           [ -f ~/cache/tarballs/dftd3.tgz ] && rsync -av ~/cache/tarballs/dftd3.tgz  src/nwpw/nwpwlib/nwpwxc/dftd3.tgz  || true
-          [ -f "cache/libxc/install/lib/libxc.a" ] && rsync -av ~/cache/libxc/install src/libext/libxc/. || true
+          [ -f "~/cache/libxc/install/lib/libxc.a" ] && rsync -av ~/cache/libxc/install src/libext/libxc/. || true
           echo "cache fetched"
