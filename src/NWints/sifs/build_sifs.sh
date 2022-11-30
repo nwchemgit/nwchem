ca9075815ee1f02267be217a470d221cf1a8bf51
Lachlan Thomas Belcher <lbelcher@koehr04.ib0.icexa.navydsrc.hpc.mil>
2022-11-30 23:50:10 +0000

    sifs: allow download of columbus sifs files from columbus repository

---
+++ src/NWints/sifs/build_sifs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+
+rm -rf columbu* *.F
+curl -LJO https://gitlab.com/api/v4/projects/36816383/repository/archive?path=Columbus/source/gcfci/colib/sifs
+ls -l columbus-master*gz
+tar xzf columbus-master*gz
+mv $(find columbus-master*s -name "*F") .
+wget -O bummer.F https://gitlab.com/columbus-program-system/columbus/-/raw/master/Columbus/source/gcfci/colib/humanio/bummer.F?inline=false
