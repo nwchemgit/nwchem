16e5c43f35d13bf4ee689e0eaff6927389174e08
edoapra <edoardo.apra@gmail.com>
2022-12-01 11:37:39 -0800

    fix for flang segv on entry

--- src/NWints/sifs/build_sifs.sh
+++ src/NWints/sifs/build_sifs.sh
@@ -8,3 +8,19 @@ wget -O columbus-master.tar.gz https://gitlab.com/api/v4/projects/36816383/repos
 tar xzf columbus-master.tar.gz
 mv $(find columbus-master*s -name "*F") .
 rm -rf columbus-master*
+rm -f ibummr.patch
+cat > ibummr.patch <<EOF
+--- bummer.F.org	2022-12-01 11:34:21.510551238 -0800
++++ bummer.F	2022-12-01 11:32:28.997830864 -0800
+@@ -201,7 +201,7 @@
+ c
+ c     # initialization...
+ c
+-      entry ibummr( iunit )
++c      entry ibummr( iunit )
+ c
+ c     # save the listing unit for use later.
+ c
+EOF
+patch -p0 < ibummr.patch
+rm -f ibummr.patch
