0fbf76c425a67e18d347a013ca883ad589233efe
edoapra <edoardo.apra@gmail.com>
2022-11-30 17:15:28 -0800

    fault-toleran wget

--- src/NWints/sifs/build_sifs.sh
+++ src/NWints/sifs/build_sifs.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 rm -rf columbu* *.F
-curl -LJO https://gitlab.com/api/v4/projects/36816383/repository/archive?path=Columbus/source/gcfci/colib/sifs
-ls -l columbus-master*gz
-tar xzf columbus-master*gz
+tries=0 ; until [ "$tries" -ge 10 ] ; do \
+wget -O columbus-master.tar.gz https://gitlab.com/api/v4/projects/36816383/repository/archive?path=Columbus/source/gcfci/colib/sifs \
+&& wget -O bummer.F https://gitlab.com/columbus-program-system/columbus/-/raw/master/Columbus/source/gcfci/colib/humanio/bummer.F?inline=false\
+	    && break ;\
+            tries=$((tries+1)) ; echo attempt no.  $tries    ; sleep 20 ;  done
+tar xzf columbus-master.tar.gz
 mv $(find columbus-master*s -name "*F") .
-wget -O bummer.F https://gitlab.com/columbus-program-system/columbus/-/raw/master/Columbus/source/gcfci/colib/humanio/bummer.F?inline=false
+rm -rf columbus-master*
