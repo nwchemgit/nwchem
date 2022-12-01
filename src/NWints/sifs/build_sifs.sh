fd57fbb985ec59171f744cb70cce531f6f3ad957
Lachlan Thomas Belcher <lbelcher@koehr02.ib0.icexa.navydsrc.hpc.mil>
2022-12-01 20:22:46 +0000

    adding more sifs files

--- src/NWints/sifs/build_sifs.sh
+++ src/NWints/sifs/build_sifs.sh
@@ -3,7 +3,9 @@
 
 rm -rf columbu* *.F
 curl -LJO https://gitlab.com/api/v4/projects/36816383/repository/archive?path=Columbus/source/gcfci/colib/sifs
-ls -l columbus-master*gz
-tar xzf columbus-master*gz
-mv $(find columbus-master*s -name "*F") .
+curl -LJO https://gitlab.com/api/v4/projects/36816383/repository/archive?path=Columbus/source/gcfci/colib/pack
+curl -LJO https://gitlab.com/api/v4/projects/36816383/repository/archive?path=Columbus/source/gcfci/colib/basicio
+for f in columbus-master*gz; do tar xzf "$f"; done
+mv $(find columbus-master*/ -name "*F") .
 wget -O bummer.F https://gitlab.com/columbus-program-system/columbus/-/raw/master/Columbus/source/gcfci/colib/humanio/bummer.F?inline=false
+wget -O izero.F https://gitlab.com/columbus-program-system/columbus/-/raw/master/Columbus/source/gcfci/colib/linear_algebra/izero.F?inline=false
