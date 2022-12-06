4cfb91e339410d6f6963285d20d329000333ed52
edoapra <edoardo.apra@gmail.com>
2022-12-06 14:49:48 -0800

    silence pip install [ci skip]

--- contrib/mkdocs/install_mkdocs.sh
+++ contrib/mkdocs/install_mkdocs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-python3 -m pip install --user mkdocs
-python3 -m pip install --user mkdocs-material
-python3 -m pip install --user git+https://github.com/cmitu/mkdocs-altlink-plugin/
-python3 -m pip install --user python-markdown-math
-python3 -m pip install --user install pymdown-extensions
+python3 -m pip install --user mkdocs --quiet
+python3 -m pip install --user mkdocs-material --quiet
+python3 -m pip install --user git+https://github.com/cmitu/mkdocs-altlink-plugin/ --quiet
+python3 -m pip install --user python-markdown-math --quiet
+python3 -m pip install --user install pymdown-extensions --quiet
