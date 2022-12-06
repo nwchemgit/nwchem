f386f552b3a17fca202973462e127bc9334b4cb5
edoapra <edoardo.apra@gmail.com>
2022-12-06 15:03:23 -0800

    use-pep517 pip option [ci skip]

--- contrib/mkdocs/install_mkdocs.sh
+++ contrib/mkdocs/install_mkdocs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+python3 -m pip install --upgrade pip --quiet
+python3 -m pip install --user  MarkupSafe --use-pep517 --quiet
 python3 -m pip install --user mkdocs --quiet
 python3 -m pip install --user mkdocs-material --quiet
-python3 -m pip install --user git+https://github.com/cmitu/mkdocs-altlink-plugin/ --quiet
+python3 -m pip install --user git+https://github.com/cmitu/mkdocs-altlink-plugin/ --use-pep517 --quiet
 python3 -m pip install --user python-markdown-math --quiet
 python3 -m pip install --user install pymdown-extensions --quiet
