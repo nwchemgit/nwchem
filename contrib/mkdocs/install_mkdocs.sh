04a55a3b9e68708d7ee5f8e21d8ade8de54d2c3e
edoapra <edoardo.apra@gmail.com>
2022-12-05 18:39:46 -0800

    markdown extension of get strikethrough working [ci skip]

--- contrib/mkdocs/install_mkdocs.sh
+++ contrib/mkdocs/install_mkdocs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python3 -m pip install --user mkdocs
 python3 -m pip install --user mkdocs-material
-python3 -m pip install git+https://github.com/cmitu/mkdocs-altlink-plugin/
+python3 -m pip install --user git+https://github.com/cmitu/mkdocs-altlink-plugin/
 python3 -m pip install --user python-markdown-math
+python3 -m pip install --user install pymdown-extensions
