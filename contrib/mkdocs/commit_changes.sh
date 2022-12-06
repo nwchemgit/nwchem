2d7013f89d8ef027158c2c909fe401e4a789809a
edoapra <edoardo.apra@gmail.com>
2022-12-06 14:50:19 -0800

    remove slow git diff --stat [ci skip]

--- contrib/mkdocs/commit_changes.sh
+++ contrib/mkdocs/commit_changes.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git config user.name "nwchemgit"
 git config user.email "nwchemgit@gmail.com"
+git config advice.updateSparsePath false
 git add -A
 git commit -m website\ changes\ on\ `date +%a_%b_%d_%H:%M:%S_%Y` 
-git diff --stat --cached origin/master
+#git diff --stat --cached origin/master
+
