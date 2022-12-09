3fb4e7717c7ea40933d25d4b1b44ba08b6eccb4a
edoapra <edoardo.apra@gmail.com>
2022-12-08 22:49:38 -0800

    add subdirs  [ci skip]

--- contrib/mkdocs/commit_changes.sh
+++ contrib/mkdocs/commit_changes.sh
@@ -2,7 +2,8 @@
 git config user.name "nwchemgit"
 git config user.email "nwchemgit@gmail.com"
 git config advice.updateSparsePath false
-git add -A
+#git add -A
+git add .
 git commit -m website\ changes\ on\ `date +%a_%b_%d_%H:%M:%S_%Y` 
 #git diff --stat --cached origin/master
 
