b59bb4b86a931e332e9a4b48fa076216043a28be
edoapra <edoardo.apra@gmail.com>
2022-12-12 17:08:36 -0800

    update [ci skip]

--- contrib/mkdocs/install_mkdocs.sh
+++ contrib/mkdocs/install_mkdocs.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-python3 -m pip install --upgrade pip --quiet
-python3 -m pip install --user  MarkupSafe --use-pep517 --quiet
-python3 -m pip install --user mkdocs --quiet
-python3 -m pip install --user mkdocs-material --quiet
-python3 -m pip install --user git+https://github.com/cmitu/mkdocs-altlink-plugin/ --use-pep517 --quiet
-python3 -m pip install --user python-markdown-math --quiet
-python3 -m pip install --user install pymdown-extensions --quiet
-#python3 -m pip install --user  install mkdocs-with-pdf --quiet
-python3 -m pip install --user  install mkdocs-print-site-plugin --quiet
+if [[ -z "$VIRTUAL_ENV" ]]; then
+    USEROPT=--user
+else
+    USEROPT=
+fi
+python3 -m pip install --upgrade pip --quiet  $USEROPT
+python3 -m pip install  MarkupSafe --use-pep517 --quiet $USEROPT
+python3 -m pip install mkdocs --quiet $USEROPT
+python3 -m pip install mkdocs-material --quiet $USEROPT
+python3 -m pip install git+https://github.com/cmitu/mkdocs-altlink-plugin/ --use-pep517 --quiet $USEROPT
+python3 -m pip install python-markdown-math --quiet $USEROPT
+python3 -m pip install install pymdown-extensions --quiet $USEROPT
+#python3 -m pip install install mkdocs-with-pdf --quiet $USEROPT
+python3 -m pip install install mkdocs-print-site-plugin --quiet $USEROPT
