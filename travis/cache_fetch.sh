1078817034080d558f2ead2f4be510aaef3bceb3
edoapra <edoardo.apra@gmail.com>
2023-03-03 15:33:06 -0800

    check mpich cache

--- travis/cache_fetch.sh
+++ travis/cache_fetch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -v
           ls -Rla ~/cache
           [ -d ~/cache/libext ] && rsync -av ~/cache/libext/* src/libext/.  
           [ -d ~/cache/simint/simint_install/lib ] && \
@@ -7,3 +8,7 @@
           [ -f ~/cache/tarballs/dftd3.tgz ] && rsync -av ~/cache/tarballs/dftd3.tgz  src/nwpw/nwpwlib/nwpwxc/dftd3.tgz  || true
           [ -f ~/cache/libxc/install/lib/libxc.a ] && rsync -av ~/cache/libxc/install src/libext/libxc/. || true
           echo "cache fetched"
+	  echo " mpich debug "
+	  ls -lart /home/runner/work/nwchem/nwchem/src/libext/mpich/mpich/../../include || true
+	  ls -lart /home/runner/work/nwchem/nwchem/src/libext/include || true
+	  ls -lart /home/runner/work/nwchem/nwchem/src/libext/mpich || true
