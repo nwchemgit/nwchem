94c7f121cf4d36c2a62cd2d27681628c4de39e7e
edoapra <edoardo.apra@gmail.com>
2023-03-16 13:06:36 -0700

    use _FC when set

--- src/config/strip_compiler.sh
+++ src/config/strip_compiler.sh
@@ -1,4 +1,8 @@
 #/usr/bin/env bash
 #      ifeq ($(shell basename -- $(FC)| cut -d \- -f 1),nvfortran)
-echo $(basename -- $1 | cut -d \- -f 1 | sed 's/[0-9]*//g')
+if [[ -z $_FC ]]; then
+    echo $(basename -- $1 | cut -d \- -f 1 | sed 's/[0-9]*//g')
+else
+    echo $_FC
+fi
 
