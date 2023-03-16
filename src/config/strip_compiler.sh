e64a264100d64e39378f9c4884bfb95540873ba9
edoapra <edoardo.apra@gmail.com>
2023-03-16 13:20:05 -0700

    update

--- src/config/strip_compiler.sh
+++ src/config/strip_compiler.sh
@@ -1,8 +1,11 @@
 #/usr/bin/env bash
 #      ifeq ($(shell basename -- $(FC)| cut -d \- -f 1),nvfortran)
-if [[ -z $_FC ]]; then
-    echo $(basename -- $1 | cut -d \- -f 1 | sed 's/[0-9]*//g')
-else
+#gcc  or gfortran?
+if [[ "$1" == *fortran* ]] && [[ ! -z $_FC ]]; then
     echo $_FC
+elif [[ "$1" == *cc* ]] && [[ ! -z $_CC ]]; then
+    echo $_CC
+else
+    echo $(basename -- $1 | cut -d \- -f 1 | sed 's/[0-9]*//g')
 fi
 
