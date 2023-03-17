7e4ea3c5cd9205572fbe42c8a86f18ada43409ec
edoapra <edoardo.apra@gmail.com>
2023-03-17 11:03:03 -0700

    strip-debug only on linux

--- src/NWints/simint/libsimint_source/build_simint.sh
+++ src/NWints/simint/libsimint_source/build_simint.sh
@@ -245,9 +245,7 @@ cd ../..
 echo ln -sf  simint.l${SIMINT_MAXAM}_p${PERMUTE_SLOW}_d${DERIV}.install simint_install
 ln -sf  simint.l${SIMINT_MAXAM}_p${PERMUTE_SLOW}_d${DERIV}.install simint_install
 cd simint_install/lib
-if [[ $(uname -s) == "Linux" ]]; then
-    strip --strip-debug libsimint.a
-fi    
+strip --strip-debug libsimint.a
 ln -sf libsimint.a libnwc_simint.a
 export SIMINT_HOME=${SRC_HOME}/simint.l${SIMINT_MAXAM}_p${PERMUTE_SLOW}_d${DERIV}.install
 echo 'SIMINT library built with maximum angular momentum='${SIMINT_MAXAM}
