10729d0c32452f930237dd1aaeff5a9fc5a3e75a
edoapra <edoardo.apra@gmail.com>
2023-08-01 14:53:20 -0700

    GFORTRAN_MARCH to set -march value (e.g. GFORTRAN_MARCH=znver3 sets -march=znver3)

--- src/config/check_env.sh
+++ src/config/check_env.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-if [[ -z $1 ]]; then
+if [[ ! -z $GFORTRAN_MARCH ]] ; then
+    exit=0
+elif [[ -z $1 ]]; then
     #undefined
     exit=1
-elif [[ $1 == "N" ]] || [[ $1 == "n" ]] || [[ $1 == "0" ]] ; then
+elif [[ $1 == "N" ]] || [[ $1 == "n" ]] || [[ $1 == "0" ]] || [[ ! -z $GFORTRAN_MARCH ]] ; then
     exit=0
 else
     exit=1
