8975c57a2c90c7db0181b3222911850b9674681e
edoapra <edoardo.apra@gmail.com>
2023-09-22 13:33:30 -0700

    fix typo

--- src/config/fix_xcode15.sh
+++ src/config/fix_xcode15.sh
@@ -6,7 +6,7 @@ if [[ $(uname -s) == "Darwin" ]]; then
 	echo got xcode15
 	export GOT_XCODE15=1
 	export OMPI_FCFLAGS=" -Wl,-ld_classic "
-        export OMPI_CLAGS=" -Wl,-ld_classic -Wno-unused-command-line-argument "
+        export OMPI_CFLAGS=" -Wl,-ld_classic -Wno-unused-command-line-argument "
 #	export MPICH_FC="mpif90 -Wl,-ld_classic "
 #	export MPICH_CC="mpicc -Wl,-ld_classic "
 #	env|egrep MPICH_
