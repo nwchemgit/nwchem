4d1cebf2b6ae0389b920f2d9e995e58945c07f78
edoapra <edoardo.apra@gmail.com>
2023-12-04 10:19:43 -0800

    fix for xcode 15 linker

--- src/config/fix_xcode15.sh
+++ src/config/fix_xcode15.sh
@@ -5,8 +5,11 @@ if [[ $(uname -s) == "Darwin" ]]; then
     if [[ $( [ $xcode_v -ge 15 ] && echo 1) ]] ; then
 	echo got xcode15
 	export GOT_XCODE15=1
-	export OMPI_FCFLAGS=" -Wl,-ld_classic "
-        export OMPI_CFLAGS=" -Wl,-ld_classic -Wno-unused-command-line-argument "
+#figure out where the open-mpi libs are
+        OMPILIBDIR=$(brew --cellar)/../opt/open-mpi/lib
+	export OMPI_LDFLAGS=" -ld_classic  -L$OMPILIBDIR"
+#	export OMPI_FCFLAGS=" -Wl,-ld_classic "
+#        export OMPI_CFLAGS=" -Wl,-ld_classic -Wno-unused-command-line-argument "
 #	export MPICH_FC="mpif90 -Wl,-ld_classic "
 #	export MPICH_CC="mpicc -Wl,-ld_classic "
 #	env|egrep MPICH_
