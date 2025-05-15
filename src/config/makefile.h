8e423e84c5486d2e9bb8f909a08cc7733548172b
edoapra <edoardo.apra@gmail.com>
2025-05-15 12:32:06 -0700

    check existence of ga-config

--- src/config/makefile.h
+++ src/config/makefile.h
@@ -3750,13 +3750,14 @@ endif
 
 # lower level libs used by communication libraries 
 #case guard against case when tools have not been compiled yet
-#  ifeq ("$(wildcard ${GA_PATH}/bin/ga-config)","")
-#  else
+ifeq ("$(wildcard ${GA_PATH}/bin/ga-config)","")
+else
 COMM_LIBS :=  $(shell ${GA_PATH}/bin/ga-config --network_ldflags)
 COMM_LIBS :=  $(COMM_LIBS) $(shell ${GA_PATH}/bin/ga-config --network_libs)
 #comex bit
 #COMM_LIBS +=  $(shell [ -e ${NWCHEM_TOP}/src/tools/build/comex/config.h ] && grep LIBS\ = ${NWCHEM_TOP}/src/tools/build/comex/Makefile|grep -v _LIBS| cut -b 8-) -lpthread
 COMM_LIBS := $(COMM_LIBS) $(shell [ -e ${GA_PATH}/bin/comex-config ] && ${GA_PATH}/bin/comex-config --libs) -lpthread
+endif
 ifdef COMM_LIBS 
     CORE_LIBS += $(COMM_LIBS) 
 endif 
