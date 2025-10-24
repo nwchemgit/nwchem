214f1f453150dc7e9bfbf73c99b990b953bb79e2
edoapra <edoardo.apra@gmail.com>
2025-10-23 22:26:36 -0700

    gcc

--- travis/cpuinfo.sh
+++ travis/cpuinfo.sh
@@ -2,7 +2,7 @@
 git clone -q https://github.com/pytorch/cpuinfo
 cd cpuinfo
 mkdir build && cd build
-CMAKE_POLICY_VERSION_MINIMUM=3.5 cmake  .. >& cmake.log
+CC=gcc CMAKE_POLICY_VERSION_MINIMUM=3.5 cmake  .. >& cmake.log
 tail -20 cmake.log
 make -j3 >& make.log
 tail -3 make.log
