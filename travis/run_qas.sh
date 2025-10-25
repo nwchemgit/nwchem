ab61c0584eef776e91613580d159e707451a314a
edoapra <edoardo.apra@gmail.com>
2025-10-24 17:31:41 -0700

    openmpi fixes

--- travis/run_qas.sh
+++ travis/run_qas.sh
@@ -5,8 +5,7 @@ set -e
     export OMPI_MCA_plm_rsh_agent=ssh
     export OMPI_MCA_btl=self,tcp
     export OMPI_MCA_osc=^ucx
-    export OMPI_MCA_btl_tcp_if_include=lo
-    export OMPI_MCA_btl=self,tcp
+#    export OMPI_MCA_btl_tcp_if_include=lo
 # source env. variables
 if [[ -z "$TRAVIS_BUILD_DIR" ]] ; then
     TRAVIS_BUILD_DIR=$(pwd)
